6ffca35332dcf5898a1c947ae61b3acc40bf0fd6
Federico Poli <federpoli@gmail.com>
2017-12-22 17:52:50 +0100

    Implement construction methods in the AstFactory, add test to verify an 'assert false'

--- src/ast_factory.rs
+++ src/ast_factory.rs
@@ -27,11 +27,23 @@ macro_rules! jobject_wrapper {
     );
 }
 
+macro_rules! map_to_jobject {
+    ($item:expr) => (
+        $item.map(|x| x.to_jobject())
+    );
+}
+
+macro_rules! map_to_jobjects {
+    ($items:expr) => (
+        map_to_jobject!($items.iter()).collect()
+    );
+}
+
 jobject_wrapper!(Program);
 jobject_wrapper!(Method);
 jobject_wrapper!(Seqn);
 jobject_wrapper!(Stmt);
-jobject_wrapper!(Exp);
+jobject_wrapper!(Expr);
 
 impl<'a> AstFactory<'a> {
     pub fn new(env: &'a JNIEnv) -> Self {
@@ -58,13 +70,15 @@ impl<'a> AstFactory<'a> {
     }
 
     pub fn new_program(&self, methods: Vec<&Method>) -> Program<'a> {
-        let obj: JObject = self.jni.unwrap_result(ast::Program::with(self.env).new(
+        let obj = self.jni.unwrap_result(ast::Program::with(self.env).new(
             self.jni.new_seq(vec![]),
             self.jni.new_seq(vec![]),
             self.jni.new_seq(vec![]),
             self.jni.new_seq(vec![]),
             self.jni.new_seq(
-                methods.iter().map(|x| x.to_jobject()).collect(),
+                map_to_jobjects!(
+                    methods
+                ),
             ),
             self.new_no_position(),
             self.new_no_info(),
@@ -73,13 +87,23 @@ impl<'a> AstFactory<'a> {
         Program { obj }
     }
 
-    pub fn new_method(&self, name: &str, body: Option<&Seqn>) -> Method<'a> {
-        let obj: JObject = self.jni.unwrap_result(ast::Method::with(self.env).new(
+    pub fn new_method(
+        &self,
+        name: &str,
+        body: Option<&Seqn>,
+        pres: Vec<&Expr>,
+        posts: Vec<&Expr>,
+    ) -> Method<'a> {
+        let obj = self.jni.unwrap_result(ast::Method::with(self.env).new(
             self.jni.new_string(name),
             self.jni.new_seq(vec![]),
             self.jni.new_seq(vec![]),
-            self.jni.new_seq(vec![]),
-            self.jni.new_seq(vec![]),
+            self.jni.new_seq(
+                map_to_jobjects!(pres),
+            ),
+            self.jni.new_seq(
+                map_to_jobjects!(posts),
+            ),
             self.jni.new_option(body.map(
                 |x| x.to_jobject(),
             )),
@@ -88,5 +112,46 @@ impl<'a> AstFactory<'a> {
             self.new_no_trafos(),
         ));
         Method { obj }
+    }
+
+    pub fn new_seqn(&self, stmts: Vec<&Stmt>) -> Seqn<'a> {
+        let obj = self.jni.unwrap_result(ast::Seqn::with(self.env).new(
+            self.jni.new_seq(
+                map_to_jobjects!(stmts),
+            ),
+            self.jni.new_seq(vec![]),
+            self.new_no_position(),
+            self.new_no_info(),
+            self.new_no_trafos(),
+        ));
+        Seqn { obj }
+    }
+
+    pub fn new_assert(&self, expr: &Expr) -> Stmt<'a> {
+        let obj = self.jni.unwrap_result(ast::Assert::with(self.env).new(
+            expr.to_jobject(),
+            self.new_no_position(),
+            self.new_no_info(),
+            self.new_no_trafos(),
+        ));
+        Stmt { obj }
+    }
+
+    pub fn new_true_lit(&self) -> Expr<'a> {
+        let obj = self.jni.unwrap_result(ast::TrueLit::with(self.env).new(
+            self.new_no_position(),
+            self.new_no_info(),
+            self.new_no_trafos(),
+        ));
+        Expr { obj }
+    }
+
+    pub fn new_false_lit(&self) -> Expr<'a> {
+        let obj = self.jni.unwrap_result(ast::FalseLit::with(self.env).new(
+            self.new_no_position(),
+            self.new_no_info(),
+            self.new_no_trafos(),
+        ));
+        Expr { obj }
     }
 }
--- tests/use_case.rs
+++ tests/use_case.rs
@@ -1,13 +1,17 @@
+#[macro_use]
+extern crate lazy_static;
 extern crate viper;
 extern crate error_chain;
 
 use viper::*;
 
-#[test]
-fn use_case() {
-    let viper: Viper = Viper::new();
+lazy_static! {
+    static ref VIPER: Viper = Viper::new();
+}
 
-    let verification_context: VerificationContext = viper.new_verification_context();
+#[test]
+fn empty_program() {
+    let verification_context: VerificationContext = VIPER.new_verification_context();
 
     let ast_factory = verification_context.new_ast_factory();
 
@@ -17,8 +21,40 @@ fn use_case() {
 
     let verifier = verification_context.new_verifier();
 
-    /*error_manager.register(/* ... */);
-    error_manager.register(/* ... */);*/
+    /* TODO:
+    error_manager.register(/* ... */);
+    error_manager.register(/* ... */);
+    */
+
+    let verification_result = verifier.verify(program, error_manager);
+
+    println!("Verification result: {:?}", verification_result);
+}
+
+#[test]
+fn assert_false() {
+    let verification_context: VerificationContext = VIPER.new_verification_context();
+
+    let ast_factory = verification_context.new_ast_factory();
+
+    let false_lit = ast_factory.new_false_lit();
+
+    let assertion = ast_factory.new_assert(&false_lit);
+
+    let seqn = ast_factory.new_seqn(vec![&assertion]);
+
+    let method = ast_factory.new_method("foo", Some(&seqn), vec![], vec![]);
+
+    let program = ast_factory.new_program(vec![&method]);
+
+    let error_manager = verification_context.new_error_manager();
+
+    let verifier = verification_context.new_verifier();
+
+    /* TODO:
+    error_manager.register(/* ... */);
+    error_manager.register(/* ... */);
+    */
 
     let verification_result = verifier.verify(program, error_manager);
 
