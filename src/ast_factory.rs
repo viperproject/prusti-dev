f5bee01973fc942f3f4a5941eb6a2ce6f59fea32
Federico Poli <federpoli@gmail.com>
2018-01-05 14:22:16 +0100

    Add construction of statements in ast_factory

--- src/ast_factory.rs
+++ src/ast_factory.rs
@@ -42,7 +42,8 @@ macro_rules! map_to_jobjects {
 
 jobject_wrapper!(Program);
 jobject_wrapper!(Method);
-jobject_wrapper!(Seqn);
+jobject_wrapper!(Field);
+jobject_wrapper!(LocalVarDecl);
 jobject_wrapper!(Stmt);
 jobject_wrapper!(Expr);
 jobject_wrapper!(Position);
@@ -147,7 +148,7 @@ impl<'a> AstFactory<'a> {
     pub fn new_method(
         &self,
         name: &str,
-        body: Option<&Seqn>,
+        body: Option<&Stmt>,
         pres: Vec<&Expr>,
         posts: Vec<&Expr>,
     ) -> Method<'a> {
@@ -164,14 +165,60 @@ impl<'a> AstFactory<'a> {
         )
     }
 
-    pub fn new_seqn(&self, stmts: Vec<&Stmt>) -> Seqn<'a> {
+    // === Statements ===
+
+    pub fn new_new_stmt(&self, lhs: &Expr, fields: Vec<&Field>) -> Stmt<'a> {
         build_ast_node!(
             self,
-            Seqn,
-            ast::Seqn,
-            self.jni.new_seq(map_to_jobjects!(stmts)),
-            self.jni.new_seq(vec![])
+            Stmt,
+            ast::NewStmt,
+            lhs.to_jobject(),
+            self.jni.new_seq(map_to_jobjects!(fields))
         )
+    }
+
+    pub fn new_local_var_assign(&self, lhs: &Expr, rhs: &Expr) -> Stmt<'a> {
+        build_ast_node!(
+            self,
+            Stmt,
+            ast::LocalVarAssign,
+            lhs.to_jobject(),
+            rhs.to_jobject()
+        )
+    }
+
+    pub fn new_field_assign(&self, lhs: &Expr, rhs: &Expr) -> Stmt<'a> {
+        build_ast_node!(
+            self,
+            Stmt,
+            ast::FieldAssign,
+            lhs.to_jobject(),
+            rhs.to_jobject()
+        )
+    }
+
+    pub fn new_method_call(
+        &self,
+        method_name: &str,
+        args: Vec<&Expr>,
+        targets: Vec<&Expr>,
+    ) -> Stmt<'a> {
+        build_ast_node!(
+            self,
+            Stmt,
+            ast::MethodCall,
+            self.jni.new_string(method_name),
+            self.jni.new_seq(map_to_jobjects!(args)),
+            self.jni.new_seq(map_to_jobjects!(targets))
+        )
+    }
+
+    pub fn new_exhale(&self, expr: &Expr) -> Stmt<'a> {
+        build_ast_node!(self, Stmt, ast::Exhale, expr.to_jobject())
+    }
+
+    pub fn new_inhale(&self, expr: &Expr) -> Stmt<'a> {
+        build_ast_node!(self, Stmt, ast::Inhale, expr.to_jobject())
     }
 
     pub fn new_assert(&self, expr: &Expr, pos: Position) -> Stmt<'a> {
@@ -195,6 +242,99 @@ impl<'a> AstFactory<'a> {
         ));
         Stmt { obj }
     }
+
+    pub fn new_fold(&self, acc: &Expr) -> Stmt<'a> {
+        build_ast_node!(self, Stmt, ast::Fold, acc.to_jobject())
+    }
+
+    pub fn new_unfold(&self, acc: &Expr) -> Stmt<'a> {
+        build_ast_node!(self, Stmt, ast::Unfold, acc.to_jobject())
+    }
+
+    pub fn new_package(&self, wand: &Expr, proof_script: &Stmt) -> Stmt<'a> {
+        build_ast_node!(
+            self,
+            Stmt,
+            ast::Package,
+            wand.to_jobject(),
+            proof_script.to_jobject()
+        )
+    }
+
+    pub fn new_apply(&self, wand: &Expr) -> Stmt<'a> {
+        build_ast_node!(self, Stmt, ast::Apply, wand.to_jobject())
+    }
+
+    pub fn new_seqn(&self, stmts: Vec<&Stmt>) -> Stmt<'a> {
+        build_ast_node!(
+            self,
+            Stmt,
+            ast::Seqn,
+            self.jni.new_seq(map_to_jobjects!(stmts)),
+            self.jni.new_seq(vec![])
+        )
+    }
+
+    pub fn new_if(&self, cond: &Expr, then_body: &Stmt, else_body: &Stmt) -> Stmt<'a> {
+        build_ast_node!(
+            self,
+            Stmt,
+            ast::If,
+            cond.to_jobject(),
+            then_body.to_jobject(),
+            else_body.to_jobject()
+        )
+    }
+
+    pub fn new_while(&self, cond: &Expr, invs: Vec<&Expr>, body: &Stmt) -> Stmt<'a> {
+        build_ast_node!(
+            self,
+            Stmt,
+            ast::While,
+            cond.to_jobject(),
+            self.jni.new_seq(map_to_jobjects!(invs)),
+            body.to_jobject()
+        )
+    }
+
+    pub fn new_label(&self, name: &str, invs: Vec<&Expr>) -> Stmt<'a> {
+        build_ast_node!(
+            self,
+            Stmt,
+            ast::Label,
+            self.jni.new_string(name),
+            self.jni.new_seq(map_to_jobjects!(invs))
+        )
+    }
+
+    pub fn new_goto(&self, target: &str) -> Stmt<'a> {
+        build_ast_node!(self, Stmt, ast::Goto, self.jni.new_string(target))
+    }
+
+    pub fn new_fresh(&self, vars: Vec<&Expr>) -> Stmt<'a> {
+        build_ast_node!(
+            self,
+            Stmt,
+            ast::Fresh,
+            self.jni.new_seq(map_to_jobjects!(vars))
+        )
+    }
+
+    pub fn new_constraining(&self, vars: Vec<&Expr>, body: &Stmt) -> Stmt<'a> {
+        build_ast_node!(
+            self,
+            Stmt,
+            ast::Constraining,
+            self.jni.new_seq(map_to_jobjects!(vars)),
+            body.to_jobject()
+        )
+    }
+
+    pub fn new_local_var_decl_stmt(&self, decl: &LocalVarDecl) -> Stmt<'a> {
+        build_ast_node!(self, Stmt, ast::LocalVarDeclStmt, decl.to_jobject())
+    }
+
+    // === Boolean expressions ===
 
     pub fn new_or(&self, left: &Expr, right: &Expr) -> Expr<'a> {
         build_ast_node!(self, Expr, ast::Or, left.to_jobject(), right.to_jobject())
