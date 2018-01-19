d0b1dd99abb77240bf09b246753b247495a04d75
Vytautas Astrauskas <vastrauskas@gmail.com>
2018-01-19 15:41:15 +0100

    Created stubs.

--- src/data.rs
+++ src/data.rs
@@ -2,13 +2,5 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-//! Prusti Interface is an interface between Prusti and Prusti-Viper.
-
-#![warn(missing_docs)]
-#![feature(rustc_private)]
-
-extern crate rustc;
-
-pub mod environment;
-pub mod verifier;
-pub mod data;
+//! This module defines data structures exchanged between a verifier and
+//! its environment.
