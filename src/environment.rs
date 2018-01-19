20e3600c532db3c3d9f1065091004b580290c880
Vytautas Astrauskas <vastrauskas@gmail.com>
2018-01-19 16:44:06 +0100

    Added stubs for verifier and environment.

--- src/environment.rs
+++ src/environment.rs
@@ -3,3 +3,27 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 //! This module defines the interface provided to a verifier.
+
+#[derive(Debug, Default, PartialEq, Eq, Hash, Clone, Copy)]
+/// Epoch uniquely identifies the state of the environment. In other
+/// words, if the epoch is the same, then we are verifying still exactly
+/// the same program.
+///
+/// The main idea of epochs is to allow reducing the amount of code that
+/// is sent to the back-end verifier because presence of many irrelevant
+/// terms can significantly slow down the prover.
+/// A unique identifier of the Rust procedure.
+pub struct Epoch(usize);
+
+impl Epoch {
+    /// Constructor.
+    pub fn new() -> Self {
+        Self { 0: 1 }
+    }
+}
+
+/// A facade to the Rust compiler.
+pub trait Environment {
+    /// Get the current epoch.
+    fn get_current_epoch(&self) -> Epoch;
+}
--- src/verifier.rs
+++ src/verifier.rs
@@ -2,4 +2,39 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-//! This module defines verifier's interface.
+//! This module defines the verifier's interface.
+
+use environment::Environment;
+use data::{VerificationResult, VerificationTask};
+
+/// A verifier builder is an object that lives entire program's
+/// lifetime, has no mutable state, and is responsible for constructing
+/// verifier instances. The user of this interface is supposed to create
+/// a new verifier for each crate he or she wants to verify. The main
+/// motivation for having a builder is to be able to cache the JVM
+/// initialization.
+trait VerifierBuilder {
+    /// Construct a new verifier object.
+    fn new_verifier(&mut self) -> Verifier;
+}
+
+/// A verifier is an object for verifying a single crate, potentially
+/// many times.
+trait Verifier {
+    /// Perform a specific verification task.
+    ///
+    /// A verifier is allowed to mutate its state and preserve state
+    /// between `verify` invocations, for example, to cache translation
+    /// results. However, verifier is not allowed to cache results from
+    /// queries to the environment via facade `env` because these
+    /// results may have changed between `verify` invocations.
+    ///
+    /// `env` is `mut` to allow caching query results.
+    fn verify(&mut self, env: &mut Environment, task: &VerificationTask) -> VerificationResult;
+
+    /// Invalidate all caches.
+    ///
+    /// TODO: Introduce a method `invalidate` that takes a list of
+    /// changes and invalidates only caches affected by these changes.
+    fn invalidate_all(&mut self, env: &mut Environment);
+}
