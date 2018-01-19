f5a1c66c62241c2ca4364a4a0c4f1c43f841e165
Vytautas Astrauskas <vastrauskas@gmail.com>
2018-01-19 16:01:16 +0100

    Added stubs for communication between prusti-viper and prusti.

--- src/data.rs
+++ src/data.rs
@@ -4,3 +4,48 @@
 
 //! This module defines data structures exchanged between a verifier and
 //! its environment.
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
+/// A unique identifier of the Rust procedure.
+pub struct ProcedureDefId(usize);
+
+/// A generator that generates unique procedure def ids.
+#[derive(Debug)]
+pub struct ProcedureDefIdGenerator {
+    last_value: ProcedureDefId,
+}
+
+impl ProcedureDefIdGenerator {
+    /// Constructor.
+    pub fn new() -> Self {
+        Self {
+            last_value: ProcedureDefId(1),
+        }
+    }
+    /// Generate a new unique ID.
+    pub fn get_new_id(&mut self) -> ProcedureDefId {
+        self.last_value = ProcedureDefId(self.last_value.0 + 1);
+        self.last_value
+    }
+}
+
+impl Default for ProcedureDefIdGenerator {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+/// A list of items to verify that is passed to a verifier.
+pub struct VerificationTask {
+    /// A list of procedures to verify.
+    pub procedures: Vec<ProcedureDefId>,
+}
+
+/// Verification result returned by a verifier.
+pub enum VerificationResult {
+    /// Verification was successful.
+    Success,
+    /// Verification failed. Errors should have been already emitted by
+    /// the verifier.
+    Failure,
+}
