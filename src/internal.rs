fa3d5d983308d12bb7bf413b2e494a5378d090a9
Federico Poli <federpoli@gmail.com>
2018-01-19 15:14:15 +0100

    Run rustfmt

--- src/internal.rs
+++ src/internal.rs
@@ -7,17 +7,14 @@
 
 /// This function is used to provide expression id to the surrounding
 /// function.
-pub fn __id(_uuid: usize) {
-}
+pub fn __id(_uuid: usize) {}
 
 /// This function is used to type-check boolean assertions. The first
 /// argument is the unique id of the expression. The second argument is
 /// the expression itself.
-pub fn __assertion(_uuid: usize, _spec: bool) {
-}
+pub fn __assertion(_uuid: usize, _spec: bool) {}
 
 /// This function is used to type-check terms used in triggers. The
 /// first argument is the unique id of the expression. The second
 /// argument is the expression itself.
-pub fn __trigger<T>(_uuid: usize, _spec: T) {
-}
+pub fn __trigger<T>(_uuid: usize, _spec: T) {}
