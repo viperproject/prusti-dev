a8cdc94d144635a3d911a34e232837151defcc71
Federico Poli <federpoli@gmail.com>
2018-03-20 16:26:54 +0100

    Detect basic blocks that are not part of the specification

--- src/environment/procedure.rs
+++ src/environment/procedure.rs
@@ -9,14 +9,17 @@ use prusti_interface::environment::Procedure as ProcedureSpec;
 use std::collections::HashSet;
 use rustc::mir::Mir;
 use std::cell::Ref;
+use rustc::mir::Terminator;
 use rustc::mir::BasicBlock;
 use rustc::mir::BasicBlockData;
+use rustc::mir::TerminatorKind;
+use rustc::middle::const_val::ConstInt;
 
 pub struct Procedure<'a, 'tcx: 'a> {
     tcx: TyCtxt<'a, 'tcx, 'tcx>,
     proc_def_id: ProcedureDefId,
     mir: Ref<'a, Mir<'tcx>>,
-    spec_basic_blocks: HashSet<BasicBlock>,
+    nonspec_basic_blocks: HashSet<BasicBlock>,
 }
 
 fn get_type_def_id<'tcx>(ty: Ty<'tcx>) -> Option<DefId> {
@@ -33,15 +36,111 @@ fn clean_type<'a, 'gcx, 'tcx>(tcx: TyCtxt<'a, 'gcx, 'tcx>, ty: Ty<'tcx>) -> Ty<'
     }
 }
 
-fn build_spec_basic_blocks<'tcx>(mir: &Mir<'tcx>) -> HashSet<BasicBlock> {
-    unimplemented!();
+fn get_normal_targets(terminator: &Terminator) -> Vec<BasicBlock> {
+    match terminator.kind {
+        TerminatorKind::Goto { ref target } |
+        TerminatorKind::Assert { ref target, .. } =>
+            vec![*target],
+
+        TerminatorKind::SwitchInt { ref targets, .. } =>
+            targets.clone(),
+
+        TerminatorKind::Resume |
+        TerminatorKind::Abort |
+        TerminatorKind::Return |
+        TerminatorKind::Unreachable =>
+            vec![],
+
+        TerminatorKind::DropAndReplace { ref target, ref unwind, .. } |
+        TerminatorKind::Drop { ref target, ref unwind, .. } =>
+            vec![*target],
+
+        TerminatorKind::Call { ref destination, ref cleanup, .. } => {
+            match *destination {
+                Some((_, target)) => vec![target],
+                None => vec![]
+            }
+        }
+
+        TerminatorKind::FalseEdges { ref real_target, ref imaginary_targets } => vec![*real_target],
+
+        TerminatorKind::FalseUnwind { ref real_target, ref unwind } => vec![*real_target],
+
+        TerminatorKind::Yield { .. } |
+        TerminatorKind::GeneratorDrop => unimplemented!(),
+    }
+}
+
+fn build_nonspec_basic_blocks<'tcx>(mir: &Mir<'tcx>) -> HashSet<BasicBlock> {
+    let dominators = mir.dominators();
+    let mut loop_heads: HashSet<BasicBlock> = HashSet::new();
+
+    for source in mir.basic_blocks().indices() {
+        let terminator = &mir[source].terminator;
+        if let Some(ref term) = *terminator {
+            for target in get_normal_targets(term) {
+                if dominators.is_dominated_by(source, target) {
+                    loop_heads.insert(target);
+                }
+            }
+        }
+    }
+
+    let mut nonspec_basic_blocks: HashSet<BasicBlock> = HashSet::new();
+    let mut visited: HashSet<BasicBlock> = HashSet::new();
+    let mut to_visit: Vec<BasicBlock> = vec![];
+    to_visit.push(mir.basic_blocks().indices().next().unwrap());
+
+    while !to_visit.is_empty() {
+        let source = to_visit.pop().unwrap();
+
+        if visited.contains(&source) {
+            continue;
+        } else {
+            visited.insert(source);
+            nonspec_basic_blocks.insert(source);
+        }
+
+        let terminator = &mir[source].terminator;
+        if let Some(ref term) = *terminator {
+            let is_loop_head = loop_heads.contains(&source);
+            if is_loop_head {
+                debug!("MIR block {:?} is a loop head", source);
+            }
+            for target in get_normal_targets(term) {
+                if is_loop_head {
+                    // Skip the following "if false"
+                    let target_terminator = &mir[target].terminator;
+                    if let Some(ref target_term) = *target_terminator {
+                        if let TerminatorKind::SwitchInt { ref discr, ref values, ref targets, .. } = target_term.kind {
+                            if format!("{:?}", discr) == "const false" {
+                                // Some assumptions
+                                assert!(values[0] == ConstInt::U8(0));
+                                assert!(values.len() == 1);
+
+                                // Do not visit the 'then' branch.
+                                // So, it will not be put into nonspec_basic_blocks.
+                                debug!("MIR block {:?} is the head of a specification branch", targets[0]);
+                                visited.insert(targets[0]);
+                            }
+                        }
+                    }
+                }
+                if !visited.contains(&target) {
+                    to_visit.push(target);
+                }
+            }
+        }
+    }
+
+    nonspec_basic_blocks
 }
 
 impl<'a, 'tcx> Procedure<'a, 'tcx> {
     pub fn new(tcx: TyCtxt<'a, 'tcx, 'tcx>, proc_def_id: ProcedureDefId) -> Self {
         let mir = tcx.mir_validated(proc_def_id).borrow();
-        let spec_basic_blocks = build_spec_basic_blocks(&mir);
-        Procedure { tcx, proc_def_id, mir, spec_basic_blocks }
+        let nonspec_basic_blocks = build_nonspec_basic_blocks(&mir);
+        Procedure { tcx, proc_def_id, mir, nonspec_basic_blocks }
     }
 
     pub fn get_declared_types(&self) -> Vec<Ty<'tcx>> {
@@ -72,7 +171,7 @@ impl<'a, 'tcx> ProcedureSpec for Procedure<'a, 'tcx> {
     fn walk_once_cfg<F>(&self, mut visitor: F) where F: FnMut(BasicBlock, &BasicBlockData) {
         let basic_blocks = self.mir.basic_blocks();
         for bbi in basic_blocks.indices() {
-            if !self.spec_basic_blocks.contains(&bbi) {
+            if self.nonspec_basic_blocks.contains(&bbi) {
                 let bb_data = &basic_blocks[bbi];
                 visitor(bbi, bb_data);
             }
