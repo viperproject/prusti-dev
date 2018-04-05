17900184a82fe6f35496aa338e7579fa149bbf81
Vytautas Astrauskas <vastrauskas@gmail.com>
2018-04-05 18:17:31 +0200

    Some version of the type visitor.

--- src/utils/type_visitor.rs
+++ src/utils/type_visitor.rs
@@ -15,11 +15,13 @@ pub trait TypeVisitor<'a, 'tcx> {
     fn tcx(&self) -> TyCtxt<'a, 'tcx, 'tcx>;
 
     fn visit_ty(&mut self, ty: Ty<'tcx>) {
+        trace!("visit_ty({:?})", ty);
         self.visit_sty(&ty.sty);
         self.visit_flags(ty.flags);
     }
 
     fn visit_sty(&mut self, sty: &TypeVariants<'tcx>) {
+        trace!("visit_sty({:?})", sty);
         match *sty {
             TyBool => {
                 self.visit_bool();
@@ -36,7 +38,8 @@ pub trait TypeVisitor<'a, 'tcx> {
             TyRef(region, tym) => {
                 self.visit_ref(region, tym);
             },
-            TyTuple(parts, _) => {
+            TyTuple(parts, diverging) => {
+                assert!(!diverging);
                 self.visit_tuple(parts);
             },
             _ => {
@@ -58,35 +61,41 @@ pub trait TypeVisitor<'a, 'tcx> {
     fn visit_uint(&mut self, _ty: UintTy) {
     }
 
-    fn visit_adt(&mut self, adt_def: &'tcx AdtDef, _subts: &'tcx Substs<'tcx>) {
-        self.visit_adt_def(adt_def);
-    }
-
-    fn visit_adt_def(&mut self, adt_def: &'tcx AdtDef) {
+    fn visit_adt(&mut self, adt_def: &'tcx AdtDef, substs: &'tcx Substs<'tcx>) {
+        trace!("visit_adt({:?})", adt_def);
         for variant in adt_def.variants.iter() {
-            self.visit_adt_variant(variant);
+            self.visit_adt_variant(variant, substs);
         }
     }
 
-    fn visit_adt_variant(&mut self, variant: &VariantDef) {
+    fn visit_adt_variant(&mut self, variant: &VariantDef, substs: &'tcx Substs<'tcx>) {
+        trace!("visit_adt_variant({:?})", variant);
         for field in variant.fields.iter() {
-            self.visit_field(field);
+            self.visit_field(field, substs);
         }
     }
 
-    fn visit_field(&mut self, field: &FieldDef) {
-        println!("field name: {:?}", field.name);
+    fn visit_field(&mut self, field: &FieldDef, substs: &'tcx Substs<'tcx>) {
+        trace!("visit_field({:?})", field);
+        let ty = field.ty(self.tcx(), substs);
+        self.visit_ty(ty);
     }
 
     fn visit_ref(&mut self, region: Region<'tcx>, tym: TypeAndMut<'tcx>) {
+        trace!("visit_ref({:?}, {:?})", region, tym);
         self.visit_ref_type(tym.ty, tym.mutbl);
     }
 
     fn visit_ref_type(&mut self, ty: Ty<'tcx>, mutability: Mutability) {
+        trace!("visit_ref_type({:?}, {:?})", ty, mutability);
         self.visit_ty(ty);
     }
 
     fn visit_tuple(&mut self, parts: &'tcx Slice<Ty<'tcx>>) {
+        trace!("visit_tuple({:?})", parts);
+        for part in parts.iter() {
+            self.visit_ty(part);
+        }
     }
 
 }
