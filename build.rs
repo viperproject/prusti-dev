6ab82101e9832a0725d85208cde32a19416ddad7
Federico Poli <federpoli@gmail.com>
2018-05-04 17:08:41 +0200

    Avoid unnecessary format!()

--- build.rs
+++ build.rs
@@ -21,7 +21,7 @@ fn main() {
     let generated_dir = format!("{}/gen", env::var("CARGO_MANIFEST_DIR").unwrap());
 
     let deps_dir = TempDir::new("deps").unwrap_or_else(|e| {
-        panic!(format!("{}", e));
+        panic!(e.to_string());
     });
 
     // If ASM_JAR is set, use it. Otherwise, download asm.jar to a temporary location.
@@ -30,14 +30,14 @@ fn main() {
         None => {
             let target = "http://central.maven.org/maven2/asm/asm/3.3.1/asm-3.3.1.jar";
             let mut response = reqwest::get(target).unwrap_or_else(|e| {
-                panic!(format!("{}", e));
+                panic!(e.to_string());
             });
             let fname = deps_dir.path().join("asm.jar");
             let mut dest = File::create(fname.clone()).unwrap_or_else(|e| {
-                panic!(format!("{}", e));
+                panic!(e.to_string());
             });
             copy(&mut response, &mut dest).unwrap_or_else(|e| {
-                panic!(format!("{}", e));
+                panic!(e.to_string());
             });
             fname.to_str().unwrap().to_string()
         }
@@ -589,7 +589,7 @@ fn main() {
         ])
         .generate(&generated_dir)
         .unwrap_or_else(|e| {
-            panic!(format!("{}", e.display_chain().to_string()));
+            panic!(e.display_chain().to_string());
         });
 
     // Remove the temporary directory
--- tests/verify_empty_program.rs
+++ tests/verify_empty_program.rs
@@ -44,11 +44,11 @@ fn verify_empty_program() {
         //.option("-XX:+TraceJNICalls")
         .build()
         .unwrap_or_else(|e| {
-            panic!(format!("{}", e.display_chain().to_string()));
+            panic!(e.display_chain().to_string());
         });
 
     let jvm = JavaVM::new(jvm_args).unwrap_or_else(|e| {
-        panic!(format!("{}", e.display_chain().to_string()));
+        panic!(e.display_chain().to_string());
     });
 
     let env = jvm.attach_current_thread()
@@ -112,6 +112,6 @@ fn verify_empty_program() {
             env.exception_describe()
                 .unwrap_or_else(|e| panic!(format!("{:?}", e)));
         }
-        panic!(format!("{}", e.display_chain().to_string()));
+        panic!(e.display_chain().to_string());
     });
 }
