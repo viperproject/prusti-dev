9430b5f347c34678fedcfcf9db5ee82b54926d48
Federico Poli <federpoli@gmail.com>
2018-05-18 09:55:01 +0200

    Add workaround for Silicon issue #315

--- src/viper.rs
+++ src/viper.rs
@@ -86,6 +86,15 @@ impl Viper {
 
         let this = Viper { jvm };
 
+        {
+            // Initialize the logging context
+            // See:
+            // - https://bitbucket.org/viperproject/silicon/issues/315/exception-while-building-silicon-instances
+            // - https://stackoverflow.com/a/16013093/2491528
+            let ver_context = this.new_verification_context();
+            ver_context.new_verifier();
+        }
+
         this
     }
 
