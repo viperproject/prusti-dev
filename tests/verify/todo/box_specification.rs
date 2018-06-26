7065ca69e74668f86940fbd82398e9198293e803
Federico Poli <federpoli@gmail.com>
2018-06-26 15:29:11 +0200

    Add test, remove unused file

---
+++ tests/verify/todo/box_specification.rs
@@ -0,0 +1,12 @@
+//! Example: functions that takes a mutable reference
+
+extern crate prusti_contracts;
+
+#[ensures="result == old(*my_box)"]
+fn foo(my_box: Box<i32>) -> i32 {
+    *my_box
+}
+
+fn main() {
+
+}
