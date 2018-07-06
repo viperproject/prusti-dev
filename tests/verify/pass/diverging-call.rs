8ff2d5649ec7422ce757fb2c5c9fc77e6bf750f8
Federico Poli <federpoli@gmail.com>
2018-07-06 16:12:37 +0200

    Fix non-terminating test

--- tests/verify/pass/diverging-call.rs
+++ tests/verify/pass/diverging-call.rs
@@ -5,9 +5,11 @@ fn diverging() -> ! {
     loop {}
 }
 
-fn main() {
+fn test() {
     diverging();
     // The return type is `Never`, so Prusti should assume `false` here.
     // The following assertion should not raise verification errors
     assert!(1 == 2);
 }
+
+fn main() {}
