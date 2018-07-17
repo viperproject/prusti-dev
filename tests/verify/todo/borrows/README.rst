ace9d76970cadc6a57be637c74ac4cf8d2bef1b9
Federico Poli <federpoli@gmail.com>
2018-07-17 11:20:54 +0200

    Remove ignored tests

--- tests/verify/todo/borrows/README.rst
+++
@@ -1,5 +0,0 @@
-==========
-2018-06-22
-==========
-
-Two examples for testing encoding of mutable borrows.
--- tests/verify/todo/print.rs
+++
@@ -1,8 +0,0 @@
-//! Example: print!(..) macro
-
-extern crate prusti_contracts;
-
-fn main() {
-    let x = 42;
-    println!("Hello world!", x);
-}
