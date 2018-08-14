474b09fd6be05d623748f4fa9419e7d3d8934dc4
Federico Poli <federpoli@gmail.com>
2018-08-14 14:03:29 +0200

    Add test for issue #15

---
+++ tests/verify/fail/unreachable.rs
@@ -0,0 +1,10 @@
+#![feature(nll)]
+
+extern crate prusti_contracts;
+
+// The end of the following function is unreachable
+fn foo(x: i32) -> i32 {
+    unreachable!(); //~ ERROR unreachable!(..) statement might be reachable
+}
+
+fn main(){}
