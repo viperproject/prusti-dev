00d1e454f7bc0a184a10fe8008f1e8862889adde
Federico Poli <federpoli@gmail.com>
2018-09-21 19:01:27 +0200

    Add annotations to the vector wrapper test

--- prusti/tests/verify/pass/with-spec/vec-wrapper.rs
+++ prusti/tests/verify/pass/with-spec/vec-wrapper.rs
@@ -5,28 +5,36 @@ pub struct VecWrapperI32{
 }
 
 impl VecWrapperI32 {
-
+    // Encoded as body-less Viper function
     #[trusted]
     #[pure]
     pub fn len(&self) -> usize {
         self.v.len()
     }
 
+    // Encoded as body-less Viper method
     #[trusted]
     #[ensures="result.len() == 0"]
-    pub fn new() -> Self {
-        VecWrapperI32{ v: vec![] }
+    #[ensures="forall i: usize :: (0 <= i && i < length) ==> result.lookup(i) == 0"]
+    pub fn new(length: usize) -> Self {
+        VecWrapperI32{ v: vec![0; length] }
     }
 
+    // Encoded as body-less Viper function
     #[trusted]
     #[pure]
+    #[requires="0 <= index && index < self.len()"]
     pub fn lookup(&self, index: usize) -> i32 {
         self.v[index]
     }
 
+    // Encoded as body-less Viper method
     #[trusted]
+    #[requires="0 <= index && index < self.len()"]
     #[ensures="self.lookup(old(index)) == old(value)"]
     pub fn store(&mut self, index: usize, value: i32) {
         self.v[index] = value;
     }
 }
+
+fn main() {}
