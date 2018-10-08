cc2c20d7a3cedb07abf919ab69444a43c09bd244
Vytautas Astrauskas <vastrauskas@gmail.com>
2018-10-08 20:26:50 +0200

    Compute loan trees for the package statements.

---
+++ prusti/tests/verify/pass/no-annotations/return_borrow.rs
@@ -0,0 +1,44 @@
+extern crate prusti_contracts;
+
+struct G {
+    value: u32,
+}
+
+struct F {
+    f: G,
+    g: G,
+}
+
+fn test1(x: &mut F) -> &mut G {
+    let y = x;
+    let z = y;
+    let z = &mut *z;
+    let z = &mut *z;
+    let z = &mut *z;
+    let z = &mut *z;
+    let z = &mut *z;
+    let z = &mut *z;
+    let z = &mut *z;
+    &mut z.f
+}
+
+fn test2(x: &mut F, b: bool) -> &mut G {
+    let y = &mut *x;
+    let z = y;
+    let z2;
+    if b {
+        z2 = z;
+    } else {
+        z2 = x;
+    }
+    let z = z2;
+    let z = z;
+    let z = &mut *z;
+    let z = &mut *z;
+    let z = &mut *z;
+    let z = &mut *z;
+    &mut z.g
+}
+
+fn main() {
+}
