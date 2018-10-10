2f1734941d0ed65981b747690474e63ad24beeba
Vytautas Astrauskas <vastrauskas@gmail.com>
2018-10-10 12:07:39 +0200

    Fix region parsing regex.

--- prusti-interface/src/environment/borrowck/regions.rs
+++ prusti-interface/src/environment/borrowck/regions.rs
@@ -20,7 +20,7 @@ pub fn load_variable_regions(path: &Path) -> io::Result<HashMap<mir::Local, fact
     let file = File::open(path)?;
     let fn_sig = Regex::new(r"^fn [a-zA-Z\d_]+\((?P<args>.*)\) -> (?P<result>.*)\{$").unwrap();
     let arg = Regex::new(r"^_(?P<local>\d+): &'(?P<rvid>\d+)rv (mut)? [a-zA-Z\d_]+\s*$").unwrap();
-    let local = Regex::new(r"^\s+let mut _(?P<local>\d+): &'(?P<rvid>\d+)rv ").unwrap();
+    let local = Regex::new(r"^\s+let( mut)? _(?P<local>\d+): &'(?P<rvid>\d+)rv ").unwrap();
     for line in io::BufReader::new(file).lines() {
         let line = line?;
         if let Some(caps) = fn_sig.captures(&line) {
