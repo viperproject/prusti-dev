5f8f39ca8445a93d9217a14a8fab534f1a8c9698
Vytautas Astrauskas <vastrauskas@gmail.com>
2018-10-16 12:17:54 +0200

    Added problematic examples.

--- prusti/tests/verify/pass/no-annotations/borrow_die.rs
+++ prusti/tests/verify/pass/no-annotations/borrow_die.rs
@@ -9,6 +9,16 @@ struct F {
     g: G,
 }
 
+fn test4(y: &mut F, z: &mut F) {
+    let mut x = &mut *y;
+    let f = &mut x.f;
+    let g = &mut x.g;
+    x = &mut *z;
+    use_both(f, g);
+    let f2 = &mut x.f;
+}
+
+
 fn test1(x: &mut F) {
     let y = x;
     let z = y;
@@ -36,6 +46,16 @@ fn test2(x: &mut F, b: bool) {
     let _z = &mut *_z;
     let _z = &mut *_z;
     let _z = &mut *_z;
+}
+
+fn test3(y: &mut F) {
+    let x = &mut *y;
+    let f = &mut x.f;
+    let g = &mut x.g;
+    use_both(f, g);
+}
+
+fn use_both(_f: &mut G, _g: &mut G) {
 }
 
 fn main() {
