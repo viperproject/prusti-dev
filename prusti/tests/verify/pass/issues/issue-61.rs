ce6bb56b178129bed0abcdafe1f791273314a9d7
Federico Poli <federpoli@gmail.com>
2018-10-20 16:23:21 +0200

    Add test for issue #61

---
+++ prusti/tests/verify/pass/issues/issue-61.rs
@@ -0,0 +1,59 @@
+/// Issue #61: "Precondition of pure function `lookup` does not hold"
+
+extern crate prusti_contracts;
+
+pub struct VecWrapperI32{
+    v: Vec<i32>
+}
+
+impl VecWrapperI32 {
+    // Encoded as body-less Viper function
+    #[trusted]
+    #[pure]
+    pub fn len(&self) -> usize {
+        self.v.len()
+    }
+
+    // Encoded as body-less Viper method
+    #[trusted]
+    #[ensures="result.len() == length"]
+    #[ensures="forall i: usize :: (0 <= i && i < length) ==> result.lookup(i) == 0"]
+    pub fn new(length: usize) -> Self {
+        VecWrapperI32{ v: vec![0; length] }
+    }
+
+    // Encoded as body-less Viper function
+    #[trusted]
+    #[pure]
+    #[requires="0 <= index && index < self.len()"]
+    pub fn lookup(&self, index: usize) -> i32 {
+        self.v[index]
+    }
+
+    // Encoded as body-less Viper method
+    #[trusted]
+    #[requires="0 <= index && index < self.len()"]
+    #[ensures="self.lookup(old(index)) == old(value)"]
+    pub fn store(&mut self, index: usize, value: i32) {
+        self.v[index] = value;
+    }
+}
+
+#[requires="index >= 0"]
+#[requires="forall i: usize :: (0 <= i && i < v.len()) ==> v.lookup(i) == 100"]
+//#[ensures="index == old(index)"]
+//#[ensures="default_val == old(default_val)"]
+//#[ensures="if index < old(v.len()) { result == 100 } else { result == default_val }"]
+fn extract(v: VecWrapperI32, index: usize, default_val: i32) -> i32 {
+    if index < v.len() {
+        assert!(index < v.len());
+        v.lookup(index)
+    } else {
+        default_val
+    }
+}
+
+#[trusted]
+fn main(){
+    println!("It works!");
+}
