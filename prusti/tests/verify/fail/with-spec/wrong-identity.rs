ba1dccdd3e4c015a12d2d945020dc35ce9515a7f
Federico Poli <federpoli@gmail.com>
2018-10-20 16:32:29 +0200

    Fix annotation of 'fail' tests

--- prusti/tests/verify/fail/with-spec/wrong-identity.rs
+++ prusti/tests/verify/fail/with-spec/wrong-identity.rs
@@ -1,7 +1,7 @@
 extern crate prusti_contracts;
 
 #[ensures="result == old(x)"]
-fn identity(x: i32) -> i32 { //~ ERROR Postcondition might not hold
+fn identity(x: i32) -> i32 { //~ ERROR postcondition might not hold
     x + 1
 }
 
