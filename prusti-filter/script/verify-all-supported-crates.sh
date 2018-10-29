b68829273ca4c682b2a905efc46aa78b84963779
Federico Poli <federpoli@gmail.com>
2018-10-29 11:40:15 +0100

    Add nice final summary of summaries

--- prusti-filter/script/verify-all-supported-crates.sh
+++ prusti-filter/script/verify-all-supported-crates.sh
@@ -38,3 +38,26 @@ for crate in "$CRATE_DOWNLOAD_DIR"/*/; do
 		echo "Summary for crate '$crate_name': exit status $exit_status, $whitelist_items items, $duration seconds ($(date '+%Y-%m-%d %H:%M:%S'))"
 	) | tee "$log_file" || true
 done
+
+# Print nice summary of summaries
+
+summaries="$(grep -h "Summary" "$CRATE_DOWNLOAD_DIR"/*/verify-all-supported-crates.log)"
+prusti_ok="$(echo "$summaries" | grep "exit status 0")"
+prusti_error="$(echo "$summaries" | grep -v "exit status 42" | grep -v "exit status 0")"
+compilation_error="$(echo "$summaries" | grep "exit status 42")"
+
+summaries_num="$(echo "$summaries" | wc -l)"
+prusti_ok_num="$(echo "$prusti_ok" | wc -l)"
+prusti_error_num="$(echo "$prusti_error" | wc -l)"
+compilation_error_num="$(echo "$compilation_error" | wc -l)"
+
+# Note: these are only items in whitelists, not *verified* items
+summaries_items="$(echo "$(echo "$summaries" | cut -d ' ' -f 8 | tr "\n" '+')" 0 | bc)"
+prusti_ok_items="$(echo "$(echo "$prusti_ok" | cut -d ' ' -f 8 | tr "\n" '+')" 0 | bc)"
+prusti_error_items="$(echo "$(echo "$prusti_error" | cut -d ' ' -f 8 | tr "\n" '+')" 0 | bc)"
+compilation_error_items="$(echo "$(echo "$compilation_error" | cut -d ' ' -f 8 | tr "\n" '+')" 0 | bc)"
+
+echo "Collected $summaries_num summaries, for a total of $summaries_items items (in whitelists)"
+echo " - Prusti ok: $prusti_ok_num crates, $prusti_ok_items items (in whitelists)"
+echo " - Prusti error: $prusti_error_num crates, $prusti_error_items items (in whitelists)"
+echo " - Invalid: $compilation_error_num crates, $compilation_error_items items (in whitelists)"
