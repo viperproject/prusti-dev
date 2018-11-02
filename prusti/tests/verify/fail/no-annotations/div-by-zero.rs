a29fa130523de039c7b7ef4521d20fa31cc1c351
Federico Poli <federpoli@gmail.com>
2018-11-02 16:53:34 +0100

    Force compilation in debug mode

---
+++ prusti/tests/verify/fail/no-annotations/div-by-zero.rs
@@ -0,0 +1,14 @@
+/// Source: https://doc.servo.org/src/fixedbitset/lib.rs.html#18-21
+
+extern crate prusti_contracts;
+
+#[inline]
+fn div_rem(x: usize, d: usize) -> (usize, usize)
+{
+    (
+        x / d,  //~ ERROR
+        x % d
+    )
+}
+
+fn main(){}
