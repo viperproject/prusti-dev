2141276c6bd6cd3b77d4dbb73e5077d5e9e619ec
Federico Poli <federpoli@gmail.com>
2018-11-03 13:12:36 +0100

    Fix evaluation script

--- prusti-filter/script/evaluate-crate.sh
+++ prusti-filter/script/evaluate-crate.sh
@@ -39,7 +39,7 @@ SECONDS=0
 exit_status="$?"
 end_date="$(date '+%Y-%m-%d %H:%M:%S')"
 duration="$SECONDS"
-whitelist_items="$(grep '"' "$crate_source_dir/Prusti.toml" | wc -l || echo "0")"
+whitelist_items="$(grep '"' "$crate_source_dir/Prusti.toml" 2> /dev/null | wc -l)"
 verified_items="$(egrep 'Received [0-9]+ items to be verified' "$log_file" | tail -n 1 | cut -d ' ' -f 6 | sed 's/^$/0/')"
 
 (
