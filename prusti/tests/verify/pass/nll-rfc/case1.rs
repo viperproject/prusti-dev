c35184496689b5ed1d4b4e5078041e0eb950d720
Vytautas Astrauskas <vastrauskas@gmail.com>
2018-11-11 15:31:23 +0100

    Added functional specifications to case1.rs.

--- prusti/tests/verify/pass/nll-rfc/case1.rs
+++ prusti/tests/verify/pass/nll-rfc/case1.rs
@@ -3,7 +3,8 @@
 /// Adapted from
 /// [here](https://github.com/nikomatsakis/nll-rfc/blob/master/0000-nonlexical-lifetimes.md).
 ///
-/// TODO: Add specifications.
+/// Some lower bounds could be omitted if `CHECK_BINARY_OPERATIONS` is
+/// set to `true`.
 
 extern crate prusti_contracts;
 
@@ -37,27 +38,53 @@ impl VecWrapperI32 {
 
     // Encoded as body-less Viper method
     #[trusted]
-    #[requires="0 <= index && index < self.len()"]
+    #[requires="index < self.len()"]
     #[ensures="self.len() == old(self.len())"]
-    #[ensures="self.lookup(old(index)) == old(value)"]
+    #[ensures="self.lookup(index) == value"]
+    #[ensures="forall i: usize :: (0 <= i && i < self.len() && i != index) ==>
+                    self.lookup(i) == old(self.lookup(i))"]
     pub fn store(&mut self, index: usize, value: i32) {
         self.v[index] = value;
     }
 
     #[trusted]
+    #[ensures="self.len() == old(self.len()) + 1"]
+    #[ensures="self.lookup(old(self.len())) == value"]
+    #[ensures="forall i: usize :: (0 <= i && i < old(self.len())) ==>
+                    self.lookup(i) == old(self.lookup(i))"]
     pub fn push(&mut self, value: i32) {
         self.v.push(value);
     }
 }
 
+#[requires="vec.len() >= 0"]    // TODO: This one should not be needed.
+#[ensures="vec.len() == old(vec.len())"]
+#[ensures="forall i: usize :: (0 <= i && i < vec.len()) ==> vec.lookup(i) <= 0"]
+#[ensures=r"forall j: usize :: (0 <= j && j < vec.len() && old(vec.lookup(j)) > 0) ==>
+                -old(vec.lookup(j)) == vec.lookup(j)"]
+#[ensures=r"forall j: usize :: (0 <= j && j < vec.len() && old(vec.lookup(j)) <= 0) ==>
+                old(vec.lookup(j)) == vec.lookup(j)"]
 fn capitalize(vec: &mut VecWrapperI32) {
     let mut i = 0;
     let mut not_finished = i < vec.len();
+    #[invariant="vec.len() == old(vec.len())"]
     #[invariant="0 <= i && i <= vec.len()"]
     #[invariant="not_finished ==> i < vec.len()"]
+    #[invariant="!not_finished ==> i == vec.len()"]
+    #[invariant="forall j: usize :: (0 <= j && j < i) ==> vec.lookup(j) <= 0"]
+    #[invariant=r"forall j: usize :: (i <= j && j < vec.len()) ==>
+                    old(vec.lookup(j)) == vec.lookup(j)"]
+    #[invariant=r"forall j: usize :: (0 <= j && j < i && old(vec.lookup(j)) > 0) ==>
+                    -old(vec.lookup(j)) == vec.lookup(j)"]
+    #[invariant=r"forall j: usize :: (0 <= j && j < i && old(vec.lookup(j)) <= 0) ==>
+                    old(vec.lookup(j)) == vec.lookup(j)"]
     while not_finished {
         let value = vec.lookup(i);
-        vec.store(i, value);
+        if value > 0 {
+            vec.store(i, -value);
+        } else {
+            vec.store(i, value);
+        }
         i += 1;
         not_finished = i < vec.len();
     }
@@ -66,14 +93,27 @@ fn capitalize(vec: &mut VecWrapperI32) {
 fn bar() {
     let mut data = VecWrapperI32::new();
     data.push(1);
-    data.push(2);
+    data.push(-2);
     data.push(3);
     let slice = &mut data;
     capitalize(slice);
+
+    assert!(data.lookup(0) == -1);
+    assert!(data.lookup(1) == -2);
+    assert!(data.lookup(2) == -3);
+
     data.push(4);
-    data.push(5);
+    data.push(-5);
     data.push(6);
+
+    assert!(data.lookup(0) == -1);
+    assert!(data.lookup(1) == -2);
+    assert!(data.lookup(2) == -3);
+    assert!(data.lookup(3) == 4);
+    assert!(data.lookup(4) == -5);
+    assert!(data.lookup(5) == 6);
 }
 
 fn main() {
+    bar();
 }
