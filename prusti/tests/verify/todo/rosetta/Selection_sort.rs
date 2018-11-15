826f4224d8aa217c981ecf32b2b9f6134af4eba0
Vytautas Astrauskas <vastrauskas@gmail.com>
2018-11-15 09:30:58 +0100

    A version of the selection sort.

--- prusti/tests/verify/todo/rosetta/Selection_sort.rs
+++ prusti/tests/verify/todo/rosetta/Selection_sort.rs
@@ -52,14 +52,6 @@ impl VecWrapperI32 {
     }
 }
 
-#[pure]
-#[requires="0 <= i && i <= array.len()"]
-#[requires="forall k1: usize, k2: usize :: (0 <= k1 && k1 < k2 && k2 < i) ==>
-             array.lookup(k1) <= array.lookup(k2)"]
-fn check(array: &mut VecWrapperI32, i: usize) -> bool {
-    true
-}
-
 #[ensures="array.len() == old(array.len())"]
 #[ensures="forall k1: usize, k2: usize :: (0 <= k1 && k1 < k2 && k2 < array.len()) ==>
              array.lookup(k1) <= array.lookup(k2)"]
@@ -80,71 +72,73 @@ fn selection_sort(array: &mut VecWrapperI32) {
     while continue_loop_1 {
         min = i;
 
-        let mut j = i+1;
-        let mut continue_loop_2 = j < array.len();
-        #[invariant="array.len() == old(array.len())"]
-        #[invariant="0 <= i && i < array.len()"]
-        #[invariant="forall k1: usize, k2: usize :: (0 <= k1 && k1 < k2 && k2 < i) ==>
-                     array.lookup(k1) <= array.lookup(k2)"]
-        #[invariant="forall k1: usize, k2: usize :: (0 <= k1 && k1 < i && i <= k2 && k2 < array.len()) ==>
-                     array.lookup(k1) <= array.lookup(k2)"]
+		let mut j = i+1;
+		let mut continue_loop_2 = j < array.len();
+		#[invariant="array.len() == old(array.len())"]
+		#[invariant="0 <= i && i < array.len()"]
+		#[invariant="forall k1: usize, k2: usize :: (0 <= k1 && k1 < k2 && k2 < i) ==>
+					 array.lookup(k1) <= array.lookup(k2)"]
+		#[invariant="forall k1: usize, k2: usize :: (0 <= k1 && k1 < i && i <= k2 && k2 < array.len()) ==>
+					 array.lookup(k1) <= array.lookup(k2)"]
 
-        #[invariant="i < j"]
-        #[invariant="j <= array.len()"]
-        #[invariant="continue_loop_2 ==> j < array.len()"]
-        #[invariant="!continue_loop_2 ==> j == array.len()"]
+		#[invariant="i < j"]
+		#[invariant="j <= array.len()"]
+		#[invariant="continue_loop_2 ==> j < array.len()"]
+		#[invariant="!continue_loop_2 ==> j == array.len()"]
 
-        #[invariant="i <= min"]
-        #[invariant="min < array.len()"]
-        #[invariant="forall k1: usize :: (0 <= k1 && k1 < i) ==>
-                     array.lookup(k1) <= array.lookup(min)"]
-        #[invariant="forall k: usize ::
-                     (i <= k && k < j && k < array.len()) ==>
-                     array.lookup(min) <= array.lookup(k)"]
-        while continue_loop_2 {
-            if array.lookup(j) < array.lookup(min) {
-                min = j;
-            }
-            j += 1;
-            continue_loop_2 = j < array.len();
-        }
+		#[invariant="i <= min"]
+		#[invariant="min < array.len()"]
+		#[invariant="forall k1: usize :: (0 <= k1 && k1 < i) ==>
+					 array.lookup(k1) <= array.lookup(min)"]
+		#[invariant="forall k: usize ::
+					 (i <= k && k < j && k < array.len()) ==>
+					 array.lookup(min) <= array.lookup(k)"]
+		while continue_loop_2 {
+			if array.lookup(j) < array.lookup(min) {
+				min = j;
+			}
+			j += 1;
+			continue_loop_2 = j < array.len();
+		}
 
-        let tmp = array.lookup(i);
-        let min_value = array.lookup(min);
-        array.store(i, min_value);
-        array.store(min, tmp);
+		let tmp = array.lookup(i);
+		let min_value = array.lookup(min);
+		array.store(i, min_value);
+		array.store(min, tmp);
 
-        i += 1;
+		i += 1;
 
-        continue_loop_1 = i < array.len();
+		continue_loop_1 = i < array.len();
     }
 
 }
 
-//#[trusted]
-//fn print_initial_array(array: &mut VecWrapperI32) {
-    //println!("The initial array is {:?}", array.v);
-//}
+#[trusted]
+fn print_initial_array(array: &mut VecWrapperI32) {
+	println!("The initial array is {:?}", array.v);
+}
 
-//#[trusted]
-//fn print_sorted_array(array: &mut VecWrapperI32) {
-    //println!(" The sorted array is {:?}", array.v);
-//}
+#[trusted]
+#[requires="forall k1: usize, k2: usize :: (0 <= k1 && k1 < k2 && k2 < array.len()) ==>
+             array.lookup(k1) <= array.lookup(k2)"]
+fn print_sorted_array(array: &mut VecWrapperI32) {
+	println!(" The sorted array is {:?}", array.v);
+}
  
 fn main() {
-    //let mut array = VecWrapperI32::new();
-    //array.push(9);
-    //array.push(4);
-    //array.push(8);
-    //array.push(3);
-    //array.push(-6);
-    //array.push(2);
-    //array.push(1);
-    //array.push(6);
+	let mut array = VecWrapperI32::new();
+	array.push(9);
+	array.push(4);
+	array.push(8);
+	array.push(3);
+	array.push(-6);
+	array.push(2);
+	array.push(1);
+	array.push(6);
  
-    //print_initial_array(&mut array);
+	print_initial_array(&mut array);
 
-    //selection_sort(&mut array);
+	selection_sort(&mut array);
 
-    //print_sorted_array(&mut array);
+	print_sorted_array(&mut array);
 }
