721ecd9056ec5242a388b1ace5d2b9bd3deaede8
Vytautas Astrauskas <vastrauskas@gmail.com>
2018-11-16 20:58:09 +0100

    Moved Binary search to checked overflow.

--- prusti/tests/verify/pass-overflows/evaluation/Binary_search.rs
+++ prusti/tests/verify/pass-overflows/evaluation/Binary_search.rs
@@ -11,6 +11,7 @@
 //! Verified properties:
 //!
 //! +   Absence of panics.
+//! +   Absence of overflows.
 
 extern crate prusti_contracts;
 
@@ -22,6 +23,7 @@ impl VecWrapperI32 {
     #[trusted]
     #[pure]
     #[ensures="result >= 0"]
+    #[ensures="result < 18446744073709551615"]
     pub fn len(&self) -> usize {
         self.v.len()
     }
@@ -41,6 +43,7 @@ impl VecWrapperI32 {
 
     #[trusted]
     #[requires="0 <= index && index < self.len()"]
+    #[ensures="*result == old(self.lookup(index))"]
     #[ensures="after_expiry(
         self.len() == old(self.len()) &&
         self.lookup(index) == before_expiry(*result) &&
@@ -99,24 +102,46 @@ pub enum Ordering {
 use self::Ordering::*;
 
 // Adapted from https://doc.rust-lang.org/src/core/cmp.rs.html#962-966
+#[ensures="*a == old(*a)"]
+#[ensures="*b == old(*b)"]
+#[ensures="(match result {
+                Equal => *a == *b,
+                Less => *a < *b,
+                Greater => *a > *b,
+            })"]
 fn cmp(a: &mut i32, b: &mut i32) -> Ordering {
     if *a == *b { Equal }
     else if *a < *b { Less }
     else { Greater }
 }
 
+#[requires="forall k1: usize, k2: usize :: (0 <= k1 && k1 < k2 && k2 < arr.len()) ==>
+             arr.lookup(k1) <= arr.lookup(k2)"]
+#[ensures="arr.len() == old(arr.len())"]
+#[ensures="forall k: usize:: (0 <= k && k < arr.len()) ==> arr.lookup(k) == old(arr.lookup(k))"]
+#[ensures="*elem == old(*elem)"]
 fn binary_search(arr: &mut VecWrapperI32, elem: &mut i32) -> UsizeOption
 {
     let mut size = arr.len();
     let mut base = 0;
 
     let mut result = UsizeOption::None;
-    let mut continue_loop = size > 2;
+    let mut continue_loop = size > 0;
 
     #[invariant="0 <= base"]
     #[invariant="0 <= size"]
+    #[invariant="base < 18446744073709551615 - size"]
     #[invariant="base + size <= arr.len()"]
-    #[invariant="continue_loop ==> size > 2"]
+    #[invariant="continue_loop ==> size > 0"]
+    #[invariant="arr.len() == old(arr.len())"]
+    #[invariant="*elem == old(*elem)"]
+    #[invariant="forall k1: usize, k2: usize :: (0 <= k1 && k1 < k2 && k2 < arr.len()) ==>
+                arr.lookup(k1) <= arr.lookup(k2)"]
+    #[invariant="forall k: usize:: (0 <= k && k < arr.len()) ==> arr.lookup(k) == old(arr.lookup(k))"]
+    #[invariant="forall k: usize:: (0 <= k && k < base) ==> arr.lookup(k) < *elem"]
+    // TODO: Finish
+    //#[invariant="continue_loop ==>
+                 //(forall k: usize:: (base + size < k && k < arr.len()) ==> *elem < arr.lookup(k))"]
     while continue_loop {
         size /= 2;
         let mid = base + size;
@@ -128,10 +153,10 @@ fn binary_search(arr: &mut VecWrapperI32, elem: &mut i32) -> UsizeOption
             Greater => base,
             Equal   => {
                 result = UsizeOption::Some(mid);
-                0   // Just return anything because we are finished.
+                base   // Just return anything because we are finished.
             }
         };
-        continue_loop = size > 2 && result.is_none();
+        continue_loop = size > 0 && result.is_none();
     }
 
     result
