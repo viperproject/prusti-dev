b819ba43acdc3f607e2f42d76c9e6a81df13b154
Federico Poli <federpoli@gmail.com>
2018-12-05 14:31:05 +0100

    Fix generation of argument names in jni-gen

--- jni-gen/src/generators/method.rs
+++ jni-gen/src/generators/method.rs
@@ -134,7 +134,7 @@ pub fn generate_method(
                 .into(),
         )?;
 
-        parameter_names.push(format!("arg_{}", java_str_to_string(&parameter_name)?));
+        parameter_names.push(java_str_to_valid_rust_argument_name(&parameter_name)?);
         parameter_signatures.push(java_str_to_string(&parameter_signature)?);
     }
 
