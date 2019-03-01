ad3e147e99cd54e347c1f6c3c461dc8dcead7ad1
Vytautas Astrauskas <vastrauskas@gmail.com>
2019-03-01 14:59:41 +0100

    Added license headers.

--- viper/src/verification_backend.rs
+++ viper/src/verification_backend.rs
@@ -1,3 +1,9 @@
+// © 2019, ETH Zurich
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 use std::fmt;
 
 #[derive(Clone,Copy,Debug,Eq,PartialEq,Hash)]
