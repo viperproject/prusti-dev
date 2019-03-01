b44c3dac64780da97c525134d2b83afdb2899706
Vytautas Astrauskas <vastrauskas@gmail.com>
2019-03-01 13:08:47 +0100

    Added license headers.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# © 2019, ETH Zurich
+#
+# Licensed under the Mozilla Public License Version 2.0 (see LICENSE or
+# http://www.mozilla.org/MPL/2.0/). This file may not be copied,
+# modified, or distributed except according to those terms.
+
 set -eu
 
 #timeout=${PLAYGROUND_TIMEOUT:-60}
