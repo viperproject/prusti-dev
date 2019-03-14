dd3a613e714854d4008c2e1780932183c49753bf
Vytautas Astrauskas <vastrauskas@gmail.com>
2019-03-14 17:45:11 +0100

    Add license headers to prusti-filter.

--- prusti-filter/src/validators/mod.rs
+++ prusti-filter/src/validators/mod.rs
@@ -1,3 +1,9 @@
+// © 2019, ETH Zurich
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 #[macro_use]
 mod support_status;
 mod procedure_validator;
--- prusti-filter/src/validators/unsafety_validator.rs
+++ prusti-filter/src/validators/unsafety_validator.rs
@@ -1,3 +1,9 @@
+// © 2019, ETH Zurich
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 use rustc::ty::TyCtxt;
 use syntax::ast::NodeId;
 use rustc::hir::intravisit::{self, NestedVisitorMap, Visitor};
