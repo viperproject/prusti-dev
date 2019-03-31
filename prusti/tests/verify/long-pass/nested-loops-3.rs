8ba65a39e865dc0b6454b650ba30d8aeb82801e6
Vytautas Astrauskas <vastrauskas@gmail.com>
2019-03-31 16:47:47 +0200

    Remove non-linear assertions from the test.

--- prusti/tests/verify/long-pass/nested-loops-3.rs
+++ prusti/tests/verify/long-pass/nested-loops-3.rs
@@ -1,27 +1,27 @@
 extern crate prusti_contracts;
 
 #[requires="n >= 0"]
-#[ensures="result == old(n) * (1 + old(n) * (1 + old(n)))"]
+//#[ensures="result == old(n) * (1 + old(n) * (1 + old(n)))"]
 fn test(n: i32) -> i32 {
     let mut res = 0;
     let mut ia = 0;
 
     #[invariant="n == old(n)"]
-    #[invariant="res == ia * (1 + n * (1 + n))"]
+    //#[invariant="res == ia * (1 + n * (1 + n))"]
     #[invariant="ia <= n"]
     while ia < n {
         res += 1;
         let mut ib = 0;
 
         #[invariant="n == old(n)"]
-        #[invariant="res == ia * (1 + n * (1 + n)) + 1 + ib * (1 + n)"]
+        //#[invariant="res == ia * (1 + n * (1 + n)) + 1 + ib * (1 + n)"]
         #[invariant="ib <= n"]
         while ib < n {
             res += 1;
             let mut ic = 0;
 
             #[invariant="n == old(n)"]
-            #[invariant="ic <= n && res == ia * (1 + n * (1 + n)) + 1 + ib * (1 + n) + 1 + ic"]
+            //#[invariant="ic <= n && res == ia * (1 + n * (1 + n)) + 1 + ib * (1 + n) + 1 + ic"]
             while ic < n {
                 res += 1;
                 ic += 1;
