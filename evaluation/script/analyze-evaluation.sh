a237459766bc0d438f3f8470b137a78c2fb61f74
Federico Poli <federpoli@gmail.com>
2019-04-02 16:51:36 +0200

    Improve script

--- evaluation/script/analyze-evaluation.sh
+++ evaluation/script/analyze-evaluation.sh
@@ -181,10 +181,20 @@ cat "$CRATE_DOWNLOAD_DIR"/*/source/prusti-filter-results.json | jq '.functions[]
 space
 
 info "Functions that have assertions but not an overflow verification error"
-for f in $(cat "$CRATE_DOWNLOAD_DIR"/fine-grained-verification-report-supported-procedures-with-assertions.csv.csv | grep true | cut -d',' -f2); do echo "$f" ; cat "$CRATE_DOWNLOAD_DIR"/*/source/prusti-filter-results.json | jq ".functions[] | select(.node_path == $f) | .source_code" | sed 's/^"//;s/"$/\n/;s/\\n/\n/g;s/\\"/"/g;s/\\t/\t/g'; done
+for line in $(cat "$CRATE_DOWNLOAD_DIR"/fine-grained-verification-report-supported-procedures-with-assertions.csv.csv | grep true); do
+    crate="$(echo "$line" | cut -d',' -f1)"
+    f="$(echo "$line" | cut -d',' -f2)"
+    echo "$crate, $f";
+    cat "$CRATE_DOWNLOAD_DIR"/"$crate"/source/prusti-filter-results.json | jq ".functions[] | select(.node_path == $f) | .source_code" | sed 's/^"//;s/"$/\n/;s/\\n/\n/g;s/\\"/"/g;s/\\t/\t/g';
+done
 
 info "Functions that have an overflow verification error"
-for f in $(cat "$CRATE_DOWNLOAD_DIR"/fine-grained-verification-report-supported-procedures-with-assertions.csv.csv | grep false | cut -d',' -f2); do echo "$f" ; cat "$CRATE_DOWNLOAD_DIR"/*/source/prusti-filter-results.json | jq ".functions[] | select(.node_path == $f) | .source_code" | sed 's/^"//;s/"$/\n/;s/\\n/\n/g;s/\\"/"/g;s/\\t/\t/g'; done
+for line in $(cat "$CRATE_DOWNLOAD_DIR"/fine-grained-verification-report-supported-procedures-with-assertions.csv.csv | grep false); do
+    crate="$(echo "$line" | cut -d',' -f1)"
+    f="$(echo "$line" | cut -d',' -f2)"
+    echo "$crate, $f";
+    cat "$CRATE_DOWNLOAD_DIR"/"$crate"/source/prusti-filter-results.json | jq ".functions[] | select(.node_path == $f) | .source_code" | sed 's/^"//;s/"$/\n/;s/\\n/\n/g;s/\\"/"/g;s/\\t/\t/g';
+done
 
 inlineinfo "Lines of generated Viper code"
 cat "$CRATE_DOWNLOAD_DIR"/*/source/log/viper_program/*.vpr | grep -v '^$\|^\s*//\|^.$' | wc -l
