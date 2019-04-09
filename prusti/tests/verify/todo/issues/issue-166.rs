60489b7b4379a30660c25eda1db566300bd14729
Federico Poli <federpoli@gmail.com>
2019-04-09 12:01:48 +0200

    Add test for issue #166

---
+++ prusti/tests/verify/todo/issues/issue-166.rs
@@ -0,0 +1,19 @@
+extern crate prusti_contracts;
+
+struct T(i32);
+
+fn foo(x: &T) {}
+fn bar(x: &mut T) {}
+
+fn test (x: T, y: T) {
+    let mut x = x;
+    let mut y = y;
+    let mut r = &x;
+    loop {
+        foo(r);
+        r = &y;
+        bar(&mut x);
+    }
+}
+
+fn main(){}
