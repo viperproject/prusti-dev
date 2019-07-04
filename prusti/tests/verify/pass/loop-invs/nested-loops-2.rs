188d8c5d35ba1afac41c3d722896313da0f54c69
Vytautas Astrauskas <vastrauskas@gmail.com>
2019-07-04 13:41:40 +0200

    Remove unnecessary loop invariants.

--- prusti/tests/verify/pass/loop-invs/nested-loops-2.rs
+++ prusti/tests/verify/pass/loop-invs/nested-loops-2.rs
@@ -6,16 +6,12 @@ fn test(n: i32, k: i32) -> i32 {
     let mut res = 0;
     let mut ia = 0;
 
-    #[invariant="n == old(n)"] // TODO: remove this
-    #[invariant="k == old(k)"] // TODO: remove this
     #[invariant="ia <= n"]
     #[invariant="k >= 0 ==> res >= 0"]
     while ia < n {
         res += 1;
         let mut ib = 0;
 
-        #[invariant="n == old(n)"] // TODO: remove this
-        #[invariant="k == old(k)"] // TODO: remove this
         #[invariant="ib <= n"]
         #[invariant="k >= 0 ==> res >= 0"]
         while ib < n {
