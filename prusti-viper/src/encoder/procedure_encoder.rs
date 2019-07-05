0b19fb22cb40bd48ba1f291cab0d0de7104ae379
Vytautas Astrauskas <vastrauskas@gmail.com>
2019-07-05 12:10:23 +0200

    Change the order in which optimisations are performed.

--- prusti-viper/src/encoder/procedure_encoder.rs
+++ prusti-viper/src/encoder/procedure_encoder.rs
@@ -392,17 +392,19 @@ impl<'p, 'v: 'p, 'r: 'v, 'a: 'r, 'tcx: 'a> ProcedureEncoder<'p, 'v, 'r, 'a, 'tcx
             self.encoder, self.cfg_method, loan_positions, method_pos);
 
         // Fix variable declarations.
-        let mut fixed_method = fix_ghost_vars(method_with_fold_unfold);
-
-        if config::use_assume_false_back_edges() {
-            let havoc_methods = self.encoder.encode_havoc_methods();
-            havoc_assigned_locals(&mut fixed_method, &havoc_methods);
-        }
+        let fixed_method = fix_ghost_vars(method_with_fold_unfold);
 
         // Optimise encoding a bit
         let method_without_unused_vars = remove_unused_vars(fixed_method);
-        let method_without_trivial_assertions =
-            remove_trivial_assertions(method_without_unused_vars);
+        let mut method_without_trivial_assertions = remove_trivial_assertions(
+                method_without_unused_vars);
+
+        if config::use_assume_false_back_edges() {
+            let havoc_methods = self.encoder.encode_havoc_methods();
+            havoc_assigned_locals(&mut method_without_trivial_assertions, &havoc_methods);
+        }
+
+        // Optimise encoding a bit
         let final_method = optimiser::rewrite(method_without_trivial_assertions);
 
         // Dump final CFG
