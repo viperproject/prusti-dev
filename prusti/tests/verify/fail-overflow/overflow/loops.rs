96552f8262a2fe5a4266209880e46b6b9f31f802
Federico Poli <federpoli@gmail.com>
2019-10-01 14:34:37 +0200

    Fix tests

--- prusti/tests/verify/fail-overflow/overflow/loops.rs
+++ prusti/tests/verify/fail-overflow/overflow/loops.rs
@@ -29,8 +29,8 @@ pub fn test2(a: u32, b: u32) -> u32 {
 }
 
 #[requires="a + b <= std::u32::MAX"]
-#[ensures="result == b"]
-pub fn test3(a: u32, b: u32) -> u32 {    //~ ERROR
+#[ensures="result == b"] //~ ERROR
+pub fn test3(a: u32, b: u32) -> u32 {
     let mut c = a;
     let mut d = b;
     #[invariant="c + d == old(a + b)"]
