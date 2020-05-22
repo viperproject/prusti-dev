8bd86a350973537c10e714f6acab6d406df12283
Vytautas Astrauskas <vastrauskas@gmail.com>
2020-05-22 21:30:55 +0200

    Hack around the issue with smallvec.

--- new/prusti/src/bin/prusti-rustc.rs
+++ new/prusti/src/bin/prusti-rustc.rs
@@ -1,93 +1,6 @@
+use prusti_env_utils::{add_to_loader_path, get_latest_crate_artefact, prusti_sysroot};
 use std::env;
-use std::path::{Path, PathBuf};
 use std::process::{Command, Stdio};
-
-/// Find the specified crate and construct the `--extern` compiler argument to
-/// load it.
-fn get_proc_macro_crate(path: &Path, crate_name: &str, file_extension: &str) -> String {
-    let walker = walkdir::WalkDir::new(path).follow_links(true);
-
-    let file_prefix = format!("lib{}-", crate_name);
-
-    let mut candidates = Vec::new();
-    for entry in walker {
-        let entry = match entry {
-            Ok(entry) => entry,
-            Err(_e) => continue,
-        };
-
-        let file_name = entry.file_name().to_str().unwrap_or("");
-        let extension = entry
-            .path()
-            .extension()
-            .and_then(|x| x.to_str())
-            .unwrap_or("");
-
-        if extension == file_extension && file_name.starts_with(&file_prefix) {
-            candidates.push(entry);
-        }
-    }
-    let file_path = candidates
-        .iter()
-        .max_by_key(|entry| entry.metadata().unwrap().modified().unwrap())
-        .map(|entry| entry.path())
-        .unwrap_or_else(|| panic!("failed to find {} in {:?}", crate_name, path));
-
-    format!(
-        "{}={}",
-        crate_name,
-        file_path
-            .as_os_str()
-            .to_str()
-            .expect("the file path contains invalid UTF-8")
-    )
-}
-
-/// Find Prusti's sysroot
-fn prusti_sysroot() -> Option<PathBuf> {
-    Command::new("rustup")
-        .arg("run")
-        .arg(include_str!("../../../rust-toolchain").trim())
-        .arg("rustc")
-        .arg("--print")
-        .arg("sysroot")
-        .output()
-        .ok()
-        .and_then(|out| {
-            print!("{}", String::from_utf8(out.stderr).ok().unwrap());
-            String::from_utf8(out.stdout).ok()
-        })
-        .map(|s| PathBuf::from(s.trim().to_owned()))
-}
-
-/// Prepend paths to an environment variable
-pub fn env_prepend_path(name: &str, value: Vec<PathBuf>, cmd: &mut Command) {
-    let old_value = env::var_os(name);
-    let mut parts: Vec<PathBuf>;
-    if let Some(ref v) = old_value {
-        parts = value;
-        parts.extend(env::split_paths(v).collect::<Vec<_>>());
-    } else {
-        parts = value;
-    }
-    match env::join_paths(parts) {
-        Ok(new_value) => {
-            cmd.env(name, new_value);
-        }
-        Err(err) => panic!("Error: {:?}", err),
-    }
-}
-
-/// Append paths to the loader environment variable
-fn add_to_loader_path(paths: Vec<PathBuf>, cmd: &mut Command) {
-    #[cfg(target_os = "windows")]
-    const LOADER_PATH: &str = "PATH";
-    #[cfg(target_os = "linux")]
-    const LOADER_PATH: &str = "LD_LIBRARY_PATH";
-    #[cfg(target_os = "macos")]
-    const LOADER_PATH: &str = "DYLD_FALLBACK_LIBRARY_PATH";
-    env_prepend_path(LOADER_PATH, paths, cmd);
-}
 
 fn process(mut args: Vec<String>) -> Result<(), i32> {
     let mut prusti_driver_path = std::env::current_exe()
@@ -148,14 +61,14 @@ fn process(mut args: Vec<String>) -> Result<(), i32> {
                 .expect("the Prusti HOME path contains invalid UTF-8")
         ));
         cmd.arg("--extern");
-        cmd.arg(get_proc_macro_crate(
+        cmd.arg(get_latest_crate_artefact(
             &prusti_home,
             "prusti_contracts",
             "rlib",
         ));
     }
     cmd.arg("--extern");
-    cmd.arg(get_proc_macro_crate(
+    cmd.arg(get_latest_crate_artefact(
         &prusti_home,
         "prusti_contracts_internal",
         "so",
