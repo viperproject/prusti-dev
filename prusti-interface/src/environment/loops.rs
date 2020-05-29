5a2efb4a198720278e8f54d8dbd41ab6dbe63d16
Federico Poli <federpoli@gmail.com>
2020-05-29 15:07:34 +0200

    Always use stable sort to better reproduce errors

--- prusti-interface/src/environment/loops.rs
+++ prusti-interface/src/environment/loops.rs
@@ -288,9 +288,7 @@ impl ProcedureLoops {
         let mut ordered_loop_bodies = HashMap::new();
         for (&loop_head, loop_body) in loop_bodies.iter() {
             let mut ordered_body: Vec<_> = loop_body.iter().cloned().collect();
-            ordered_body.sort_unstable_by(
-                |a, b| block_order[a].partial_cmp(&block_order[b]).unwrap()
-            );
+            ordered_body.sort_by_key(|bb| block_order[bb]);
             debug_assert_eq!(loop_head, ordered_body[0]);
             ordered_loop_bodies.insert(loop_head, ordered_body);
         }
@@ -315,7 +313,7 @@ impl ProcedureLoops {
         let mut enclosing_loop_heads = HashMap::new();
         for (&block, loop_heads) in enclosing_loop_heads_set.iter() {
             let mut heads: Vec<BasicBlockIndex> = loop_heads.iter().cloned().collect();
-            heads.sort_unstable_by_key(|bbi| loop_head_depths[bbi]);
+            heads.sort_by_key(|bbi| loop_head_depths[bbi]);
             enclosing_loop_heads.insert(block, heads);
         }
 
