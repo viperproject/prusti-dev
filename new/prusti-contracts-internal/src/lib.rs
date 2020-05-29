2ffa98ab5df3e58c1b021532a1cc1ab8de896d08
Vytautas Astrauskas <vastrauskas@gmail.com>
2020-05-29 21:33:34 +0200

    Expand requires and ensures to tool attributes.

--- new/prusti-contracts-internal/src/lib.rs
+++ new/prusti-contracts-internal/src/lib.rs
@@ -1,15 +1,8 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
-use quote::quote;
-use syn::{parse_macro_input, File};
 
 #[proc_macro_attribute]
-pub fn expand_specs(_attr: TokenStream, tokens: TokenStream) -> TokenStream {
-    let krate = parse_macro_input!(tokens as File);
-    let expanded = quote! {
-        #krate
-    };
-    // Hand the output tokens back to the compiler
-    TokenStream::from(expanded)
+pub fn expand_specs(attr: TokenStream, tokens: TokenStream) -> TokenStream {
+    prusti_specs::expand_specs(attr.into(), tokens.into()).into()
 }
--- new/prusti-specs/src/lib.rs
+++ new/prusti-specs/src/lib.rs
@@ -1,14 +1,33 @@
 use proc_macro2::TokenStream;
 use quote::quote;
+use syn::File;
 
-pub fn requires(_attr: TokenStream, tokens: TokenStream) -> TokenStream {
-    tokens
+pub fn requires(attr: TokenStream, tokens: TokenStream) -> TokenStream {
+    quote! {
+        #[prusti::requires(#attr)]
+        #tokens
+    }
 }
 
-pub fn ensures(_attr: TokenStream, tokens: TokenStream) -> TokenStream {
-    tokens
+pub fn ensures(attr: TokenStream, tokens: TokenStream) -> TokenStream {
+    quote! {
+        #[prusti::ensures(#attr)]
+        #tokens
+    }
 }
 
 pub fn invariant(_tokens: TokenStream) -> TokenStream {
     quote!(if (false) {})
+}
+
+pub fn expand_specs(_attr: TokenStream, tokens: TokenStream) -> TokenStream {
+    let krate: File = match syn::parse2(tokens) {
+        Ok(data) => data,
+        Err(err) => return err.to_compile_error(),
+    };
+    let expanded = quote! {
+        #krate
+    };
+    // Hand the output tokens back to the compiler
+    TokenStream::from(expanded)
 }
