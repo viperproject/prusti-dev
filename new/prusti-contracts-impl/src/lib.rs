9e8c9d97374901a9e6d38bbc7a0e6949231755f7
Vytautas Astrauskas <vastrauskas@gmail.com>
2020-06-16 21:53:25 +0200

    Add a test that checks that prusti-contracts compile to no-op by default.

--- new/prusti-contracts-impl/src/lib.rs
+++ new/prusti-contracts-impl/src/lib.rs
@@ -2,6 +2,7 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use proc_macro_hack::proc_macro_hack;
+use quote::quote;
 
 #[proc_macro_attribute]
 pub fn requires(_attr: TokenStream, tokens: TokenStream) -> TokenStream {
@@ -15,5 +16,5 @@ pub fn ensures(_attr: TokenStream, tokens: TokenStream) -> TokenStream {
 
 #[proc_macro_hack]
 pub fn invariant(_tokens: TokenStream) -> TokenStream {
-    TokenStream::new()
+    (quote! { () }).into()
 }
