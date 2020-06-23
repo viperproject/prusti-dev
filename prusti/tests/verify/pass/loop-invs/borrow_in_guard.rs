9679c3aff62d4dc9ec2faebc376d18bca0223b65
Federico Poli <federpoli@gmail.com>
2020-06-23 10:35:54 +0200

    Ignore test

--- prusti/tests/verify/pass/loop-invs/borrow_in_guard.rs
+++ prusti/tests/verify/pass/loop-invs/borrow_in_guard.rs
@@ -1,6 +1,6 @@
 extern crate prusti_contracts;
 
-// test-ignore: Not yet supported (entered unreachable code: Borrow restores rvalue const true)
+// ignore-test: Not yet supported (entered unreachable code: Borrow restores rvalue const true)
 
 fn test_borrow_in_guard() {
     let mut x: &mut Box<i32>;
