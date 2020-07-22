e1420aa68dd50c1109d8525c3b6ec4144401f4a4
Christoph Matheja <cmatheja@inf.ethz.ch>
2020-07-22 11:50:30 +0200

    test for specs including calls of pure functions returning copy types

---
+++ prusti/tests/verify/pass/equality/pure-chain-1.rs
@@ -0,0 +1,22 @@
+extern crate prusti_contracts;
+
+#[derive(Clone,Copy,PartialEq,Eq)]
+struct A {
+    i: i32,
+}
+
+#[pure]
+fn pos(x: A) -> bool {
+    x.i > 0
+}
+
+#[pure]
+#[requires="pos(x) && pos(y)"]
+#[ensures="pos(result)"]
+fn first(x: A, y: A) -> A {
+    x
+}
+
+fn main() {
+}
+
