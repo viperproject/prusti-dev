6c3eec68adab40b4929aa4cbea6ced1768d439f9
karenhong <karenhong4@gmail.com>
2020-08-07 12:45:04 -0700

    create map resolving external specifications to functions

--- prusti-interface/src/specs/external.rs
+++ prusti-interface/src/specs/external.rs
@@ -1,35 +1,24 @@
 use rustc_hir::intravisit;
-use rustc_hir::def_id::LocalDefId;
+use rustc_hir::def_id::DefId;
 use rustc_middle::hir::map::Map;
 use rustc_middle::ty::TyCtxt;
 use rustc_span::Span;
 
-use std::collections::HashSet;
 use std::collections::HashMap;
-
-type CrateSymbol = rustc_span::symbol::Symbol;
-type FnInfoMap = HashMap<rustc_span::symbol::Symbol, Vec<FnInfo>>;
-
-struct FnInfo {
-    def_id: LocalDefId,
-    path: Vec<i32>
-}
 
 pub struct ExternSpecResolver<'tcx> {
     tcx: TyCtxt<'tcx>,
-    krates: HashMap<CrateSymbol, FnInfoMap>,
+    /// Map of def id of the real function to the fake function containing the specifications
+    extern_fn_map: HashMap<DefId, DefId>,
+    current_def_id: Option<DefId>,
 }
 
 impl<'tcx> ExternSpecResolver<'tcx> {
     pub fn new(tcx: TyCtxt<'tcx>) -> Self {
-        let mut krates: HashMap<CrateSymbol, FnInfoMap> = HashMap::new();
-        for k_num in tcx.all_crate_nums(rustc_hir::def_id::LOCAL_CRATE) {
-            let k_sym = tcx.original_crate_name(*k_num);
-            krates.insert(k_sym, HashMap::new());
-        }
         Self {
             tcx: tcx,
-            krates: krates,
+            extern_fn_map: HashMap::new(),
+            current_def_id: None,
         }
     }
 
@@ -40,28 +29,8 @@ impl<'tcx> ExternSpecResolver<'tcx> {
                          span: Span,
                          id: rustc_hir::hir_id::HirId,) {
 
-        // let node = self.tcx.hir().get(body_id.hir_id);
-        // println!();
-        // println!("{:?}", node);
-        // println!();
+        self.current_def_id = Some(self.tcx.hir().local_def_id(id).to_def_id());
         intravisit::Visitor::visit_fn(self, fn_kind, fn_decl, body_id, span, id);
-
-        let local_id = self.tcx.hir().local_def_id(id);
-        // self.tcx.hir().
-        // let p: syn::Path = syn::parse_str(&path).unwrap();
-        // let krate: CrateSymbol = CrateSymbol::intern(&p.segments[0].ident.to_string());
-        // if self.krates.get(&krate).is_none() {
-        //     return;
-        // }
-
-        // let info = FnInfo {
-        //     def_id: local_id, //.to_def_id()
-        //     path: Vec::new()
-        // };
-
-        // println!("Path: {:?}", self.tcx.hir().def_path(def_id.unwrap()));
-        // println!("path: {:?}", p);
-        // println!("ident: {:?}", p.segments[0].ident);
     }
 }
 
@@ -84,40 +53,12 @@ impl<'tcx> intravisit::Visitor<'tcx> for ExternSpecResolver<'tcx> {
     }
 
     fn visit_expr(&mut self, ex: &'tcx rustc_hir::Expr<'tcx>) {
-        match ex.kind {
-            rustc_hir::ExprKind::Call(ref callee_expr, ref arguments) => {
+        if let rustc_hir::ExprKind::Call(ref callee_expr, ref arguments) = ex.kind {
+            if self.current_def_id.is_some() {
                 let def_id = self.tcx.typeck(callee_expr.hir_id.owner).type_dependent_def_id(callee_expr.hir_id);
-                println!("Callee Def Id: {:?}", def_id);
+                assert!(def_id.is_some());
+                self.extern_fn_map.insert(def_id.unwrap(), self.current_def_id.take().unwrap());
             }
-            rustc_hir::ExprKind::MethodCall(ref segment, _, arguments, _) => {
-
-                let def_id = self.tcx.typeck(ex.hir_id.owner).type_dependent_def_id(ex.hir_id);
-
-                println!("Callee Def Id: {:?}", def_id);
-                println!("Original Crate Name: {:?}", self.tcx.original_crate_name(def_id.unwrap().krate));
-                println!("Typeof: {:?}", self.tcx.type_of(def_id.unwrap()));
-                println!("Kind: {:?}", self.tcx.type_of(def_id.unwrap()).kind);
-                println!("Path str: {:?}", self.tcx.def_path_str(def_id.unwrap()));
-                println!("Path: {:?}", self.tcx.def_path(def_id.unwrap()));
-            }
-            rustc_hir::ExprKind::Path(ref qpath) => {
-                // println!();
-                // println!("{:?}", qpath);
-                match *qpath {
-                    rustc_hir::QPath::Resolved(ref maybe_qself, ref path) => {}
-                    rustc_hir::QPath::TypeRelative(ref qself, ref segment) => {
-
-                        if let rustc_hir::TyKind::Path(ref qpath) = qself.kind {
-                            if let rustc_hir::QPath::Resolved(_, ref path) = qpath {
-                                if let rustc_hir::def::Res::Def(_, ref def_id) = path.res {
-                                    // println!("path: {:?}", def_id);
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            _ => {}
         }
         intravisit::walk_expr(self, ex);
     }
