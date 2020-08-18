f43bcb46c0d58782599ad01f43e2d161df75b4d1
Fabian Wolff <wolfffa@student.ethz.ch>
2020-08-18 22:40:09 +0200

    Move closure!() parsing into separate file

--- prusti-specs/src/lib.rs
+++ prusti-specs/src/lib.rs
@@ -1,13 +1,15 @@
 #![feature(box_syntax)]
 #![feature(box_patterns)]
 
-use proc_macro2::{TokenStream, TokenTree, TokenTree::{Ident, Group, Punct}};
+use proc_macro2::TokenStream;
 use quote::{quote, ToTokens};
 use syn::spanned::Spanned;
 
 use specifications::untyped;
+use parse_closure_macro::ClosureWithSpec;
 
 mod rewriter;
+mod parse_closure_macro;
 pub mod specifications;
 
 macro_rules! handle_result {
@@ -137,87 +139,14 @@ pub fn body_invariant(tokens: TokenStream) -> TokenStream {
 /// the function whether to keep the specification (for -internal) or
 /// drop it (for -impl).
 pub fn closure(tokens: TokenStream, drop_spec: bool) -> TokenStream {
-    let mut requires: Vec<TokenTree> = vec! [];
-    let mut ensures: Vec<TokenTree> = vec! [];
-    let mut cl: Option<syn::ExprClosure> = None;
-
-    let mut iter = tokens.clone ().into_iter();
-
-    while let Some(tok) = iter.next() {
-        match tok {
-            Ident(id) => {
-                let total_span;
-
-                if id.to_string() == "requires" {
-                    match iter.next() {
-                        Some(Group(g)) => {
-                            total_span = id.span().join(g.span()).unwrap_or(g.span());
-                            requires.push(Group(g));
-                        },
-                        _ => {
-                            return syn::Error::new(
-                                id.span(), "\"requires\" expects one parenthesized argument")
-                                .to_compile_error();
-                        }
-                    }
-                } else if id.to_string() == "ensures" {
-                    match iter.next() {
-                        Some(Group(g)) => {
-                            total_span = id.span().join(g.span()).unwrap_or(g.span());
-                            ensures.push(Group(g));
-                        },
-                        _ => {
-                            return syn::Error::new(
-                                id.span(), "\"ensures\" expects one parenthesized argument")
-                                .to_compile_error();
-                        }
-                    }
-                } else {
-                    return syn::Error::new(id.span(), "invalid closure specification")
-                        .to_compile_error();
-                }
-
-                match iter.next() {
-                    Some(Punct(p)) => {
-                        match p.as_char() {
-                            ',' => {},
-                            _ => {
-                                return syn::Error::new(
-                                    p.span(), "expected a comma here")
-                                    .to_compile_error();
-                            }
-                        }
-                    },
-                    _ => {
-                        return syn::Error::new(
-                            total_span, "expected a comma after this")
-                            .to_compile_error()
-                    }
-                }
-            },
-
-            _ => {
-                let mut cl_toks = TokenStream::from(tok);
-                cl_toks.extend(iter);
-
-                match syn::parse2::<syn::ExprClosure>(cl_toks) {
-                    Ok(cl_expr) => {
-                        cl = Some(cl_expr);
-                    },
-                    Err(e) => {
-                        return e.to_compile_error();
-                    }
-                }
-
-                break;
-            }
-        }
+    let cl_spec = syn::parse::<ClosureWithSpec>(tokens.into());
+    if let Err(err) = cl_spec {
+        return err.to_compile_error();
     }
-
-    let cl = cl.expect("closure must be parsed by now");
+    let cl_spec = cl_spec.unwrap();
 
     if drop_spec {
-        cl.into_token_stream()
+        cl_spec.cl.into_token_stream()
     } else {
         let mut rewriter = rewriter::AstRewriter::new();
 
@@ -226,9 +155,9 @@ pub fn closure(tokens: TokenStream, drop_spec: bool) -> TokenStream {
 
         let mut cl_annotations = TokenStream::new();
 
-        for r in requires {
+        for r in cl_spec.pres {
             let spec_id = rewriter.generate_spec_id();
-            let precond = handle_result!(rewriter.parse_assertion(spec_id, r.into()));
+            let precond = handle_result!(rewriter.parse_assertion(spec_id, r.to_token_stream()));
             preconds.push((spec_id, precond));
             let spec_id_str = spec_id.to_string();
             cl_annotations.extend(quote! {
@@ -236,9 +165,9 @@ pub fn closure(tokens: TokenStream, drop_spec: bool) -> TokenStream {
             });
         }
 
-        for e in ensures {
+        for e in cl_spec.posts {
             let spec_id = rewriter.generate_spec_id();
-            let postcond = handle_result!(rewriter.parse_assertion(spec_id, e.into()));
+            let postcond = handle_result!(rewriter.parse_assertion(spec_id, e.to_token_stream()));
             postconds.push((spec_id, postcond));
             let spec_id_str = spec_id.to_string();
             cl_annotations.extend(quote! {
@@ -248,7 +177,7 @@ pub fn closure(tokens: TokenStream, drop_spec: bool) -> TokenStream {
 
         let (spec_toks_pre, spec_toks_post) = rewriter.generate_cl_spec(preconds, postconds);
         let syn::ExprClosure { attrs, asyncness, movability, capture, or1_token,
-                               inputs, or2_token, output, body } = cl;
+                               inputs, or2_token, output, body } = cl_spec.cl;
 
         let mut attrs_ts = TokenStream::new();
         for a in attrs {
