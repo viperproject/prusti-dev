451357e77750910a99dcee00aeb9ac4f19333e69
Lorenz Gorse <lorenz.gorse@gmail.com>
2020-09-08 13:16:30 +0200

    When adding a statement, also pick up on nested labels.
    Adding a statement to a CFG checks if the statement is a label, and adds
    it to a CFG-global list of labels if this is true. However, this misses
    labels when they are nested within other statements (e.g., if state-
    ments). With this change, also nested labels are added to the CFG-global
    list of labels.

---
+++ prusti-common/src/vir/gather_labels.rs
@@ -0,0 +1,19 @@
+use vir::StmtWalker;
+use vir::ast;
+
+pub(super) fn gather_labels(stmt: &ast::Stmt) -> Vec<String> {
+    let mut gather_labels = GatherLabels::default();
+    gather_labels.walk(stmt);
+    gather_labels.labels
+}
+
+#[derive(Default)]
+struct GatherLabels {
+    labels: Vec<String>
+}
+
+impl StmtWalker for GatherLabels {
+    fn walk_label(&mut self, label: &str) {
+        self.labels.push(label.to_string());
+    }
+}
