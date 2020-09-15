8ffbfeeffc2f97511cf0c8c7ddd6a4fdbb56e88b
Federico Poli <federpoli@gmail.com>
2020-09-15 19:01:35 +0200

    Fix no_verify configuration flag

--- prusti-tests/tests_old/parse/pass/attribute_macros.rs
+++ prusti-tests/tests_old/parse/pass/attribute_macros.rs
@@ -3,14 +3,14 @@
 use prusti_contracts::*;
 
 // Extend Prusti to allow writing specifications in functional style
-#[requires("x > 0 && y > 0")]
-#[ensures("result >= x && result >= y")]
-#[ensures("result == x || result == y")]
+#[requires(x > 0 && y > 0)]
+#[ensures(result >= x && result >= y)]
+#[ensures(result == x || result == y)]
 pub fn loop_max(x: u32, y: u32) -> u32 {
     let mut r = x;
-    #[invariant("r >= x")]
-    #[invariant("r == x || r <= y")]
     while r < y {
+        body_invariant!(r >= x);
+        body_invariant!(r == x || r <= y);
         r += 1
     }
     r
