421350dc3bc19addef62a074fe7f582341c5087f
Karuna Grewal <karunagrewal98@gmail.com>
2020-09-28 23:20:28 +0530

    Add tests for ghost types

---
+++ prusti-tests/tests/typecheck/fail/ghost_regular_types.rs
@@ -0,0 +1,16 @@
+use prusti_contracts::*;
+use crate::ghost::GhostInt;
+
+
+fn test_ghost_regular_sum() -> GhostInt {
+    let a = GhostInt;
+    let b = 10;
+    a + b //~ ERROR mismatched types
+}
+
+// ghost code should not interfere with regular code
+fn test_ghost_arguments(arg1: GhostInt, arg2: &i32) {
+    *arg2 = arg1 //~ ERROR mismatched types
+}
+
+fn main() {}
