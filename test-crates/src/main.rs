e363245b08b45b5f98b5a70161e21b3230b1e5a1
Federico Poli <federpoli@gmail.com>
2020-10-05 11:40:02 +0200

    Fix test-crates runner

--- test-crates/src/main.rs
+++ test-crates/src/main.rs
@@ -4,15 +4,21 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::error::Error;
 use std::io::Read;
-use std::fs::File;
-use std::fmt;
+use std::fs;
 
-use rustwide::{cmd::SandboxBuilder, Crate, Toolchain, WorkspaceBuilder};
+use rustwide::{
+    cmd,
+    Crate,
+    Toolchain,
+    WorkspaceBuilder,
+    logging,
+    logging::LogStorage,
+};
 use serde::Deserialize;
-use log::{info, error};
+use log::{info, error, LevelFilter};
 
 #[derive(Debug, Deserialize)]
 struct CrateRecord {
@@ -20,9 +26,9 @@ struct CrateRecord {
     version: String,
 }
 
-impl fmt::Display for CrateRecord {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{} {}", self.name, self.version)
+impl Into<Crate> for CrateRecord {
+    fn into(self) -> Crate {
+        Crate::crates_io(&self.name, &self.version)
     }
 }
 
@@ -35,60 +41,136 @@ fn setup_logs() {
     rustwide::logging::init_with(env.build());
 }
 
+struct CargoPrusti {
+    prusti_home: PathBuf,
+    viper_home: PathBuf,
+    z3_exe: PathBuf,
+}
+
+impl cmd::Runnable for CargoPrusti {
+    fn name(&self) -> cmd::Binary {
+        cmd::Binary::Global(self.prusti_home.join("cargo-prusti"))
+    }
+
+    fn prepare_command<'w, 'pl>(&self, cmd: cmd::Command<'w, 'pl>) -> cmd::Command<'w, 'pl> {
+        cmd.env("VIPER_HOME", self.viper_home.to_str().unwrap())
+            .env("Z3_EXE", self.z3_exe.join("z3").to_str().unwrap())
+            .env("JAVA_HOME", "/usr/lib/jvm/java-8-openjdk-amd64")
+            .env("CARGO_PATH", "/opt/rustwide/cargo-home/bin/cargo")
+    }
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     color_backtrace::install();
     setup_logs();
 
+    let workspace_path = Path::new("../workspaces/test-crates-builder");
+    let host_prusti_home = Path::new("target/release");
+    let host_viper_home = Path::new("viper_tools/backends");
+    let host_z3_home = Path::new("viper_tools/z3/bin");
+    let guest_prusti_home = Path::new("/opt/rustwide/prusti-home");
+    let guest_viper_home = Path::new("/opt/rustwide/viper-home");
+    let guest_z3_home = Path::new("/opt/rustwide/z3-home");
+
+    let cargo_prusti = CargoPrusti {
+        prusti_home: guest_prusti_home.to_path_buf(),
+        viper_home: guest_viper_home.to_path_buf(),
+        z3_exe: guest_z3_home.to_path_buf(),
+    };
+
     info!("Crate a new workspace...");
     let workspace = WorkspaceBuilder::new(
-        Path::new("../workspaces/test-crates-builder"),
+        workspace_path,
         "prusti-test-crates"
     ).init()?;
 
     info!("Install the toolchain...");
     let toolchain = {
         let mut contents = String::new();
-        std::fs::File::open("rust-toolchain")
-            .expect("Failed to open file 'rust-toolchain'.")
-            .read_to_string(&mut contents)
-            .expect("Failed to read file 'rust-toolchain'.");
+        fs::File::open("rust-toolchain")?
+            .read_to_string(&mut contents)?;
         let dist = contents.trim().to_string();
         info!("toolchain: {}", dist);
         Toolchain::dist(&dist)
     };
-    //toolchain.add_component(&workspace, "rustc-dev")?;
     toolchain.install(&workspace)?;
+    toolchain.add_component(&workspace, "rustc-dev")?;
+    toolchain.add_component(&workspace, "llvm-tools-preview")?;
 
-    info!("Read list of crates...");
-    let crate_list_file = File::open("test-crates/crates.csv")?;
-    let mut crate_list_reader = csv::Reader::from_reader(crate_list_file);
-    let crate_list: Vec<CrateRecord> = crate_list_reader.deserialize().collect::<Result<_, _>>()?;
+    info!("Read lists of crates...");
+    let crates_list: Vec<Crate> = csv::Reader::from_reader(
+       fs::File::open("test-crates/crates.csv")?
+    ).deserialize()
+        .collect::<Result<Vec<CrateRecord>, _>>()?
+        .into_iter()
+        .map(|c| c.into())
+        .take(10)
+        .collect();
 
-    info!("Iterate over all {} crates in the list...", crate_list.len());
-    for (index, crate_record) in crate_list.iter().enumerate() {
-        info!("Crate {}/{}: {}", index, crate_list.len(), crate_record);
+    info!("Iterate over all {} crates...", crates_list.len());
+    for (index, krate) in crates_list.iter().enumerate() {
+        info!("Crate {}/{}: {}", index + 1, crates_list.len(), krate);
+
+        let mut build_dir = workspace.build_dir("build");
 
         info!("Fetch crate...");
-        let krate = Crate::crates_io(&crate_record.name, &crate_record.version);
         krate.fetch(&workspace)?;
 
-        info!("Configure sandbox...");
-        let sandbox = SandboxBuilder::new()
-            .memory_limit(Some(1024 * 1024 * 1024))
-            .enable_networking(false);
-
         info!("Build crate...");
-        let mut crate_build_dir = workspace.build_dir(
-            &format!("build_{}_{}", &crate_record.name, &crate_record.version)
-        );
-        let build_result = crate_build_dir.build(&toolchain, &krate, sandbox).run(|build| {
-            build.cargo().args(&["check"]).run()?;
-            Ok(())
-        });
-        if let Err(build_error) = build_result {
-            error!("Error: {}", build_error);
+        {
+            build_dir.purge()?;
+
+            let sandbox = cmd::SandboxBuilder::new()
+                .memory_limit(Some(1024 * 1024 * 1024))
+                .enable_networking(false);
+
+            let mut storage = LogStorage::new(LevelFilter::Info);
+            storage.set_max_size(1024 * 1024);
+
+            let build_status = logging::capture(&storage, || {
+                build_dir.build(&toolchain, &krate, sandbox).run(|build| {
+                    build.cargo().args(&["check"]).run()?;
+                    Ok(())
+                })
+            });
+
+            if let Err(err) = build_status {
+                error!("Build error: {:?}", err);
+                error!("Output:\n{}", storage);
+            }
+        }
+
+        info!("Verify crate...");
+        {
+            build_dir.purge()?;
+
+            let sandbox = cmd::SandboxBuilder::new()
+                .memory_limit(Some(1024 * 1024 * 1024))
+                .enable_networking(false)
+                .mount(&host_prusti_home, &guest_prusti_home, cmd::MountKind::ReadOnly)
+                .mount(&host_viper_home, &guest_viper_home, cmd::MountKind::ReadOnly)
+                .mount(&host_z3_home, &guest_z3_home, cmd::MountKind::ReadOnly);
+
+            let mut storage = LogStorage::new(LevelFilter::Info);
+            storage.set_max_size(1024 * 1024);
+
+            let verification_status = logging::capture(&storage, || {
+                build_dir.build(&toolchain, &krate, sandbox).run(|build| {
+                    build.cmd(&cargo_prusti)
+                        .env("RUST_BACKTRACE", "1")
+                        .run()?;
+                    Ok(())
+                })
+            });
+
+            if let Err(err) = verification_status {
+                error!("Verification error: {:?}", err);
+                error!("Output:\n{}", storage);
+            }
         }
     }
+
+    info!("Done.");
 
     Ok(())
 }
