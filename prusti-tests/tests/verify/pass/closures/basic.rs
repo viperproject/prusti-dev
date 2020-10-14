fde002b41c1daa57cb3bf2bcf611d894e0826718
Aurel Bílý <aurel.bily@gmail.com>
2020-10-14 17:16:46 +0200

    basic closure verification tests

---
+++ prusti-tests/tests/verify/pass/closures/basic.rs
@@ -0,0 +1,29 @@
+#![feature(stmt_expr_attributes)]
+
+use prusti_contracts::*;
+
+#[requires(add |= |a: i32, b: i32| [
+    requires(a >= 0),
+    requires(b >= 0),
+    ensures(result == a + b)
+])]
+#[ensures(result == 16)]
+fn test1<F: Fn (i32, i32) -> i32>(add: F) -> i32 {
+    add(7, 9)
+}
+
+fn main() {
+    let f = closure!(
+        requires(i >= 0),
+        ensures(result == i + 1),
+        |i: i32| -> i32 { i + 1 }
+    );
+    f(0);
+
+    let add = closure!(
+        requires(a >= 0 && b >= 0),
+        ensures(result == a + b),
+        |a: i32, b: i32| -> i32 { a + b }
+    );
+    test1(add);
+}
