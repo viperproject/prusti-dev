15e0db7a8f112c6154cc58ae7f4912238f95f00e
ELowis <lengel@student.ethz.ch>
2020-10-20 23:56:45 +0200

    updated AbstractState interface

--- analysis/src/abstract_state.rs
+++ analysis/src/abstract_state.rs
@@ -11,8 +11,8 @@ use std::vec::Vec;
 pub trait AbstractState<'tcx> {
     //fn make_top(self) -> Self;
     //fn make_bottom(self) -> Self;
-    //fn new_bottom() -> Self;
-    fn new_top() -> Self;
+    fn new_bottom() -> Self;
+    //fn new_top() -> Self;
     fn new_initial(args: &[mir::LocalDecl<'tcx>]) -> Self;
 
     fn widening_threshold() -> u32;
@@ -20,10 +20,10 @@ pub trait AbstractState<'tcx> {
     //fn is_top(&self) -> bool;
     //fn is_bottom(&self) -> bool;
     //fn less_equal(&self, other: &Self) -> bool;
-    fn join(&mut self, other: &Self) -> Self;
+    fn join(&mut self, other: &Self);
     //fn meet(&mut self, other: &Self) -> Self;
-    fn widen(&mut self, previous: &Self) -> Self;
+    fn widen(&mut self, previous: &Self);
 
-    fn apply_statement_effect(&mut self, stmt: &mir::Statement<'tcx>) -> Self;
-    fn apply_terminator_effect(&self, terminator: &mir::terminator::Terminator<'tcx>) -> Vec<(mir::BasicBlock, Self)>;
+    fn apply_statement_effect(&mut self, location: &mir::Location, stmt: &mir::Statement<'tcx>);
+    fn apply_terminator_effect(&self, location: &mir::Location, terminator: &mir::terminator::Terminator<'tcx>) -> Vec<(mir::BasicBlock, Self)>;
 }
