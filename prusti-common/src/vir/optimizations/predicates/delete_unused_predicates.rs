0cf80c5922fc6c9222e590e9913e8aca1f52ddc0
Till Arnold <throwable42@gmail.com>
2020-10-21 16:23:02 +0200

    Clean up delete unused predicates optimisation

--- prusti-common/src/vir/optimizations/predicates/delete_unused_predicates.rs
+++ prusti-common/src/vir/optimizations/predicates/delete_unused_predicates.rs
@@ -41,24 +41,15 @@ fn get_used_predicates(methods: &[CfgMethod], functions: &[Function]) -> BTreeSe
     collector.used_predicates
 }
 
-fn get_used_predicates_old(methods: &[CfgMethod], functions: &[Function]) -> BTreeSet<String> {
-    let mut used_preds = BTreeSet::new();
-    // DeadBorrowToken$ is a used predicate but it does not appear in VIR becaue it is only created when viper code is created from VIR
-    used_preds.insert("DeadBorrowToken$".to_string());
-    methods.visit(&mut used_preds);
-    functions.visit(&mut used_preds);
-    used_preds
-}
-
 fn get_used_predicates_in_predicates(predicates: &[Predicate]) -> BTreeSet<String> {
     let mut collector = UsedPredicateCollector::new();
 
     for pred in predicates {
         match pred {
-            Predicate::Struct(s) => s
-                .body
-                .iter()
-                .for_each(|e| ExprWalker::walk(&mut collector, e)),
+            Predicate::Struct(StructPredicate { body: Some(e), .. }) => {
+                ExprWalker::walk(&mut collector, e)
+            }
+            Predicate::Struct(_) => { /* ignore */ }
             Predicate::Enum(p) => {
                 ExprWalker::walk(&mut collector, &p.discriminant);
                 ExprWalker::walk(&mut collector, &p.discriminant_bounds);
@@ -76,19 +67,10 @@ fn get_used_predicates_in_predicates(predicates: &[Predicate]) -> BTreeSet<Strin
     collector.used_predicates
 }
 
-fn get_used_predicates_in_predicates_old(predicates: &[Predicate]) -> BTreeSet<String> {
-    let mut predicates_used_in_predicates = BTreeSet::new();
-    predicates.visit(&mut predicates_used_in_predicates);
-    return predicates_used_in_predicates;
-}
-
-fn remove_body_of_predicates_if_never_unfolded(
+fn remove_body_of_predicates_if_possible(
     predicates: &[Predicate],
     predicates_only_used_in_predicates: &BTreeSet<String>,
 ) -> Vec<Predicate> {
-    for predicate in predicates {
-        {}
-    }
     let mut new_predicates = predicates.to_vec();
 
     new_predicates.iter_mut().for_each(|predicate| {
@@ -115,24 +97,14 @@ pub fn delete_unused_predicates(
     let mut has_changed = true;
     let mut new_predicates: Vec<Predicate> = predicates.to_vec();
 
-    let used_preds_new = get_used_predicates(methods, functions);
-    let used_preds_old = get_used_predicates_old(methods, functions);
+    let used_preds = get_used_predicates(methods, functions);
 
-    assert_eq!(used_preds_new, used_preds_old);
-    let used_preds = used_preds_new;
     dbg!(&used_preds);
 
     while has_changed {
         has_changed = false;
-        let predicates_used_in_predicates_old =
-            get_used_predicates_in_predicates_old(&new_predicates);
 
-        let predicates_used_in_predicates_new = get_used_predicates_in_predicates(&new_predicates);
-        assert_eq!(
-            predicates_used_in_predicates_old,
-            predicates_used_in_predicates_new
-        );
-        let predicates_used_in_predicates = predicates_used_in_predicates_new;
+        let predicates_used_in_predicates = get_used_predicates_in_predicates(&new_predicates);
         dbg!(&predicates_used_in_predicates);
         new_predicates = new_predicates
             .into_iter()
@@ -159,7 +131,7 @@ pub fn delete_unused_predicates(
 
     // FIXME: This acctually removes bodies that are needed
     /*new_predicates =
-    remove_body_of_predicates_if_never_unfolded(&new_predicates, &only_used_in_predicates);*/
+    remove_body_of_predicates_if_possible(&new_predicates, &only_used_in_predicates);*/
     return new_predicates;
 }
 
@@ -227,177 +199,5 @@ impl StmtWalker for UsedPredicateCollector {
         _variant: &MaybeEnumVariantIndex,
     ) {
         self.used_predicates.insert(predicate_name.to_string());
-    }
-}
-pub trait UsedPredicates {
-    fn visit(&self, set: &mut BTreeSet<String>);
-}
-
-impl<T> UsedPredicates for &[T]
-where
-    T: UsedPredicates,
-{
-    fn visit(&self, set: &mut BTreeSet<String>) {
-        for e in *self {
-            e.visit(set)
-        }
-    }
-}
-
-impl<T> UsedPredicates for Vec<T>
-where
-    T: UsedPredicates,
-{
-    fn visit(&self, set: &mut BTreeSet<String>) {
-        for e in self {
-            e.visit(set)
-        }
-    }
-}
-
-impl<T> UsedPredicates for Option<T>
-where
-    T: UsedPredicates,
-{
-    fn visit(&self, set: &mut BTreeSet<String>) {
-        match self {
-            Some(e) => e.visit(set),
-            None => {}
-        }
-    }
-}
-impl UsedPredicates for CfgMethod {
-    fn visit(&self, set: &mut BTreeSet<String>) {
-        for block in &self.basic_blocks {
-            block.visit(set);
-        }
-    }
-}
-
-impl UsedPredicates for CfgBlock {
-    fn visit(&self, set: &mut BTreeSet<String>) {
-        //FIXME: This is private?
-        //for expr in self.invs {}
-
-        self.stmts.visit(set);
-
-        if let Successor::GotoSwitch(conds, _) = &self.successor {
-            for (e, _) in conds {
-                e.visit(set);
-            }
-        }
-    }
-}
-
-impl UsedPredicates for Expr {
-    fn visit(&self, set: &mut BTreeSet<String>) {
-        match self {
-            Expr::Cond(e1, e2, e3, _) => {
-                e1.visit(set);
-                e2.visit(set);
-                e3.visit(set);
-            }
-            Expr::ForAll(_, triggers, e, _) => {
-                e.visit(set);
-                triggers.visit(set);
-            }
-            Expr::FuncApp(_, e, _, _, _) | Expr::DomainFuncApp(_, e, _) => {
-                e.visit(set);
-            }
-            Expr::PredicateAccessPredicate(name, expr, _, _) => {
-                expr.visit(set);
-                set.insert(name.clone());
-            }
-            Expr::LabelledOld(_, e, _)
-            | Expr::FieldAccessPredicate(e, _, _)
-            | Expr::AddrOf(e, _, _)
-            | Expr::Variant(e, _, _)
-            | Expr::Field(e, _, _)
-            | Expr::UnaryOp(_, e, _) => e.visit(set),
-
-            Expr::Unfolding(name, e1, e2, _, _, _) => {
-                set.insert(name.clone());
-                e1.visit(set);
-                e2.visit(set);
-            }
-
-            Expr::LetExpr(_, e1, e2, _)
-            | Expr::MagicWand(e1, e2, _, _)
-            | Expr::InhaleExhale(e1, e2, _)
-            | Expr::BinOp(_, e1, e2, _) => {
-                e1.visit(set);
-                e2.visit(set);
-            }
-            Expr::Const(_, _) | Expr::Local(_, _) => { /*ignore*/ }
-        }
-    }
-}
-
-impl UsedPredicates for Trigger {
-    fn visit(&self, set: &mut BTreeSet<String>) {
-        self.elements().visit(set);
-    }
-}
-
-impl UsedPredicates for Stmt {
-    fn visit(&self, set: &mut BTreeSet<String>) {
-        match self {
-            Stmt::PackageMagicWand(e, stmts, _, _, _) => {
-                e.visit(set);
-                stmts.visit(set);
-            }
-            Stmt::ExpireBorrows(eb) => {
-                println!("ignoring {:?} because cannot access", eb)
-                /*FIXME*/
-            }
-            Stmt::If(e, s1, s2) => {
-                e.visit(set);
-                s1.visit(set);
-                s2.visit(set);
-            }
-            Stmt::ApplyMagicWand(e, _)
-            | Stmt::Obtain(e, _)
-            | Stmt::Exhale(e, _)
-            | Stmt::Inhale(e, _)
-            | Stmt::Assert(e, _, _) => e.visit(set),
-
-            Stmt::MethodCall(_, e, _) => e.visit(set),
-            Stmt::TransferPerm(e1, e2, _) | Stmt::Assign(e1, e2, _) => {
-                e1.visit(set);
-                e2.visit(set);
-            }
-            Stmt::Fold(name, e, _, _, _) | Stmt::Unfold(name, e, _, _) => {
-                set.insert(name.clone());
-                e.visit(set);
-            }
-
-            Stmt::Label(_) | Stmt::Comment(_) | Stmt::BeginFrame | Stmt::EndFrame => { /* ignore */
-            }
-        }
-    }
-}
-
-impl UsedPredicates for Predicate {
-    fn visit(&self, set: &mut BTreeSet<String>) {
-        match self {
-            Predicate::Struct(s) => s.body.visit(set),
-            Predicate::Enum(p) => {
-                p.discriminant.visit(set);
-                p.discriminant_bounds.visit(set);
-                for (e, _, sp) in &p.variants {
-                    e.visit(set);
-                    sp.body.visit(set);
-                }
-            }
-            Predicate::Bodyless(_, _) => { /* ignore */ }
-        }
-    }
-}
-
-impl UsedPredicates for Function {
-    fn visit(&self, set: &mut BTreeSet<String>) {
-        self.pres.visit(set);
-        self.posts.visit(set);
-        self.body.visit(set);
     }
 }
