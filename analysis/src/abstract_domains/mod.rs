970c618fdf8fc953426490fee78f9b160c501665
ELowis <lengel@student.ethz.ch>
2020-10-21 10:45:16 +0200

    added refactored mod.rs

--- analysis/src/abstract_domains/mod.rs
+++ analysis/src/abstract_domains/mod.rs
@@ -4,10 +4,10 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-mod liveness;
+mod reaching_definitions;
 mod definitely_initialized;
 mod pcs;
 
-pub use liveness::LivenessState;
+pub use reaching_definitions::ReachingDefsState;
 pub use definitely_initialized::DefinitelyInitializedState;
 pub use pcs::PCSState;
