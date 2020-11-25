80a04a6ce93a1e7747f7e43f074c4bf172982d70
Vytautas Astrauskas <vastrauskas@gmail.com>
2020-11-25 19:18:17 +0100

    Fix a missing position error for result keyword.

--- prusti-tests/tests/verify/ui/failing-postcondition.rs
+++ prusti-tests/tests/verify/ui/failing-postcondition.rs
@@ -8,4 +8,12 @@ fn something_true() -> bool {
 #[ensures(something_true() && false)]
 fn client(a: u32) {}
 
+#[pure]
+#[ensures(result)]
+fn test1() -> bool { false }
+
+#[pure]
+#[ensures(x)]
+fn test2(x: bool) -> bool { x }
+
 fn main() {}
