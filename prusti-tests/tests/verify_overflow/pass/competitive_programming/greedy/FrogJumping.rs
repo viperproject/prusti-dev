647074a204cf5533820d1a4a5b3cb8c7aca5f7a9
Ahmed Kamel <ahmed.kamel15431@gmail.com>
2020-12-09 17:35:04 +0100

    Add trustted to the last function

--- prusti-tests/tests/verify_overflow/pass/competitive_programming/greedy/FrogJumping.rs
+++ prusti-tests/tests/verify_overflow/pass/competitive_programming/greedy/FrogJumping.rs
@@ -39,7 +39,7 @@ impl VecWrapperI32 {
         (0 <= i && i < self.len() && i != idx) ==>
         self.lookup(i) == old(self.lookup(i))))]
     pub fn set(&mut self, idx: isize, value: isize) -> () {
-        self.v[idx as usize]= value
+        self.v[idx as usize] = value
     }
 }
 
@@ -67,13 +67,18 @@ fn min(a: isize, b: isize) -> isize {
 #[requires(forall(|i: isize, j: isize| (i >= 0 && i < positions.len() - 1 && j > i && j < positions.len()) ==>
         (last_positions.lookup(i) <= last_positions.lookup(j))))]
 #[requires(last_positions.lookup(positions.len() - 1) == positions.len() - 1)]
-// #[ensures(forall(|i: isize, j:  isize| (i >= idx && i < positions.len() - 1 && j > i && j < positions.len()) ==> solve_rec(positions, last_positions, i) >= solve_rec(positions, last_positions, j)))]
-// #[ensures(forall(|i: isize| (i >= idx && i < positions.len() - 1) ==> solve_rec(positions, last_positions, i) >= solve_rec(positions, last_positions, i + 1)))]
-fn solve_rec(positions: &VecWrapperI32, last_positions: &VecWrapperI32, idx: isize) -> isize{
+#[ensures(forall(|i: isize, j:  isize| (i >= idx && i < positions.len() - 1 && j > i && j < positions.len()) ==> solve_rec(positions, last_positions, i) >= solve_rec(positions, last_positions, j)))]
+#[ensures(forall(|i: isize| (i >= idx && i < positions.len() - 1) ==> solve_rec(positions, last_positions, i) >= solve_rec(positions, last_positions, i + 1)))]
+fn solve_rec(positions: &VecWrapperI32, last_positions: &VecWrapperI32, idx: isize) -> isize {
     if idx == positions.len() - 1 {
         0
     } else {
-        helper(positions, last_positions, idx + 1, last_positions.lookup(idx)) + 1
+        helper(
+            positions,
+            last_positions,
+            idx + 1,
+            last_positions.lookup(idx),
+        ) + 1
     }
 }
 
@@ -87,38 +92,36 @@ fn solve_rec(positions: &VecWrapperI32, last_positions: &VecWrapperI32, idx: isi
 #[requires(last_positions.lookup(positions.len() - 1) == positions.len() - 1)]
 #[requires(forall(|i: isize, j: isize| (i >= 0 && i < positions.len() - 1 && j > i && j < positions.len()) ==>
         (last_positions.lookup(i) <= last_positions.lookup(j))))]
-// #[ensures (result == solve_rec(positions, last_positions, end))]
-// #[ensures (forall(|i: isize| (i >= idx && i <= end) ==> result <= solve_rec(positions,  last_positions, i)))]
-fn helper(positions: &VecWrapperI32, last_positions: &VecWrapperI32, idx: isize, end: isize) -> isize {
+#[ensures (result == solve_rec(positions, last_positions, end))]
+#[ensures (forall(|i: isize| (i >= idx && i <= end) ==> result <= solve_rec(positions,  last_positions, i)))]
+fn helper(
+    positions: &VecWrapperI32,
+    last_positions: &VecWrapperI32,
+    idx: isize,
+    end: isize,
+) -> isize {
     if idx == end {
         solve_rec(positions, last_positions, idx)
     } else {
-        min(solve_rec(positions,  last_positions, idx), helper(positions, last_positions, idx + 1, end))
+        min(
+            solve_rec(positions, last_positions, idx),
+            helper(positions, last_positions, idx + 1, end),
+        )
     }
 }
+
+// Greedy Soulution
 
 #[requires(positions.len() >= 2 && positions.len() <= 100000)]
-#[requires(idx >= 0 && idx < positions.len())]
-#[requires(end >= idx && end < positions.len())]
 #[requires(positions.lookup(0) == 0)]
-#[requires(last_positions.len() == positions.len())]
-#[requires(forall(|i: isize| (i >= 0 && i < positions.len() - 1) ==>  (last_positions.lookup(i) > i && last_positions.lookup(i) <= last_positions.lookup(i + 1) && last_positions.lookup(i) < positions.len())))]
-#[requires(last_positions.lookup(positions.len() - 1) == positions.len() - 1)]
-#[requires(forall(|i: isize, j: isize| (i >= 0 && i < positions.len() - 1 && j > i && j < positions.len()) ==>
-        (last_positions.lookup(i) <= last_positions.lookup(j))))]
-#[ensures (forall(|i: isize| (i >= idx && i <= end) ==> helper(positions, last_positions, idx, end) <= solve_rec(positions,  last_positions, i)))]
-#[ensures (helper(positions, last_positions, idx, end) == solve_rec(positions, last_positions, end))]
-fn lemma(positions: &VecWrapperI32, last_positions: &VecWrapperI32, idx: isize, end: isize) {
-    if idx == end {
-        // solve_rec(positions, last_positions, idx)
-    } else {
-        lemma2(positions,  last_positions, idx);
-        lemma(positions, last_positions, idx + 1, end);
-        assert!(idx < end);
-        assert!(helper(positions, last_positions, idx, end) == min(solve_rec(positions,  last_positions, idx), helper(positions, last_positions, idx + 1, end)))
-    }
+#[requires(forall(|i: isize| (i > 0 && i < positions.len()) ==> (positions.lookup(i) >= positions.lookup(i - 1))))]
+#[requires(forall(|i: isize| (i > 0 && i < positions.len()) ==> (positions.lookup(i) - positions.lookup(i - 1) <= r)))]
+fn frog_jumping_greedy(positions: &VecWrapperI32, r: isize) -> isize {
+    let last_positions = last(positions, r);
+    solve_greedy(positions, &last_positions, 0)
 }
 
+#[pure]
 #[requires(positions.len() >= 2 && positions.len() <= 100000)]
 #[requires(idx >= 0 && idx < positions.len())]
 #[requires(positions.lookup(0) == 0)]
@@ -129,82 +132,38 @@ fn lemma(positions: &VecWrapperI32, last_positions: &VecWrapperI32, idx: isize, 
 #[requires(forall(|i: isize, j: isize| (i >= 0 && i < positions.len() - 1 && j > i && j < positions.len()) ==>
         (last_positions.lookup(i) <= last_positions.lookup(j))))]
 #[requires(last_positions.lookup(positions.len() - 1) == positions.len() - 1)]
-#[ensures(forall(|i: isize, j:  isize| (i >= idx && i < positions.len() - 1 && j > i && j < positions.len()) ==> 
-                solve_rec(positions, last_positions, i) >= solve_rec(positions, last_positions, j)))]
-#[ensures(forall(|i: isize| (i >= idx && i < positions.len() - 1) ==> solve_rec(positions, last_positions, i) >= solve_rec(positions, last_positions, i + 1)))]
-fn lemma2(positions: &VecWrapperI32, last_positions: &VecWrapperI32, idx: isize) {
+#[ensures(result == solve_rec(positions, last_positions, idx))]
+fn solve_greedy(positions: &VecWrapperI32, last_positions: &VecWrapperI32, idx: isize) -> isize {
     if idx == positions.len() - 1 {
-        //0
+        0
     } else {
-        assume_false();
-        // lemma(positions, last_positions, idx + 1, last_positions.lookup(idx));
-        //helper(positions, last_positions, idx + 1, last_positions.lookup(idx)) + 1
+        solve_greedy(positions, last_positions, last_positions.lookup(idx)) + 1
     }
 }
 
 #[trusted]
-#[ensures(false)]
-fn assume_false() {
-
+#[requires(positions.len() >= 2 && positions.len() <= 100000)]
+#[requires(forall(|i: isize| (i > 0 && i < positions.len()) ==> (positions.lookup(i) >= positions.lookup(i - 1))))]
+#[requires(forall(|i: isize| (i > 0 && i < positions.len()) ==> (positions.lookup(i) - positions.lookup(i - 1) <= r)))]
+#[ensures(result.len() == positions.len())]
+#[ensures(forall(|i: isize| (i >= 0 && i < positions.len() - 1) ==>
+        (result.lookup(i) > i && result.lookup(i) <= result.lookup(i + 1) &&
+        result.lookup(i) < positions.len())))]
+#[ensures(forall(|i: isize, j: isize| (i >= 0 && i < positions.len() - 1 && j > i && j < positions.len()) ==>
+        (result.lookup(i) <= result.lookup(j))))]
+#[ensures(result.lookup(positions.len() - 1) == positions.len() - 1)]
+fn last(positions: &VecWrapperI32, r: isize) -> VecWrapperI32 {
+    let len = positions.len();
+    let mut result = VecWrapperI32::new(len);
+    result.set(positions.len() - 1, positions.len() - 1);
+    let mut idx1 = positions.len() - 2;
+    while idx1 >= 0 {
+       let mut idx2 = idx1 + 1;
+        while idx2 < positions.len() && positions.lookup(idx2) - positions.lookup(idx1) <= r {
+            idx2 += 1;
+        }
+        result.set(idx1, idx2 - 1);
+        idx1 -= 1;
+    }
+    result
 }
-
-// Greedy Soulution
-
-// #[requires(positions.len() >= 2 && positions.len() <= 100000)]
-// #[requires(positions.lookup(0) == 0)]
-// #[requires(forall(|i: isize| (i > 0 && i < positions.len()) ==> (positions.lookup(i) - positions.lookup(i - 1) <= r)))]
-// fn frog_jumping_greedy(positions: &VecWrapperI32, r: isize) -> isize {
-//     let last_positions = last(positions, r);
-//     solve_greedy(positions, &last_positions, 0)
-// }
-
-// #[pure]
-// #[requires(positions.len() >= 2 && positions.len() <= 100000)]
-// #[requires(idx >= 0 && idx < positions.len())]
-// #[requires(positions.lookup(0) == 0)]
-// #[requires(last_positions.len() == positions.len())]
-// #[requires(forall(|i: isize| (i >= 0 && i < positions.len() - 1) ==>
-//         (last_positions.lookup(i) > i && last_positions.lookup(i) <= last_positions.lookup(i + 1) &&
-//         last_positions.lookup(i) < positions.len())))]
-// #[requires(last_positions.lookup(positions.len() - 1) == positions.len() - 1)]
-// #[ensures(result == solve_rec(positions, last_positions, idx))]
-// fn solve_greedy(positions: &VecWrapperI32, last_positions: &VecWrapperI32, idx: isize) -> isize{
-//     if idx == positions.len() - 1 {
-//         0
-//     } else {
-//         solve_greedy(positions, last_positions, last_positions.lookup(idx)) + 1
-//     }
-// }
-
-
-// #[requires(positions.len() >= 2 && positions.len() <= 100000)]
-// #[requires(forall(|i: isize| (i > 0 && i < positions.len()) ==> (positions.lookup(i) >= positions.lookup(i - 1))))]
-// #[requires(forall(|i: isize| (i > 0 && i < positions.len()) ==> (positions.lookup(i) - positions.lookup(i - 1) <= r)))]
-// #[ensures(result.len() == positions.len())]
-// // #[ensures(forall(|i: isize| (i >= 0 && i < positions.len() - 1) ==>
-// //         (result.lookup(i) > i && result.lookup(i) <= result.lookup(i + 1) &&
-// //         result.lookup(i) < positions.len())))]
-// // #[ensures(result.lookup(positions.len() - 1) == positions.len() - 1)]
-// fn last(positions: &VecWrapperI32, r: isize) -> VecWrapperI32{
-//     let len = positions.len();
-//     let mut result = VecWrapperI32::new(len);
-//     result.set(positions.len() - 1, positions.len() - 1);
-//     let mut idx1 = positions.len() - 2;
-//     while idx1 >= 0  {
-//         body_invariant!(idx1 >= 0 && idx1 < positions.len() - 1);
-//         body_invariant!(result.len() == positions.len());
-//         body_invariant!(forall(|i: isize| (i > idx1 && i < positions.len() - 1) ==> (result.lookup(i) > i && result.lookup(i) < positions.len())));
-//         // body_invariant!(forall(|i: isize, j: isize| (i > idx1 && i < positions.len() - 1 && j > result.lookup(i) && j < positions.len()) ==> 
-//         //                 positions.lookup(j) - positions.lookup(i) > r));
-//         let mut idx2 = idx1 + 1;
-//         while idx2 < positions.len() && positions.lookup(idx2) - positions.lookup(idx1) <= r {
-//             body_invariant!(idx2 > idx1 && idx2 < positions.len());
-//             body_invariant!(true);
-//             idx2 += 1;
-//         }
-//         assert!(idx2 == positions.len() || positions.lookup(idx2) - positions.lookup(idx1) > r);
-//         result.set(idx1, idx2 - 1);
-//         idx1 -= 1;
-//     }
-//     result
-//}
