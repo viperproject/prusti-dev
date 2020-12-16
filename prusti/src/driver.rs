33b3e901e03a1077e3350d2cdcd2b0c7851d18ff
Federico Poli <federpoli@gmail.com>
2020-12-16 10:35:41 +0100

    Revert "Use CARGO_PRIMARY_PACKAGE to skip dependencies"
    This reverts commit e5a4477e6cbaa735e04b552623abc6010d8d4542.

--- prusti/src/driver.rs
+++ prusti/src/driver.rs
@@ -30,6 +30,7 @@ extern crate prusti_common;
 
 mod callbacks;
 mod verifier;
+mod arg_value;
 
 use log::debug;
 use std::{env, panic, borrow::Cow, path::PathBuf};
@@ -39,6 +40,7 @@ use lazy_static::lazy_static;
 use callbacks::PrustiCompilerCalls;
 use rustc_middle::ty::TyCtxt;
 use prusti_common::config;
+use arg_value::arg_value;
 
 /// Link to report Prusti bugs
 const BUG_REPORT_URL: &str = "https://github.com/viperproject/prusti-dev/issues/new";
@@ -123,11 +125,8 @@ fn main() {
 
     // If the environment asks us to actually be rustc, then do that.
     // If cargo is compiling a dependency, then be rustc.
-    let is_prusti_test = env::var("PRUSTI_TESTS").is_ok();
-    let is_cargo_running = env::var("CARGO_PKG_NAME").is_ok();
-    let in_primary_package = env::var("CARGO_PRIMARY_PACKAGE").is_ok();
     let prusti_be_rustc = config::be_rustc()
-        || (!is_prusti_test && is_cargo_running && !in_primary_package);
+        || arg_value(&rustc_args, "--cap-lints", |val| val == "allow").is_some();
     if prusti_be_rustc {
         rustc_driver::main();
     }
