035dd82d45298ddaa7f8b7c24828e4244c32d079
Till Arnold <throwable42@gmail.com>
2021-02-13 17:48:11 +0100

    Purify some local variables in methods

---
+++ prusti-common/src/vir/optimizations/purification/mod.rs
@@ -0,0 +1,156 @@
+use crate::vir::{ast::*, cfg, cfg::CfgMethod, utils::walk_method, CfgBlock, Type};
+use log::warn;
+use std::collections::{BTreeMap, BTreeSet, HashMap};
+
+pub fn purify_methods(mut methods: Vec<CfgMethod>) -> Vec<CfgMethod> {
+    for method in &mut methods {
+        purify_method(method);
+    }
+
+    methods
+}
+
+fn translate_type(typ: &Type) -> Type {
+    match &typ {
+        Type::TypedRef(name) => match name.as_str() {
+            "i32" | "usize" | "u32" => Type::Int,
+            "bool" => Type::Bool,
+            _ => todo!("{:?}", typ),
+        },
+        _ => todo!(),
+    }
+}
+
+fn purify_method(method: &mut CfgMethod) {
+    let mut collector = Collector::new();
+    for var in &method.local_vars {
+        match &var.typ {
+            &Type::TypedRef(ref t) if t == "bool" => {
+                collector.tp.insert(var.name.clone(), var.typ.clone());
+            }
+            _ => {}
+        };
+    }
+    walk_method(method, &mut collector);
+    warn!("Collector {:?}", collector);
+
+    for var in &mut method.local_vars {
+        if collector.tp.contains_key(&var.name) {
+            var.typ = translate_type(&var.typ);
+        }
+    }
+    let mut p = Purifier::new(collector);
+    //for stmt in method.
+    for block in &mut method.basic_blocks {
+        block.stmts = block
+            .stmts
+            .clone()
+            .into_iter()
+            .map(|stmt| StmtFolder::fold(&mut p, stmt))
+            .collect();
+    }
+}
+#[derive(Debug)]
+struct Collector {
+    tp: HashMap<String, Type>,
+}
+
+impl Collector {
+    fn new() -> Self {
+        Collector { tp: HashMap::new() }
+    }
+}
+
+impl ExprWalker for Collector {}
+
+impl StmtWalker for Collector {
+    fn walk_expr(&mut self, expr: &Expr) {
+        ExprWalker::walk(self, expr);
+    }
+
+    fn walk_local_var(&mut self, local_var: &LocalVar) {}
+}
+
+#[derive(Debug)]
+struct Purifier {
+    targets: BTreeSet<String>,
+}
+
+impl Purifier {
+    fn new(c: Collector) -> Self {
+        let mut targets = BTreeSet::new();
+        for (k, v) in c.tp {
+            targets.insert(k);
+        }
+
+        Purifier { targets }
+    }
+}
+
+impl StmtFolder for Purifier {
+    fn fold_expr(&mut self, expr: Expr) -> Expr {
+        ExprFolder::fold(self, expr)
+    }
+    /*fn fold(&mut self, e: Stmt) -> Stmt {
+        Stmt::Comment(format!("Replace all {:?}", e))
+    }*/
+
+    fn fold_method_call(&mut self, name: String, args: Vec<Expr>, targets: Vec<LocalVar>) -> Stmt {
+        if name.starts_with("builtin$havoc")
+            && targets.len() == 1
+            && self.targets.contains(&targets[0].name)
+        {
+            Stmt::Comment(format!("replaced havoc call for {:?}", targets))
+        } else {
+            Stmt::MethodCall(
+                name,
+                args.into_iter().map(|e| self.fold_expr(e)).collect(),
+                targets,
+            )
+        }
+    }
+}
+
+impl ExprFolder for Purifier {
+    fn fold_field(&mut self, receiver: Box<Expr>, field: Field, pos: Position) -> Expr {
+        let rec = self.fold_boxed(receiver);
+
+        if let Expr::Local(l, lp) = *rec.clone() {
+            if self.targets.contains(&l.name) {
+                warn!(
+                    "Replacng {:?} with {:?}",
+                    Expr::Field(rec.clone(), field, pos),
+                    rec
+                );
+
+                return Expr::Local(LocalVar::new(l.name, translate_type(&l.typ)), lp);
+            }
+        }
+
+        Expr::Field(rec, field, pos)
+    }
+
+    fn fold_field_access_predicate(
+        &mut self,
+        receiver: Box<Expr>,
+        perm_amount: PermAmount,
+        pos: Position,
+    ) -> Expr {
+        warn!("fold_field_access_predicate {:?}", receiver);
+
+        if let Expr::Field(a, _field, _) = *receiver.clone() {
+            if let Expr::Local(l, _) = *a {
+                if self.targets.contains(&l.name) {
+                    warn!(
+                        "fold_field_access_predicate replaced with true {:?} ",
+                        receiver
+                    );
+
+                    return true.into();
+                }
+            }
+        }
+
+        Expr::FieldAccessPredicate(receiver, perm_amount, pos)
+    }
+}
