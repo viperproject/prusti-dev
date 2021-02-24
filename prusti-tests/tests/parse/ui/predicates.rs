5339787a96f41be92499ecb00739238f8b135a5f
Johannes Schilling <dario@deaktualisierung.org>
2021-02-24 15:51:22 +0100

    Collect predicate specifications in SpecCollector
    This also adds a ui-test for the parsing, desugaring and collecting.
    
    Also we have to update a number of ui testcases to include the new
    `predicate_body` field.

---
+++ prusti-tests/tests/parse/ui/predicates.rs
@@ -0,0 +1,14 @@
+// compile-flags: -Pprint_desugared_specs=true -Pprint_typeckd_specs=true -Pno_verify=true -Phide_uuids=true
+// normalize-stdout-test: "[a-z0-9]{32}" -> "$(NUM_UUID)"
+// normalize-stdout-test: "[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}" -> "$(UUID)"
+
+/// Tests for predicate parsing and desugaring
+
+use prusti_contracts::*;
+
+#[predicate]
+fn pred(a: bool) -> bool {
+    forall(|b: bool| a == b)
+}
+
+fn main() {}
