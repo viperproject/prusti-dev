ae15aa645f45ac20098cb7f52f7a0c22c21257d1
Maxwell Yang <migacanada@gmail.com>
2021-06-07 02:51:52 -0700

    clean up legacy

--- vir/defs/legacy/cfg/to_viper.rs
+++ vir/defs/legacy/cfg/to_viper.rs
@@ -1,7 +1,5 @@
-// © 2020, ETH Zurich
+// © 2019, ETH Zurich
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
-pub mod to_string;
