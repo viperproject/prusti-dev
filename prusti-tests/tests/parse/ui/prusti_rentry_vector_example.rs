7b19f656677bd22d551ac88af470dca67ed4b238
Justin Hu <justin.hu@alumni.ubc.ca>
2021-06-10 12:45:35 -0700

    update tests with stdout files also update ignored tests with new proposed syntax

--- prusti-tests/tests/parse/ui/prusti_rentry_vector_example.rs
+++ prusti-tests/tests/parse/ui/prusti_rentry_vector_example.rs
@@ -1,7 +1,7 @@
 use prusti_contracts::*;
 
-// ignore-test: prusti-expr! not yet implemented
-#[ensures(result == prusti_expr!(forall(|i: usize| i < self.len() ==> f(x[i]))))]
+// ignore-test: re-entry not yet implemented
+#[ensures(result == (forall(|i: usize| i < self.len() ==> f(x[i]))))]
 fn all_of(x: &Vec<u32>, f: fn (u32) -> bool) -> bool {
     for i in x {
         if !f(*i) {
