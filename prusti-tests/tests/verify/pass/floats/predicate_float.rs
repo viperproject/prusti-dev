4a1faff0da74c1037b4703444f28eb3741db0a6f
Vytautas Astrauskas <vastrauskas@gmail.com>
2021-06-22 21:35:55 +0200

    Fix snapshot encoding.

--- prusti-tests/tests/verify/pass/floats/predicate_float.rs
+++ prusti-tests/tests/verify/pass/floats/predicate_float.rs
@@ -1,10 +1,10 @@
 extern crate prusti_contracts;
 use prusti_contracts::*;
 
-#[predicate]
+#[pure]
 fn is_zero(x:f64) -> bool { x == 0. }
 
-#[predicate]
+#[pure]
 fn is_finite(x:f64) -> bool {f64::NEG_INFINITY < x && x < f64::INFINITY}
 
 #[requires(is_finite(x))]
