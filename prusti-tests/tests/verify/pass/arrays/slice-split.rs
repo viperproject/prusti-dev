baf5e79428116f64941eaa896477e2f7fbfccd08
Johannes Schilling <github@deaktualisierung.org>
2021-06-24 14:08:36 +0200

    Add slice split_at testcase
    This wraps the built-in slice split_at method, to show we have all the
    tools needed to ad specifications for that now.
    
    We can't properly use #[extern_spec], because that doesn't work with
    `impl [T]` for slices yet.

---
+++ prusti-tests/tests/verify/pass/arrays/slice-split.rs
@@ -0,0 +1,19 @@
+use prusti_contracts::*;
+
+fn main() {
+    let a = [0; 13];
+    let (s0, s1) = split_at(&a, 7);
+
+    assert!(s0.len() == 7);
+    assert!(s1.len() == 6);
+}
+
+#[trusted]
+#[requires(0 <= idx && idx < slice.len())]
+#[ensures(result.0.len() == idx)]
+#[ensures(result.1.len() == slice.len() - idx)]
+#[ensures(forall(|i: usize| (0 <= i && i < idx) ==> slice[i] == result.0[i]))]
+#[ensures(forall(|i: usize, j: usize| (idx <= i && i < slice.len() && 0 <= j && j < result.1.len()) ==> slice[i] == result.1[j]))]
+fn split_at(slice: &[i32], idx: usize) -> (&[i32], &[i32]) {
+    slice.split_at(idx)
+}
