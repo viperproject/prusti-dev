3b766087a261ee5696ec7efbc247c27326302380
Anirudh S <56960682+ani003@users.noreply.github.com>
2021-07-02 18:49:16 +0530

    Modify error message
    Modified error message based on review
    
    Co-authored-by: Aurel <Aurel300@users.noreply.github.com>

--- prusti-tests/tests/verify/fail/unsupported/two_phase_borrow.rs
+++ prusti-tests/tests/verify/fail/unsupported/two_phase_borrow.rs
@@ -1,6 +1,6 @@
 fn foo(a: &mut [i32], l: usize) {}
  
-fn bar(a: &mut [i32]) {  //~ ERROR two phase borrow is not supported
+fn bar(a: &mut [i32]) {  //~ ERROR two-phase borrows are not supported
     foo(a, a.len());
 }
  
