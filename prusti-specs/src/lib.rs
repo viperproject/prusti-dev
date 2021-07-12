9c4c71a24aac67f35045223edb20677adf911d57
LDY1998 <caliudeyu2017@gmail.com>
2021-07-12 15:42:54 -0700

    remove export_all to be in other pr

--- prusti-specs/src/lib.rs
+++ prusti-specs/src/lib.rs
@@ -491,36 +491,6 @@ pub fn refine_trait_spec(_attr: TokenStream, tokens: TokenStream) -> TokenStream
     }
 }
 
-pub fn export_all(_tokens: TokenStream) -> TokenStream {
-    quote!(
-        #[prusti::export_all]
-        #[macro_export]
-        macro_rules! crate_spec {
-            () => {
-                
-            };
-        }
-    )
-    // TODO: Transform the export_all to be an inner attribute 
-    // println!("tokens: {:?} \n\n", tokens.to_string());
-    // let body = tokens.clone().into_iter().skip(2).next().unwrap();
-    // match body {
-    //     proc_macro2::TokenTree::Group(group) => {
-    //         let stream = group.stream();
-    //         println!("stream: {:?} \n\n", &stream.to_string());
-    //         let item: untyped::CrateItem = handle_result!(syn::parse2(stream));
-    //         // println!("item after parsing: {:?} \n\n", item);
-    //         // let tokens = quote!(#item);
-    //         // println!("tokens: {:?} \n\n", &tokens.to_string());
-    //         return tokens;
-    //     },
-    //     _ => println!("what's this: {:?} \n\n", &body)
-    // }
-    // println!("export all tokens: {:?} \n\n", &body);
-    // println!("token_str: {:?} \n\n", tokens.to_string());
-    // tokens
-    // TokenStream::new()
-}
 
 pub fn extern_spec(_attr: TokenStream, tokens:TokenStream) -> TokenStream {
     let item: syn::Item = handle_result!(syn::parse2(tokens));
