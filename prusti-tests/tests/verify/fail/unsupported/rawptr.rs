f26b5cb2d5a5bc2c1dd607429ee7f6390f56d511
Anirudh S <anirudh6626@gmail.com>
2021-07-26 14:02:17 +0530

    Updated raw pointer test

--- prusti-tests/tests/verify/fail/unsupported/rawptr.rs
+++ prusti-tests/tests/verify/fail/unsupported/rawptr.rs
@@ -1,3 +1,7 @@
+fn foo(p: *const i32) {
+    let _ = p.is_null();    //~ ERROR raw pointers are not supported
+}
+
 fn main() {
     let _: *const i32 = std::ptr::null_mut();   //~ ERROR raw pointers are not supported
 }
