viperproject/prusti

update prusti-server dependencies

prusti-server/src/lib.rs
@@ -21,7 +21,7 @@ mod service;
 mod verifier_runner;
 mod verifier_thread;
 
-use futures::Future;
+use futures::executor::block_on;
 use prusti_common::{verification_context::VerifierBuilder, verification_service::*, Stopwatch};
 pub use service::*;
 use std::{
@@ -73,10 +73,8 @@ impl PrustiServer {
             )
         });
 
-        match thread
-            .verify(request.program, request.program_name.clone())
-            .wait()
-        {
+        let task = thread.verify(request.program, request.program_name.clone());
+        match block_on(task) {
             Ok(result) => {
                 // put back the thread for later reuse
                 let mut threads = self.threads.write().unwrap();

prusti-server/src/verifier_thread.rs
@@ -5,7 +5,7 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 use super::VerifierRunner;
-use futures::{sync::oneshot, Canceled, Future};
+use futures::channel::oneshot;
 use prusti_common::{
     verification_context::VerifierBuilder, verification_service::ViperBackendConfig, vir::Program,
 };
@@ -15,7 +15,7 @@ use std::{
 };
 use viper::VerificationResult;
 
-pub type FutVerificationResult = Box<dyn Future<Item = VerificationResult, Error = Canceled>>;
+pub type FutVerificationResult = futures::channel::oneshot::Receiver<VerificationResult>;
 
 struct VerificationRequest {
     pub program: Program,
@@ -78,6 +78,6 @@ impl VerifierThread {
                 sender: tx,
             })
             .unwrap();
-        Box::new(rx)
+        rx
     }
 }

viperproject/prusti-dev

prusti-server/src/lib.rs
@@ -23,7 +23,7 @@ mod service;
 mod verifier_runner;
 mod verifier_thread;
 
-use futures::Future;
+use futures::executor::block_on;
 use prusti_common::{verification_context::VerifierBuilder, verification_service::*, Stopwatch};
 pub use service::*;
 use std::{
@@ -75,10 +75,8 @@ impl PrustiServer {
             )
         });
 
-        match thread
-            .verify(request.programs, request.program_name.clone())
-            .wait()
-        {
+        let task = thread.verify(request.programs, request.program_name.clone());
+        match block_on(task) {
             Ok(result) => {
                 // put back the thread for later reuse
                 let mut threads = self.threads.write().unwrap();

prusti-server/src/verifier_thread.rs
@@ -5,7 +5,7 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 use super::VerifierRunner;
-use futures::{sync::oneshot, Canceled, Future};
+use futures::channel::oneshot;
 use prusti_common::{
     verification_context::VerifierBuilder, verification_service::ViperBackendConfig, vir::Program,
 };
@@ -15,7 +15,7 @@ use std::{
 };
 use viper::ProgramVerificationResult;
 
-pub type FutVerificationResult = Box<dyn Future<Item = ProgramVerificationResult, Error = Canceled>>;
+pub type FutVerificationResult = futures::channel::oneshot::Receiver<ProgramVerificationResult>;
 
 struct VerificationRequest {
     pub programs: Vec<Program>,
@@ -78,6 +78,6 @@ impl VerifierThread {
                 sender: tx,
             })
             .unwrap();
-        Box::new(rx)
+        rx
     }
 }