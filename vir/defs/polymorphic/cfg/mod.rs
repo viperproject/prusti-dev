72ca5dfbb233c701386d6b3fa394fccbe96bf6ef
Maxwell Yang <migacanada@gmail.com>
2021-08-10 14:21:44 -0700

    add remaining legacy functionalities to polymorphic

--- vir/defs/polymorphic/cfg/mod.rs
+++ vir/defs/polymorphic/cfg/mod.rs
@@ -4,8 +4,12 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+pub use self::assigned_vars::*;
 pub use self::display::*;
 pub use self::method::*;
+pub use self::visitor::*;
 
+mod assigned_vars;
 mod display;
 mod method;
+mod visitor;
--- vir/defs/polymorphic/mod.rs
+++ vir/defs/polymorphic/mod.rs
@@ -6,12 +6,18 @@
 
 pub use self::ast::*;
 pub use self::cfg::*;
-pub use self::borrows::*;
-pub use self::program::*;
 pub use self::conversions::*;
+pub use self::borrows::*;
+pub use self::gather_labels::*;
+pub use self::program::*;
+pub use self::to_string::*;
+pub use self::utils::*;
 
 pub mod ast;
 pub mod cfg;
-pub mod borrows;
-pub mod program;
 pub mod conversions;
+pub mod borrows;
+pub mod gather_labels;
+pub mod program;
+pub mod to_string;
+pub mod utils;
