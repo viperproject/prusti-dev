d9e8849d7ab7db08ed96f3559d90acf31eb6cb6a
Anirudh S <anirudh6626@gmail.com>
2021-08-19 18:10:47 +0530

    Shifted is_reference function to utils

--- prusti-viper/src/utils/mod.rs
+++ prusti-viper/src/utils/mod.rs
@@ -39,3 +39,7 @@ pub fn ty_to_string(typ: &ty::TyKind) -> String {
         &ty::TyKind::Error(_) => "error type",
     }).to_string()
 }
+
+pub fn is_reference(base_ty: ty::Ty) -> bool {
+    matches!(base_ty.kind(), ty::TyKind::RawPtr(..) | ty::TyKind::Ref(..))
+}
