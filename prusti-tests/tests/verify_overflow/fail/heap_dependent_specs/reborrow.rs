ba207dea0c0b8a2673aa2a92f0dce5620a6c95ab
Vytautas Astrauskas <vastrauskas@gmail.com>
2021-09-25 19:34:37 +0200

    Try fixing the test.

--- prusti-tests/tests/verify_overflow/fail/heap_dependent_specs/reborrow.rs
+++ prusti-tests/tests/verify_overflow/fail/heap_dependent_specs/reborrow.rs
@@ -10,4 +10,22 @@ fn reborrow(s: &Point) -> &i32 {
     &s.x
 }
 
+#[ensures(*reborrow(&result) == 1)]
+fn test() -> Point {
+    let p = Point {
+        x: 1,
+        y: 2,
+    };
+    p
+}
+
+#[ensures(*reborrow(&result) == 2)] //~ ERROR: postcondition might not hold
+fn test2() -> Point {
+    let p = Point {
+        x: 1,
+        y: 2,
+    };
+    p
+}
+
 fn main() {}
