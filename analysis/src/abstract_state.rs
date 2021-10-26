5c0ab257e09c1dfb57e0eafaf3827e8bbddbba06
Federico Poli <federpoli@gmail.com>
2021-10-26 12:59:57 +0200

    Update copyright

--- analysis/src/abstract_state.rs
+++ analysis/src/abstract_state.rs
@@ -1,4 +1,4 @@
-// © 2020, ETH Zurich
+// © 2021, ETH Zurich
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
