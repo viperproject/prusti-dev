e3254cf94f5035c98d391383c3341a63d458a50a
Federico Poli <federpoli@gmail.com>
2021-10-29 09:29:33 +0200

    Run cargo fmt

--- prusti-tests/tests/verify_partial/fail/issues/issue-416.rs
+++ prusti-tests/tests/verify_partial/fail/issues/issue-416.rs
@@ -7,15 +7,14 @@ pub struct SafeVec<T>(Vec<T>);
 impl<T> SafeVec<T> {
     #[pure]
     // FIXME: The error is tracked in https://github.com/viperproject/prusti-dev/issues/683
-    pub unsafe fn get_unchecked_1(&self, idx: usize) -> &T  //~ ERROR generating fold-unfold Viper statements failed
+    pub unsafe fn get_unchecked_1(&self, idx: usize) -> &T //~ ERROR generating fold-unfold Viper statements failed
     {
         self.0.get_unchecked(idx) //~ ERROR use of impure function
     }
 
     #[pure]
     #[trusted]
-    pub unsafe fn get_unchecked_2(&self, idx: usize) -> &T
-    {
+    pub unsafe fn get_unchecked_2(&self, idx: usize) -> &T {
         self.0.get_unchecked(idx)
     }
 }
