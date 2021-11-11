ed2b1605e61a6b8febdf8e4a44d64cd6f0cf4a67
Jonas <jonas.fiala.cardiff@gmail.com>
2021-11-11 15:57:24 +0100

    Add expected second error to test case

--- prusti-tests/tests/verify_overflow/fail/index-vec.rs
+++ prusti-tests/tests/verify_overflow/fail/index-vec.rs
@@ -4,6 +4,7 @@ use prusti_contracts::*;
 #[pure]
 fn get_third(v: &Vec<u32>) -> u32 {
     v[2] //~ ERROR Using usize as index/range type for &std::vec::Vec<u32> is not currently supported in pure functions
+    //~^ ERROR Non-slice LHS type '&u32' not supported yet
 }
 
 fn main(){}
