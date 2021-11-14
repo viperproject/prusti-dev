5b75e27951202b9899fec77495597672d890f88f
Pointerbender <pointerbender@gmail.com>
2021-11-14 20:52:21 +0100

    added regression test for #734

---
+++ prusti-tests/tests/verify/pass/issues/issue-734-1.rs
@@ -0,0 +1,16 @@
+extern crate prusti_contracts;
+use prusti_contracts::*;
+
+#[pure]
+pub fn foo(inner: &[usize; 4]) -> bool {
+    true
+}
+
+#[pure]
+pub fn bar(inner: &[usize; 4]) -> bool {
+    foo(inner);
+    foo(inner);
+    true
+}
+
+fn main() {}
