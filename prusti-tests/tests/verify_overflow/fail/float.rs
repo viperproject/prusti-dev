1c1da682e57f3c61fee3eff9fbbf4009da1c0412
Mohammad Hossam <60348524+mohammadhossam@users.noreply.github.com>
2021-11-15 14:26:44 +0200

    Modified fail test.

--- prusti-tests/tests/verify_overflow/fail/float.rs
+++ prusti-tests/tests/verify_overflow/fail/float.rs
@@ -2,38 +2,38 @@ extern crate prusti_contracts;
 use prusti_contracts::*;
 
 #[ensures(result < 0.0)]
-fn test_floats32_pos() -> f32 {
+fn test_floats32_pos() -> f32 { //~ ERROR
     return 1.0f32;
 }
 
 #[ensures(result > 0.0)]
-fn test_floats32_neg() -> f32 {
+fn test_floats32_neg() -> f32 { //~ ERROR
     return -1.0f32;
 }
 
-#[ensures(result != 0.0)]
+#[ensures(result != 0.0)] //~ ERROR
 fn test_floats32_zero() -> f32 {
     return 0.0f32;
 }
 
 #[ensures(result < 0.0)]
-fn test_floats64_pos() -> f64 {
+fn test_floats64_pos() -> f64 { //~ ERROR
     return 1.0f64;
 }
 
 #[ensures(result > 0.0)]
-fn test_floats64_neg() -> f64 {
+fn test_floats64_neg() -> f64 { //~ ERROR
     return -1.0f64;
 }
 
-#[ensures(result != 0.0)]
+#[ensures(result != 0.0)] //~ ERROR
 fn test_floats64_zero() -> f64 {
     return 0.0f64;
 }
 
 #[requires(a < 0.0)]
 #[ensures(result < 0.0)]
-fn test_float_with_params(a: f32) -> f32{
+fn test_float_with_params(a: f32) -> f32{ //~ ERROR
     return -a;
 }
 
