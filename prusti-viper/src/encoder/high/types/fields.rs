73cc2d38c9177433a1f6e644138c54912e22c08e
Mohammad Hossam <60348524+mohammadhossam@users.noreply.github.com>
2021-11-15 12:37:22 +0200

    Fixed some issues. Added some basic tests.

--- prusti-viper/src/encoder/high/types/fields.rs
+++ prusti-viper/src/encoder/high/types/fields.rs
@@ -12,8 +12,12 @@ pub(crate) fn create_value_field(ty: vir::Type) -> EncodingResult<vir::FieldDecl
 
         vir::Type::Int(_) => vir::FieldDecl::new("val_int", vir::Type::MInt),
 
-        vir::Type::Float(vir::ty::Float::F32) => vir::FieldDecl::new("val_float32", vir::Type::MFloat32),
-        vir::Type::Float(vir::ty::Float::F64) => vir::FieldDecl::new("val_float64", vir::Type::MFloat64),
+        vir::Type::Float(vir::ty::Float::F32) => {
+            vir::FieldDecl::new("val_float32", vir::Type::MFloat32)
+        }
+        vir::Type::Float(vir::ty::Float::F64) => {
+            vir::FieldDecl::new("val_float64", vir::Type::MFloat64)
+        }
 
         // For composed data structures, we typically use a snapshot rather than a field.
         // To unify how parameters are passed to functions, we treat them like a reference.
@@ -48,7 +52,9 @@ pub(crate) fn create_value_field(ty: vir::Type) -> EncodingResult<vir::FieldDecl
             )));
         }
 
-        vir::Type::MBool | vir::Type::MInt | vir::Type::MFloat32 | vir::Type::MFloat64 => unreachable!(),
+        vir::Type::MBool | vir::Type::MInt | vir::Type::MFloat32 | vir::Type::MFloat64 => {
+            unreachable!()
+        }
     };
     Ok(field_decl)
 }
