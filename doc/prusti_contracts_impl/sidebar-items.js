b6f78cbf88b4754d1bddc6c3cfa7b41af9e23d16
fpoli <fpoli@users.noreply.github.com>
2021-11-23 13:57:20 +0000

    deploy: e9a23ebf4d6b364cd9944e197ea906f57e3aaaea

--- doc/prusti_contracts_impl/sidebar-items.js
+++ doc/prusti_contracts_impl/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"attr":[["after_expiry",""],["after_expiry_if",""],["ensures",""],["extern_spec",""],["pure",""],["refine_trait_spec",""],["requires",""],["trusted",""]],"macro":[["body_invariant",""],["closure",""],["predicate",""]]});
+initSidebarItems({"attr":[["after_expiry",""],["after_expiry_if",""],["ensures",""],["extern_spec",""],["pure",""],["refine_trait_spec",""],["requires",""],["trusted",""]],"macro":[["body_invariant",""],["predicate",""]]});
