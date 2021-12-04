0bcb5a5cf8ea499cb73c8827cce93c9f9a7acaa3
Pointerbender <pointerbender@gmail.com>
2021-12-04 20:50:24 +0100

    fix for #770: the definition collector should perform a direct walk on bodies for functions for which its post-condition(s) depends on the `result`, plus regression tests

---
+++ prusti-tests/tests/verify_overflow/pass/issues/issue-770-3.rs
@@ -0,0 +1,27 @@
+use prusti_contracts::*;
+
+pub struct A {
+    b: isize,
+}
+
+impl A {
+    #[pure]
+    pub const fn invariant(&self) -> bool {
+        self.is_negative() != self.is_non_negative()
+    }
+
+    #[pure]
+    pub const fn is_non_negative(&self) -> bool {
+        self.b >= 0
+    }
+
+    #[pure]
+    pub const fn is_negative(&self) -> bool {
+        self.b < 0
+    }
+
+    #[requires(self.invariant())]
+    pub const fn test(&self) {}
+}
+
+fn main() {}
