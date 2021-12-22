b7094ecda5553d1abe9c441bfd4ccf379f1f15cc
Pointerbender <pointerbender@gmail.com>
2021-12-22 20:47:54 +0100

    added another regression test from #721

---
+++ prusti-tests/tests/verify/pass/issues/issue-721-3.rs
@@ -0,0 +1,10 @@
+use prusti_contracts::*;
+
+#[requires(forall(|i: usize| (0 <= i && i < 16) ==> (array[i] > 100) ))]
+#[requires(array[4] <= usize::MAX)]
+#[ensures(result[0] > 100)]
+pub fn example(array: &[usize; 16]) -> &[usize] {
+    &array[4..8]
+}
+
+fn main() {}
