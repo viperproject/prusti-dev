38fd88ef1c35aed22efc9c28fccc9777e0c07b35
Federico Poli <federpoli@gmail.com>
2022-03-17 15:52:25 +0100

    Disable optimizations when testing for precise spans

--- prusti-tests/tests/verify/ui/pure.rs
+++ prusti-tests/tests/verify/ui/pure.rs
@@ -1,4 +1,4 @@
-// compile-flags: -Pprint_desugared_specs=true -Pprint_typeckd_specs=true -Phide_uuids=true
+// compile-flags: -Pprint_desugared_specs=true -Pprint_typeckd_specs=true -Phide_uuids=true -Poptimizations=none
 // normalize-stdout-test: "[a-z0-9]{32}" -> "$(NUM_UUID)"
 // normalize-stdout-test: "[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}" -> "$(UUID)"
 // normalize-stdout-test: "\[[a-z0-9]{4}\]::" -> "[$(CRATE_ID)]::"
