d462911014c2fcd01df7fc5a6f3f8de8ecd15b2d
Vytautas Astrauskas <vastrauskas@gmail.com>
2022-04-10 18:09:36 +0200

    Improve the sequences test.

--- prusti-tests/tests/verify_overflow/fail/core_proof/mathematical_types/sequences.rs
+++ prusti-tests/tests/verify_overflow/fail/core_proof/mathematical_types/sequences.rs
@@ -11,4 +11,12 @@ fn test1() {
     let s4: Seq<u32> = Seq::concat(s0, s1);
 }
 
+fn test2() {
+    assert!(Seq::single(1) == Seq::single(1));  // Needs support for references.
+}
+
+fn test3() {
+    assert!(Seq::single(1) == Seq::single(2));  //~ERROR
+}
+
 fn main() {}
