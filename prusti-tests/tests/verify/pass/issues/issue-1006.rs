d83ec5dea4b1202f457391976083a003bdd6e03a
Pointerbender <pointerbender@gmail.com>
2022-05-17 15:22:42 +0200

    fix for #1006

---
+++ prusti-tests/tests/verify/pass/issues/issue-1006.rs
@@ -0,0 +1,39 @@
+// ignore-test: new encoder seems to break on this test
+use prusti_contracts::*;
+
+fn main() {}
+
+#[derive(Copy, Clone)]
+pub struct A {
+    inner: usize,
+}
+
+impl A {
+    #[pure]
+    pub const fn get(&self) -> usize {
+        self.inner
+    }
+}
+
+pub struct B {
+    a: A,
+}
+
+impl B {
+    #[pure]
+    #[trusted]
+    pub fn foo(&self, a: usize, b: usize) -> &[u8] {
+        unimplemented!()
+    }
+
+    #[pure]
+    #[ensures(result == self.foo(a.get(), b))]
+    pub fn bar(&self, a: A, b: usize) -> &[u8] {
+        self.foo(a.get(), b)
+    }
+
+    #[pure]
+    pub fn baz(&self) -> &[u8] {
+        self.bar(self.a, 1)
+    }
+}
