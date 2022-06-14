400d7a80b45f2adc71ca142700953f9aa979b867
markusde <markusde@student.ubc.ca>
2022-06-14 15:54:52 -0700

    refactor and explain implemented micro-mir statements with example

--- prusti-pcs/src/pcs.rs
+++ prusti-pcs/src/pcs.rs
@@ -6,47 +6,11 @@
 
 #![allow(dead_code)]
 use rustc_data_structures::stable_set::FxHashSet;
-use rustc_middle::{
-    mir::{Local, Place, PlaceElem},
-    ty::List,
-};
-
-/// Operational MIR
-///     A rewrite of the MIR into a more operational semantics
-///
-/// The key differences from MIR are this:
-///     - MicroMir expands MIR operations into many small steps whose PCS can be
-///         represented simply with pre- and post- conditions.
-///     - MicroMir places are like MIR places, but with an additional projections
-///         for temporary places (which may or may not be used, and are always
-///         only used within a MIR assignment)
-///     - MicroMir repsects the frame rule.
-///
-///     The first MicroMir encoding does not support verification along unwinding
-///     paths, and also does not support borrows.
+use rustc_middle::mir::{Local, Location, Place, PlaceElem};
 
 pub struct MicroMirBlock<'tcx> {
     pub statements: Vec<MicroMirStatement<'tcx>>,
     pub terminator: MicroMirTerminator<'tcx>,
-}
-
-/* The pre- and post- conditions are set up this way so that
-    we can add to them (int the frame rule sense: we add
-    to pre- and post- conditions at the same time and require
-    there be no seperating connective conflicts).
-
-  An invariant of a well-annotated MicroMir program is that
-  each pre- and post- condition is a non-conflicting superset
-  of the minimal PCS for each statement.
-
-  pre- and post- conditions can only be None during construction.
-  A None condition represents "uncomputed", whereas Some({})
-  represents "no condition".
-*/
-pub struct MicroMirStatement<'tcx> {
-    pre: Option<FxHashSet<PCSPermission<'tcx>>>,
-    post: Option<FxHashSet<PCSPermission<'tcx>>>,
-    operator: MicroMirOperator<'tcx>,
 }
 
 pub struct MicroMirTerminator<'tcx> {
@@ -54,74 +18,189 @@ pub struct MicroMirTerminator<'tcx> {
     pre: Option<FxHashSet<PCSPermission<'tcx>>>,
 }
 
+// TODO: Design problem: Do we want temporary places?
+// It is possible to leak permissions under this encoding, if every temporary isn't used up.
+// However, this is pretty easy to check. Any statement-level pre/post conditions should have
+// no temporaries.
+
+/// Temporary: representation of an anonymous linear (not affine!) resource.
+
+#[derive(PartialEq, Eq, Hash, Clone)]
+pub struct TemporaryPlace {
+    id: usize,
+}
+
+pub enum MicroMirStatement<'tcx> {
+    /// No-Op
+    Nop,
+
+    /// Set a Place from a temporary
+    Set(TemporaryPlace, Place<'tcx>, Mutability),
+
+    /// Move a place into a temporary place
+    Move(Place<'tcx>, TemporaryPlace),
+
+    /// Clone a place into a temporary place
+    Duplicate(Place<'tcx>, TemporaryPlace, Mutability),
+
+    /// Assume permission for a constant
+    Constant(TemporaryPlace, rustc_middle::mir::Constant<'tcx>),
+
+    // /// Permissions-level model of which places rust will deallocate at a location
+    // /// TODO: Also used to end the lifetimes of temporary variables?
+    // Kill(Place<'tcx>),
+
+    // /// Computes the length of a place into a temporary
+    // Len(Place<'tcx>, TemporaryPlace),
+
+    // /// Performs a nullary op, into a temporary
+    // NullOp(rustc_middle::mir::NullOp, TemporaryPlace),
+
+    // /// Performs a Unary op onto a temporary
+    // UnaryOp(rustc_middle::mir::UnOp, TemporaryPlace, TemporaryPlace),
+    /// Performs a Binary op onto a temporary
+    /// (bool) flag if this is a checked binary operation
+    BinaryOp(
+        rustc_middle::mir::BinOp,
+        bool,
+        TemporaryPlace,
+        TemporaryPlace,
+        TemporaryPlace,
+    ),
+}
+
 pub enum MicroMirTerminatorKind {
+    // WIP
     Goto,
     // ...
 }
 
 impl MicroMirTerminatorKind {
+    // WIP
     pub fn core_precondition(&self) {
         todo!();
     }
 }
 
-pub enum MicroMirOperator<'tcx> {
-    /* no-op */
-    Nop,
-    Set(MicroMirPlace<'tcx>, MicroMirPlace<'tcx>, Mutability),
-
-    /* Permissions-level model of which places rust will deallocate at a location */
-    /* Also used to end the lifetimes of temporary variables? */
-    Kill(MicroMirPlace<'tcx>),
-
-    /* Clone a place into one of it's temporary projections */
-    Duplicate(MicroMirPlace<'tcx>, MicroMirProjection<'tcx>),
-    // ...
-}
-
-impl<'tcx> MicroMirOperator<'tcx> {
+/// MicroMir permission model
+/// Let u/e/s mean uninit/exclusive/shared respectively.
+///
+/// TODO: I'm not convinced we should use this as a standalone language yet,
+/// however it at the very least gives us a way to compute pre- and post-
+/// conditions for MIR statements.
+///
+/// { e t, u q }    Set(t, q, Mut)              { e q }
+/// { s t, u q }    Set(t, q, Not)              { s q }
+/// { e p }         Move(p, t)                  { u p, e t }
+/// { e p }         Duplicate(p, t, Mut)        { e p, e t }
+/// { s p }         Duplicate(p, t, Not)        { s p, e t }
+/// { ? }           Kill(p)                     { u p }
+/// { }             Constant (t, k)             { e t }
+/// { }             Nop                         { }
+/// { e t1, e t2}   Binop(op, flag, t1, t2, t3) { e t3 }
+///
+/// Open questions:
+///     StorageDead/StorageLive effects?
+///     Where might wands be applied?
+///         Owned vs borrowed values?
+///      
+///
+///
+/// Example translations:
+///
+/// (1)     Assign(_4, Use(Copy(_6.f)))
+///             (where _6.f is a shared reference to a Copy type)
+///     ==> { ? }               Kill(_4)                    { u _4 }
+///         { s _6.f }          Duplicate(_6.f, temp1, mut) { s _6.f, e temp1 }
+///         { u _4, e temp1 }   Set(_4, temp1, Mut)         { e _4 }
+///
+///     ==> Preconditions for the whole statment:
+///             ? (conditions in order to kill _4, computed later)
+///             s _6.f
+///          
+///         Postconditions for the whole statement
+///             e _4
+///             s _6.f
+///     
+///         Translation can be strengthened into the pre/post conditions for the whole statement
+///         in a straightfoward way using the frame rule. Note that the pre/post conditions match
+///         between statements.
+///         
+///             { ?, s _6.f }
+///                 Kill(_4)
+///             { u _4, s _6.f }
+///                 Duplicate(_6.f, temp1, mut)
+///             { u _4, s _6.f, e temp1 }
+///                 Set(_4, temp1, Mut)
+///             { e _4, s _6.f }
+///
+///
+/// (2)     Assign(_2.f, CheckedBinaryOp(Add), Constant(5), Move(_7.g))
+///
+///     ==> { ? }                   Kill(_2.f)                               { u _2.f }
+///         { e _7.g }              Move(_7.g, temp1)                        { u _7.g, e temp1 }
+///         { }                     Constant(5, temp2)                       { e temp2 }
+///         { e temp1, e temp2 }    BinOp (Add, true, temp1, temp2, temp3)   { e temp3 }
+///         { e temp3, u _2.f }     Set (temp3, _2.f, Mut)                   { e _2.f }
+///
+///     ==> Combined translation by framing
+///
+///         { ?, e _7.g }
+///             Kill(_2.f)
+///         { u _2.f, e _7.g }
+///             Move(_7.g, temp1)
+///         { u _2.f, u _7.g, e temp1}
+///             Constant(5, temp2)
+///         { u _2.f, u _7.g, e temp1, e temp2}
+///             BinOp (Add, true, temp1, temp2, temp3)
+///         { u _7.g, u _2.f, e temp3 }
+///             Set (temp3, _2.f, Mut)
+///         { u _7.g, e _2.f }
+///
+impl<'tcx> MicroMirStatement<'tcx> {
+    /// Read off the precondition for the core memory safety proof
     pub fn core_precondition(&self) -> Option<FxHashSet<PCSPermission>> {
         match self {
-            MicroMirOperator::Set(dest, assignee, m) => Some(FxHashSet::from_iter([
-                PCSPermission::Uninit(dest.clone().into()),
-                PCSPermission::new_with_read(*m, assignee.clone().into()),
-            ])),
-            MicroMirOperator::Kill(_) => None,
-            MicroMirOperator::Duplicate(_, _) => todo!(),
-            MicroMirOperator::Nop => Some(FxHashSet::default()),
+            _ => todo!(),
         }
     }
 
     pub fn core_postcondition(&self) -> Option<FxHashSet<PCSPermission>> {
         match self {
-            MicroMirOperator::Set(dest, assignee, m) => Some(FxHashSet::from_iter([
-                PCSPermission::new_with_read(*m, dest.clone().into()),
-                PCSPermission::Uninit(assignee.clone().into()),
-            ])),
-            MicroMirOperator::Kill(p) => Some(FxHashSet::from_iter([PCSPermission::Uninit(
-                p.clone().into(),
-            )])),
-            MicroMirOperator::Duplicate(p, project) => todo!(),
-            MicroMirOperator::Nop => Some(FxHashSet::default()),
+            _ => todo!(),
         }
     }
 }
 
 #[derive(PartialEq, Eq, Hash, Clone)]
-pub enum PCSPermission<'tcx> {
-    Shared(MicroMirPlace<'tcx>),
-    Exclusive(MicroMirPlace<'tcx>),
-    Uninit(MicroMirPlace<'tcx>),
+pub enum PCSPermissionKind {
+    Shared,
+    Exclusive,
+    Uninit,
 }
 
-impl<'tcx> PCSPermission<'tcx> {
-    // New permission with read perms
-    pub fn new_with_read(m: Mutability, p: MicroMirPlace<'tcx>) -> Self {
-        match m {
-            Mutability::Mut => PCSPermission::Exclusive(p),
-            Mutability::Not => PCSPermission::Shared(p),
-        }
+#[derive(PartialEq, Eq, Hash, Clone)]
+pub enum PermissionTarget<'tcx> {
+    Mir(Place<'tcx>),
+    Tmp(TemporaryPlace),
+}
+
+impl<'tcx> From<Place<'tcx>> for PermissionTarget<'tcx> {
+    fn from(p: Place<'tcx>) -> Self {
+        PermissionTarget::Mir(p)
     }
+}
+
+impl<'tcx> From<TemporaryPlace> for PermissionTarget<'tcx> {
+    fn from(t: TemporaryPlace) -> Self {
+        PermissionTarget::Tmp(t)
+    }
+}
+
+#[derive(PartialEq, Eq, Hash, Clone)]
+pub struct PCSPermission<'tcx> {
+    target: PermissionTarget<'tcx>,
+    kind: PCSPermissionKind,
 }
 
 #[derive(PartialEq, Eq, Hash, Clone, Copy)]
@@ -130,31 +209,26 @@ pub enum Mutability {
     Not,
 }
 
-#[derive(Clone, PartialEq, Eq, Hash)]
-pub struct MicroMirPlace<'tcx> {
-    local: Local,
-    projection: MicroMirProjection<'tcx>,
-}
-
-impl<'tcx> From<Place<'tcx>> for MicroMirPlace<'tcx> {
-    fn from(p: Place<'tcx>) -> Self {
-        todo!();
-    }
-}
-
-impl<'tcx> From<Local> for MicroMirPlace<'tcx> {
-    fn from(local: Local) -> Self {
-        Self {
-            local,
-            projection: MicroMirProjection::Mir(rustc_middle::ty::List::empty()),
+impl<'tcx> PCSPermission<'tcx> {
+    pub fn new_initialized(m: Mutability, target: PermissionTarget<'tcx>) -> Self {
+        match m {
+            Mutability::Mut => PCSPermission {
+                target,
+                kind: PCSPermissionKind::Exclusive,
+            },
+            Mutability::Not => PCSPermission {
+                target,
+                kind: PCSPermissionKind::Shared,
+            },
         }
     }
-}
 
-#[derive(Clone, PartialEq, Eq, Hash)]
-pub enum MicroMirProjection<'tcx> {
-    Mir(&'tcx List<PlaceElem<'tcx>>),
-    Temp(usize),
+    pub fn new_uninit(target: PermissionTarget<'tcx>) -> Self {
+        PCSPermission {
+            target,
+            kind: PCSPermissionKind::Uninit,
+        }
+    }
 }
 
 pub fn init_analysis() {}
