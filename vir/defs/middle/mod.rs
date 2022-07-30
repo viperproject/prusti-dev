714ee88f829ede7764e46010ca1bab3c97569d57
Vytautas Astrauskas <vastrauskas@gmail.com>
2022-07-30 16:57:08 +0200

    Continue refactoring.

--- vir/defs/middle/mod.rs
+++ vir/defs/middle/mod.rs
@@ -15,7 +15,7 @@ pub use self::{
         function::FunctionDecl,
         position::Position,
         predicate::Predicate,
-        rvalue::{Operand, OperandKind, Rvalue},
+        rvalue::{self, Operand, OperandKind, Rvalue},
         statement::{self, BlockMarkerCondition, BlockMarkerConditionElement, Statement},
         ty::{self, Type},
         type_decl::{self, DiscriminantRange, DiscriminantValue, TypeDecl},
