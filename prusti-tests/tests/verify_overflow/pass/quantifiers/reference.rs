3d08cefdc5f0de9150b1dca8c38c209312598bd2
Zack Grannan <zgrannan@protonmail.com>
2022-08-05 03:45:24 -0700

    Add a comment

--- prusti-tests/tests/verify_overflow/pass/quantifiers/reference.rs
+++ prusti-tests/tests/verify_overflow/pass/quantifiers/reference.rs
@@ -1,5 +1,6 @@
 use prusti_contracts::*;
 
+// This pre-condition is unsatisfiable, but Prusti should not crash
 #[requires(forall(|t: &u32| (t != faulty_witness)))]
 fn req(faulty_witness: &u32) {
 }
