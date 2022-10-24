bccd8fe4e83849ae4a44109f4bca7275e1eb9443
fpoli <fpoli@users.noreply.github.com>
2022-10-24 12:15:42 +0000

    deploy: 638ebd1d7eb755aeff571d50fda4fd948ddfe4e2

--- doc/vir/sidebar-items.js
+++ doc/vir/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"macro":[["enum_predicate",""]],"mod":[["common",""],["converter",""],["high",""],["legacy",""],["low",""],["middle",""],["polymorphic",""],["typed",""]]};
+window.SIDEBAR_ITEMS = {"macro":[["enum_predicate",""]],"mod":[["common",""],["converter",""],["high","VIR that is as close to MIR as possible."],["legacy","Monomorphized legacy."],["low","Effectively Viper."],["middle","Fold-unfold operations are inferred."],["polymorphic","Polymorphic legacy."],["typed","Reduce the number of types. For example, tuples and structs are unified."]]};
