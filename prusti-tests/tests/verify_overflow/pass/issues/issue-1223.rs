b43f793818ce24085bd42275b2134857716f7734
Aurel Bílý <aurel.bily@gmail.com>
2022-11-01 12:25:46 +0100

    fix #1223

---
+++ prusti-tests/tests/verify_overflow/pass/issues/issue-1223.rs
@@ -0,0 +1,29 @@
+use prusti_contracts::*;
+
+fn main() {
+    let _x: i8 = 0i8 + 0i8;
+    let _x: i8 = 0i8 * 0i8;
+    let _x: i8 = 0i8 - 0i8;
+    let _x: i16 = 0i16 + 0i16;
+    let _x: i16 = 0i16 * 0i16;
+    let _x: i16 = 0i16 - 0i16;
+    let _x: i32 = 0i32 + 0i32;
+    let _x: i32 = 0i32 * 0i32;
+    let _x: i32 = 0i32 - 0i32;
+    let _x: i64 = 0i64 + 0i64;
+    let _x: i64 = 0i64 * 0i64;
+    let _x: i64 = 0i64 - 0i64;
+
+    let _x: u8 = 0u8 + 0u8;
+    let _x: u8 = 0u8 * 0u8;
+    let _x: u8 = 0u8 - 0u8;
+    let _x: u16 = 0u16 + 0u16;
+    let _x: u16 = 0u16 * 0u16;
+    let _x: u16 = 0u16 - 0u16;
+    let _x: u32 = 0u32 + 0u32;
+    let _x: u32 = 0u32 * 0u32;
+    let _x: u32 = 0u32 - 0u32;
+    let _x: u64 = 0u64 + 0u64;
+    let _x: u64 = 0u64 * 0u64;
+    let _x: u64 = 0u64 - 0u64;
+}
