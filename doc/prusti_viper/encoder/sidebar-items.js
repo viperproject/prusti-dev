244f8291338a4880d26d62a5372556da6dd45ae4
Aurel300 <Aurel300@users.noreply.github.com>
2022-11-14 12:01:10 +0000

    deploy: a2e70818ed3cccff2349fa2d4e0b199b0a25e8e4

--- doc/prusti_viper/encoder/sidebar-items.js
+++ doc/prusti_viper/encoder/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":[["SpecFunctionKind",""]],"mod":[["counterexamples",""]],"struct":[["Encoder",""]]};
+window.SIDEBAR_ITEMS = {"mod":[["counterexamples",""]],"struct":[["Encoder",""]]};
