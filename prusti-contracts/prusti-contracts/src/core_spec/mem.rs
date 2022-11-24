b9700a8c64b15d1010308e8b3492c20012987407
Julian Dunskus <julian.dunskus@gmail.com>
2022-11-24 17:10:39 +0100

    specify some known sizes & alignments

---
+++ prusti-contracts/prusti-contracts/src/core_spec/mem.rs
@@ -0,0 +1,62 @@
+use crate::*;
+
+#[extern_spec]
+mod core {
+    mod mem {
+        use crate::*;
+        
+        #[pure]
+        #[ghost_constraint(T: core_spec::mem::KnownSize, [ensures(result == T::size())])]
+        fn size_of<T>() -> usize;
+        
+        #[pure]
+        #[ghost_constraint(T: core_spec::mem::KnownSize, [ensures(result == T::align())])]
+        fn align_of<T>() -> usize;
+    }
+}
+
+pub trait KnownSize {
+    #[pure]
+    fn size() -> usize;
+
+    #[pure]
+    fn align() -> usize;
+}
+
+macro_rules! known_size_spec {
+    ($t:ty, $size:expr, $align:expr) => {
+        #[refine_trait_spec]
+        impl KnownSize for $t {
+            #[pure]
+            #[ensures(result == $size)]
+            fn size() -> usize {
+                $size
+            }
+        
+            #[pure]
+            #[ensures(result == $align)]
+            fn align() -> usize {
+                $align
+            }
+        }
+    };
+}
+
+known_size_spec!(bool, 1, 1);
+
+known_size_spec!(i8, 1, 1);
+known_size_spec!(i16, 2, 2);
+known_size_spec!(i32, 4, 4);
+known_size_spec!(i64, 8, 8);
+known_size_spec!(i128, 16, 16);
+
+known_size_spec!(u8, 1, 1);
+known_size_spec!(u16, 2, 2);
+known_size_spec!(u32, 4, 4);
+known_size_spec!(u64, 8, 8);
+known_size_spec!(u128, 16, 16);
+
+known_size_spec!(f32, 4, 4);
+known_size_spec!(f64, 8, 8);
+
+// usize/isize are not specified exactly because they are platform-dependent and programmers should not depend on their specific values anyway.
--- prusti-contracts/prusti-contracts/src/core_spec/mod.rs
+++ prusti-contracts/prusti-contracts/src/core_spec/mod.rs
@@ -2,6 +2,7 @@ pub mod default;
 pub mod option;
 pub mod result;
 pub mod clone;
+pub mod mem;
 
 // NOTE: specs marked with FUTURE are not fully expressible yet (in a clean way).
 // They are due to be revised later as features are added.
