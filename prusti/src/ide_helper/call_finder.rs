e518ef4d6433daaf5d0d3978065daa750c7b33a0
hegglinc <hegglinc@student.ethz.ch>
2022-12-19 09:49:31 +0100

    Figured out how to resolve MethodCalls, but we are still in trouble..

--- prusti/src/ide_helper/call_finder.rs
+++ prusti/src/ide_helper/call_finder.rs
@@ -7,14 +7,13 @@ use prusti_rustc_interface::middle::{
     ty::TyCtxt,
 };
 use prusti_rustc_interface::hir::{
-    Expr, ExprKind, QPath
+    Expr, ExprKind, QPath,
 };
 use prusti_rustc_interface::span::Span;
 use prusti_interface::environment::{
     Environment,
     EnvQuery,
 };
-
 
 pub struct CallSpanFinder<'tcx> {
     pub env_query: EnvQuery<'tcx>,
@@ -44,31 +43,51 @@ impl<'tcx> Visitor<'tcx> for CallSpanFinder<'tcx> {
         match ex.kind {
             // Find all calls and methodcalls, not sure what the difference is..
             ExprKind::Call(e1, _e2) => {
-                match e1.kind {
-                    ExprKind::Path(QPath::Resolved(_ty, path)) => {
-                        let defid = path.res.opt_def_id();
-                        if defid.is_some() {
-                            let defpath = self.tcx.def_path_debug_str(defid.unwrap());
-                            self.spans.push((defpath, path.span));
-                        }
-                    },
-                    _ => {},
+                println!("found a call: resolving!");
+                if let ExprKind::Path(ref qself) = e1.kind {
+                    let tyck_res = self.tcx.typeck(e1.hir_id.owner.def_id);
+                    let res = tyck_res.qpath_res(qself, e1.hir_id);
+                    if let prusti_rustc_interface::hir::def::Res::Def(_, def_id) = res {
+                        let defpath = self.tcx.def_path_debug_str(def_id);
+                        println!("Call DefPath: {}", defpath);
+                        self.spans.push((defpath, ex.span))
+                    }
                 }
             },
-            ExprKind::MethodCall(path, _e1, _e2, sp) => {
-                let ident = format!("{}", path.ident.as_str());
+            ExprKind::MethodCall(path, e1, _e2, sp) => {
+                let ident = format!("Method Call: {}", path.ident.as_str());
                 // let path: &'tcx PathSegment<'tcx> = p;
                 let maybe_method_def_id = self
                     .tcx
                     .typeck(ex.hir_id.owner.def_id)
                     .type_dependent_def_id(ex.hir_id);
                 if let Some(method_def_id) = maybe_method_def_id {
-                    let is_local = method_def_id.as_local().is_some();
-                    if !is_local {
-                        let defpath = self.tcx.def_path_debug_str(method_def_id);
-                        self.spans.push((defpath, sp));
+                    let _is_local = method_def_id.as_local().is_some();
+                    // if !is_local {
+                    let defpath = self.tcx.def_path_debug_str(method_def_id);
+                    println!("Found MethodCall: {}", defpath.clone());
+                    self.spans.push((defpath, sp));
+                // }
+                    // try something
+                    let owner_def_id = ex.hir_id.owner.def_id;
+                    let owner_def_path = self.tcx.def_path_debug_str(owner_def_id.into());
+                    // what if we give it e1's defid
+                    let tyck_res = self.tcx.typeck(owner_def_id);
+                    let substs = tyck_res.node_substs(ex.hir_id);
+                    let (other_def_id, _subst) = self
+                        .env_query
+                        .resolve_method_call(owner_def_id, method_def_id, substs);
+                    if other_def_id != method_def_id {
+                        let other_def_path = self.tcx.def_path_debug_str(other_def_id);
+                        println!("Resolved method call to different defpath: {}", other_def_path);
+                        self.spans.push((other_def_path, sp));
                     }
-                }
+                } // local methods do not need external specifications. 
+                // should we still allow it?
+                // the interesting thing about methodCalls is that in the case of traits
+                // there are 2 possible things we can annotate. The behavior of the trait 
+                // methods itself, or the methods of this specific implementation
+
             },
             ExprKind::Binary(binop, _e1, _e2) | ExprKind::AssignOp(binop, _e1, _e2) => {
                 let ident = binop.node.as_str();
@@ -82,3 +101,4 @@ impl<'tcx> Visitor<'tcx> for CallSpanFinder<'tcx> {
         }
     }
 }
+
