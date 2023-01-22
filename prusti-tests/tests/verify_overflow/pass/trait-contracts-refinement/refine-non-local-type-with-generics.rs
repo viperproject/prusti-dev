b2078f243b2280ae2b452ef706036844deaebb7b
Julian Dunskus <julian.dunskus@gmail.com>
2023-01-22 12:29:44 +0100

    more spec removal

--- prusti-tests/tests/verify_overflow/pass/trait-contracts-refinement/refine-non-local-type-with-generics.rs
+++ prusti-tests/tests/verify_overflow/pass/trait-contracts-refinement/refine-non-local-type-with-generics.rs
@@ -1,17 +1,5 @@
 use prusti_contracts::*;
 
-#[extern_spec]
-impl<T: PartialEq> std::option::Option<T> {
-    #[pure]
-    #[ensures(matches!(*self, Some(_)) == result)]
-    pub fn is_some(&self) -> bool;
-
-    #[pure]
-    #[ensures(self.is_some() == ! result)]
-    #[ensures(matches!(*self, None) == result)]
-    pub fn is_none(&self) -> bool;
-}
-
 trait OptionPeeker<T> {
     #[pure]
     #[requires(false)]
