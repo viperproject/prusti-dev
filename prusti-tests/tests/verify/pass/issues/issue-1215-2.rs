706512a5499778f19d45684de8b502c94f17afdb
Patrick-6 <93388547+Patrick-6@users.noreply.github.com>
2023-01-26 18:24:53 +0100

    Enable `encode_unsigned_num_constraint` by default (#1281)
    * Addresses https://github.com/viperproject/prusti-dev/issues/1215
    
    * Skip memchr crate
    
    * Add more tests
    
    * Removed unneeded environment variable
    
    * Skip failing crates
    
    * Fix typo
    
    * Skip failing crates
    
    * Temporarily disable fail_fast
    
    * Switch from native-tls to rustls
    
    * Re-enable skipped crates
    
    * Skip failing crates
    
    * Skip failing crates
    
    * disable failing crate
    
    * Re-enable fail_fast
    
    * Print errors with Display
    
    * Disable encode_unsigned_num_constraints
    
    * Fix typo in environment variable
    
    * Remove unnecessairy compile flags

---
+++ prusti-tests/tests/verify/pass/issues/issue-1215-2.rs
@@ -0,0 +1,28 @@
+#![allow(unused_comparisons)]
+use prusti_contracts::*;
+
+fn main() {}
+
+fn _test_u8(x: u8) {
+    prusti_assert!(x >= 0);
+}
+
+fn _test_u16(x: u16) {
+    prusti_assert!(x >= 0);
+}
+
+fn _test_u32(x: u32) {
+    prusti_assert!(x >= 0);
+}
+
+fn _test_u64(x: u64) {
+    prusti_assert!(x >= 0);
+}
+
+fn _test_u128(x: u128) {
+    prusti_assert!(x >= 0);
+}
+
+fn _test_usize(x: usize) {
+    prusti_assert!(x >= 0);
+}
