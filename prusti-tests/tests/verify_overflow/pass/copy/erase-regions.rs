4b58fae9ad340cb89ffdbccb6b2da73d8b25f884
Julian Dunskus <julian.dunskus@gmail.com>
2023-01-26 17:19:48 +0100

    update test to use core spec

--- prusti-tests/tests/verify_overflow/pass/copy/erase-regions.rs
+++ prusti-tests/tests/verify_overflow/pass/copy/erase-regions.rs
@@ -6,12 +6,8 @@ struct Iter<'a, #[generic] T: Copy> {
     position: usize,
 }
 
-type SliceTy<T> = [T];
 #[extern_spec]
-impl<T: Copy+PartialEq> SliceTy<T> {
-    #[pure]
-    fn len(&self) -> usize;
-
+impl<T: Copy + PartialEq> [T] {
     #[ensures( result.model().position == 0 )]
     fn iter(&self) -> std::slice::Iter<'_, T>;
 }
@@ -20,6 +16,4 @@ fn verify_slice_iter(slice: &[i32]) {
     let iter = slice.iter();
 }
 
-fn main() {
-
-}
+fn main() {}
