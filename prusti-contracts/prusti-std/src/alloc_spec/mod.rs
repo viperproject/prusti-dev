d7ce8c77fe527a0efcaa6cbfb384f06f22775934
Julian Dunskus <julian.dunskus@gmail.com>
2023-02-03 03:33:01 +0100

    conditionalize std specs

--- prusti-contracts/prusti-std/src/alloc_spec/mod.rs
+++ prusti-contracts/prusti-std/src/alloc_spec/mod.rs
@@ -1,5 +1,5 @@
-// In future, it might make sense to move these specs to yet another crate or have a feature to disable the std parts of this crate.
-// alloc is usable in some no_std environments, so it would be helpful to support the use case of core + alloc without std.
+// These specs can't be built-in like the core specs since some no_std crates may not even have an allocator.
+// Instead, prusti-std supports no_std through a feature.
 
 pub mod string;
 pub mod vec;
--- prusti-contracts/prusti-std/src/lib.rs
+++ prusti-contracts/prusti-std/src/lib.rs
@@ -1,6 +1,16 @@
 #![feature(allocator_api)] // to specify Vec
+#![no_std] // disable std by default
 
+// link to std if the feature is set
+#[cfg(feature = "std")]
+extern crate std;
+
+// Even alloc can be disabled for consistency with std, and in preparation for future specs for other, possibly no_std, crates.
+#[cfg(feature = "alloc")]
 extern crate alloc;
 
+// modules have a `_spec` suffix to avoid name conflicts with their crates.
+#[cfg(feature = "alloc")]
 pub mod alloc_spec;
-pub mod collections;
+#[cfg(feature = "std")]
+pub mod std_spec;
