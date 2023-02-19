04b1a8a1bf9d81613470a9ef985bcdc3e43b4a72
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2023-02-20 00:11:56 +0100

    Add Backend enum

---
+++ prusti-server/src/backend.rs
@@ -0,0 +1,19 @@
+use prusti_common::vir::ToViper;
+use viper::{AstFactory, VerificationResult};
+
+pub enum Backend<'a> {
+    Viper(viper::Verifier<'a>),
+}
+
+impl<'a> Backend<'a> {
+    pub fn verify(&mut self, program: &prusti_common::vir::Program) -> VerificationResult {
+        match self {
+            Backend::Viper(viper) => {
+                let ast_factory = AstFactory::new(&viper.env);
+                let program =
+                    program.to_viper(prusti_common::vir::LoweringContext::default(), &ast_factory);
+                viper.verify(program)
+            }
+        }
+    }
+}
