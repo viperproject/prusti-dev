a1563efda67f3a18a311a5b178b511b22eb4c362
Jan Schär <jschaer@student.ethz.ch>
2023-03-08 19:31:06 +0100

    Implement export/import of quantifiers
    When importing modules which contain specifications, these specs are
    also imported. The spec functions are serialized and written to a .specs
    file when the module is verified, and can be loaded from this file where
    the module is imported.
    However, if there are quantifiers in the exported specs, this did not
    work before. Quantifiers are represented as closures, and are not
    included the spec functions themselves, they need to be exported
    separately. This commit implements that.

--- prusti-tests/tests/cargo_verify/library_contracts_test/library_contracts_lib/src/lib.rs
+++ prusti-tests/tests/cargo_verify/library_contracts_test/library_contracts_lib/src/lib.rs
@@ -21,3 +21,21 @@ impl<T> Opt<T> {
         }
     }
 }
+
+#[trusted]
+#[pure]
+pub fn demo_fn(_a: u8) -> bool { unimplemented!() }
+
+// Test exporting quantifiers in specs.
+// Also test exporting triggers and nested quantifiers.
+#[trusted]
+#[ensures(forall(|i: u8| i == 0 ==> demo_fn(i)))]
+#[ensures(forall(|i: u8| i == 1 ==> forall(|j: u8| j == 1 ==> i == j), triggers=[(demo_fn(i),)]))]
+pub fn quantifier_test() {}
+
+// Test exporting quantifiers in predicates.
+predicate! {
+    pub fn quantifier_predicate(i: u8) -> bool {
+        forall(|j: u8| j < i ==> demo_fn(j))
+    }
+}
