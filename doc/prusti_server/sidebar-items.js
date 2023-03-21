2ae454cc5a4ce26f902e8b346d232d091f9a31b0
Aurel300 <Aurel300@users.noreply.github.com>
2023-03-21 15:03:33 +0000

    deploy: 2c988fdfa7a955e7e401aa8cfabc5d0154c6763b

--- doc/prusti_server/sidebar-items.js
+++ doc/prusti_server/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["Backend"],"fn":["dump_viper_program","process_verification_request","spawn_server_thread","start_server_on_port"],"struct":["PrustiClient","VerificationRequest","ViperBackendConfig"]};
+window.SIDEBAR_ITEMS = {"fn":["main","process"]};
