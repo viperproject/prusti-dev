648710e006f5b1ac61beffa0a81df6ebd8c79902
fpoli <fpoli@users.noreply.github.com>
2023-03-23 13:24:25 +0000

    deploy: 52cb249cd00c28e2c331b2627c20245f2126a716

--- doc/implementors/failure/trait.Fail.js
+++ doc/implementors/failure/trait.Fail.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
 "failure":[],
-"rustwide":[["impl <a class=\"trait\" href=\"failure/trait.Fail.html\" title=\"trait failure::Fail\">Fail</a> for <a class=\"enum\" href=\"rustwide/enum.PrepareError.html\" title=\"enum rustwide::PrepareError\">PrepareError</a>"],["impl <a class=\"trait\" href=\"failure/trait.Fail.html\" title=\"trait failure::Fail\">Fail</a> for <a class=\"enum\" href=\"rustwide/toolchain/enum.ToolchainError.html\" title=\"enum rustwide::toolchain::ToolchainError\">ToolchainError</a>"]]
+"rustwide":[["impl <a class=\"trait\" href=\"failure/trait.Fail.html\" title=\"trait failure::Fail\">Fail</a> for <a class=\"enum\" href=\"rustwide/toolchain/enum.ToolchainError.html\" title=\"enum rustwide::toolchain::ToolchainError\">ToolchainError</a>"],["impl <a class=\"trait\" href=\"failure/trait.Fail.html\" title=\"trait failure::Fail\">Fail</a> for <a class=\"enum\" href=\"rustwide/enum.PrepareError.html\" title=\"enum rustwide::PrepareError\">PrepareError</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
