25a6224e7a6c719334065656961a5519ab2b6602
Aurel Bílý <aurel.bily@gmail.com>
2023-05-05 11:47:55 +0200

    fix tests

--- prusti-tests/tests/verify/fail/unsupported/async2.rs
+++ prusti-tests/tests/verify/fail/unsupported/async2.rs
@@ -2,7 +2,7 @@ pub struct QueryClient {
 }
 impl QueryClient
 {
-    pub async fn accounts(
+    pub async fn accounts( //~ ERROR unsupported type
         &mut self,
         _request: u32
     ) -> Result<u32, u32> { //~ ERROR unsupported type
