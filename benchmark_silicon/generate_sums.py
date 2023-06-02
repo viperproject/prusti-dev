df47ffe204f7a937a58d3742dc52739e9d24f2d7
Vytautas Astrauskas <vastrauskas@gmail.com>
2023-06-02 13:48:30 +0200

    Silicon 18.

--- benchmark_silicon/generate_sums.py
+++ benchmark_silicon/generate_sums.py
@@ -95,8 +95,8 @@ def construct_sum(exhaled_location, locations, permission, no_permission):
         )
     return sum_expr
 
-def check_size(size):
-    #print(f"size: {size}")
+def check_size(size, add_group_assumptions):
+#   print(f"size: {size}")
     state = State()
     locations = []
     for _ in range(size):
@@ -110,18 +110,41 @@ def check_size(size):
         exhaled_sum = construct_sum(
             exhaled_location, locations[:i], -state._full_permission, state._no_permission)
     #   print(exhaled_sum)
-        assertion = z3.And(assertion, inhaled_sum + exhaled_sum >= state._full_permission)
+        check = inhaled_sum + exhaled_sum >= state._full_permission
+    #   print(check)
+        assertion = z3.And(assertion, check)
 
-    #print(assertion)
+        if add_group_assumptions:
+            for (j, location_group) in enumerate(locations):
+                inhaled = z3.If(
+                    exhaled_location == location_group,
+                    state._full_permission,
+                    state._no_permission,
+                )
+                exhaled = z3.If(
+                    exhaled_location == location_group,
+                    -state._full_permission,
+                    state._no_permission,
+                )
+                if j < i:
+                    conjunct = inhaled + exhaled >= 0
+                else:
+                    conjunct = inhaled >= 0
+        #       print(conjunct)
+                assertion = z3.And(assertion, conjunct)
+
+#   print(assertion)
     assertion = z3.Not(assertion)
     state.solver.add(assertion)
     start = datetime.datetime.now()
     result = state.solver.check()
     end = datetime.datetime.now()
-   #print(f"  start: {start}")
-   #print(f"  end: {end}")
-   #print(f"  duration: {end-start}")
+#   print(f"  start: {start}")
+#   print(f"  end: {end}")
+#   print(f"  duration: {end-start}")
+#   print(result)
     print(f"Size {size} completed in {end-start}")
+    # add_group_assumptions=False
     # Size 1 completed in 0:00:00.005373
     # Size 2 completed in 0:00:00.006233
     # Size 3 completed in 0:00:00.006431
@@ -142,12 +165,32 @@ def check_size(size):
     # Size 18 completed in 0:00:10.202696
     # Size 19 completed in 0:00:28.442166
     # Size 20 completed in 0:01:32.388904
+    # add_group_assumptions=True
+    # Size 1 completed in 0:00:00.006661
+    # Size 2 completed in 0:00:00.008673
+    # Size 3 completed in 0:00:00.008395
+    # Size 4 completed in 0:00:00.012773
+    # Size 5 completed in 0:00:00.012729
+    # Size 6 completed in 0:00:00.014022
+    # Size 7 completed in 0:00:00.012290
+    # Size 8 completed in 0:00:00.016343
+    # Size 9 completed in 0:00:00.021220
+    # Size 10 completed in 0:00:00.024776
+    # Size 11 completed in 0:00:00.032785
+    # Size 12 completed in 0:00:00.041238
+    # Size 13 completed in 0:00:00.046568
+    # Size 14 completed in 0:00:00.069082
+    # Size 15 completed in 0:00:00.457555
+    # Size 16 completed in 0:00:00.212668
+    # Size 17 completed in 0:00:00.139427
+    # Size 18 completed in 0:00:00.176176
+    # Size 19 completed in 0:00:00.180726
 
 def main():
     state = State()
 
-    for i in range(1, 50):
-        check_size(i)
+    for i in range(1, 20):
+        check_size(i, True)
 
 if __name__ == '__main__':
     main()
