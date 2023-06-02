e1e49d16dd51992d65119c86687c639e04aea01a
Vytautas Astrauskas <vastrauskas@gmail.com>
2023-06-02 11:58:47 +0200

    Silicon 15.

--- benchmark_silicon/generate_sums.py
+++ benchmark_silicon/generate_sums.py
@@ -13,6 +13,7 @@ class State:
         self.locations = []
         self.permissions = []
         self.sum = []
+        self.non_negativity_assumptions = []
         self.exhaled_location = self.fresh_location()
 
     def fresh_location(self):
@@ -39,15 +40,45 @@ class State:
             sum_expr = sum_expr + summand
         return sum_expr >= self._no_permission
 
+    def generate_sum(self):
+        sum_expr = self._no_permission
+        for summand in self.sum:
+            sum_expr = sum_expr + summand
+        return sum_expr
+
+    def is_full_assertion(self):
+        sum_expr = self._no_permission
+        for summand in self.sum:
+            sum_expr = sum_expr + summand
+        return sum_expr >= self._full_permission
+
+    def add_non_negativity_assumption(self):
+        self.solver.push()
+        assertion = self.is_non_negative_assertion()
+        self.solver.add(assertion)
+
     def check_assertion(self, assertion):
         assertion = z3.Not(assertion)
         # print(assertion)
         self.solver.push()
         self.solver.add(assertion)
-        print("checking")
+        print("checking: {assertion}")
+#       print(self.solver)
         start = datetime.datetime.now()
         result = self.solver.check()
         self.solver.pop()
+        end = datetime.datetime.now()
+        print(f"  start: {start}")
+        print(f"  end: {end}")
+        print(f"  duration: {end-start}")
+        return result
+
+    def check_assertion2(self, assertion):
+        assertion = z3.Not(assertion)
+        solver = z3.Solver()
+        solver.add(assertion)
+        start = datetime.datetime.now()
+        result = solver.check()
         end = datetime.datetime.now()
         print(f"  start: {start}")
         print(f"  end: {end}")
@@ -59,15 +90,36 @@ def main():
 
     full_permission = state.full_permission()
 
-    for i in range(100):
+    # TODO: If the original program does not have disjunctive aliasing, it
+    # is always enought to do experiment with non-negativity.
+
+#   TODO: check unsat of a sequence of implications:
+#       s1 = 0
+#       s1 >= 0
+#       s2 = s1 + ...
+#       s1 >= 0 ==> s2 >= 0
+#       s3 = s2 + ...
+#       s2 >= 0 and s1 >= 0 ==> s3 >= 0
+
+    sum_expr = state._no_permission
+    sum_expr_assume = z3.BoolVal(True)
+
+    for i in range(3):
         print(f"size: {i}")
         location = state.fresh_location()
 
-        state.add_summand(location, full_permission)
-        state.add_summand(location, -full_permission)
+        sum_expr_add = sum_expr + z3.If(state.exhaled_location == location, state._full_permission, state._no_permission)
+        sum_expr_assume_add = z3.And(sum_expr_assume, sum_expr_add >= 0)
 
-        assertion = state.is_non_negative_assertion()
-        print(state.check_assertion(assertion))
+        sum_expr_sub = sum_expr_add + z3.If(state.exhaled_location == location, -state._full_permission, state._no_permission)
+        sum_expr_assume_sub = z3.And(sum_expr_assume_add, sum_expr_sub >= 0)
+
+        sum_expr = sum_expr_sub
+        sum_expr_assume = sum_expr_assume_sub
+
+        print(sum_expr_assume)
+        if i % 10 == 0:
+            print(state.check_assertion2(sum_expr_assume))
 
 if __name__ == '__main__':
     main()
