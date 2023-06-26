f00675c189d9a77766dcdc683275b0242e8163fd
Federico Poli <federpoli@gmail.com>
2023-06-26 17:20:38 +0200

    Add flag to configure number of errors per function

--- prusti-server/src/verification_request.rs
+++ prusti-server/src/verification_request.rs
@@ -37,6 +37,10 @@ impl ViperBackendConfig {
         let mut verifier_args = config::extra_verifier_args();
         match backend {
             VerificationBackend::Silicon => {
+                verifier_args.push(format!(
+                    "--numberOfErrorsToReport={}",
+                    config::num_errors_per_function()
+                ));
                 if config::use_more_complete_exhale() {
                     verifier_args.push("--enableMoreCompleteExhale".to_string());
                 }
