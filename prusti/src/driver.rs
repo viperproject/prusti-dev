8cf753151a600530f9b59075fad6650d12c5506d
Federico Poli <federpoli@gmail.com>
2023-06-30 14:50:55 +0200

    Fix parsing error in desugared specifications
    Type ascription has been removed by RFC 803: https://github.com/rust-lang/rfcs/pull/3307

--- prusti/src/driver.rs
+++ prusti/src/driver.rs
@@ -209,7 +209,6 @@ fn main() {
         }
 
         rustc_args.push("-Zalways-encode-mir".to_owned());
-        rustc_args.push("-Zcrate-attr=feature(type_ascription)".to_owned());
         rustc_args.push("-Zcrate-attr=feature(stmt_expr_attributes)".to_owned());
         rustc_args.push("-Zcrate-attr=feature(register_tool)".to_owned());
         rustc_args.push("-Zcrate-attr=register_tool(prusti)".to_owned());
