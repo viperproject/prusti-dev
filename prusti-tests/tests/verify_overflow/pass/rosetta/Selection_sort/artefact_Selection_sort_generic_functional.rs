f1f8e38573f8217e0e67387fd890ea132e1efa3d
Vytautas Astrauskas <vastrauskas@gmail.com>
2023-12-09 15:33:30 +0100

    Verify sortedness.

--- prusti-tests/tests/verify_overflow/pass/rosetta/Selection_sort/artefact_Selection_sort_generic_functional.rs
+++ prusti-tests/tests/verify_overflow/pass/rosetta/Selection_sort/artefact_Selection_sort_generic_functional.rs
@@ -8,6 +8,7 @@
 //! +   Wrapped built-in types and functions.
 //! +   Rewrote loops into supported shape (while bool with no break, continue, or return).
 //! +   Replaced ``println!`` with calling trusted functions.
+//! +   Made generic over `T`.
 //!
 //! Verified properties:
 //!
@@ -83,7 +84,61 @@ impl<T> VecWrapper<T> {
     }
 }
 
+#[derive(Clone, Copy)]
+pub enum Ordering {
+    Less,
+    Equal,
+    Greater,
+}
+
+use self::Ordering::*;
+
+#[trusted]
+#[pure]
+#[ensures(matches!(result, Equal) == (a === b))]
+fn cmp<T: Ord>(a: &T, b: &T) -> Ordering {
+    match Ord::cmp(a, b) {
+        std::cmp::Ordering::Less => Less,
+        std::cmp::Ordering::Equal => Equal,
+        std::cmp::Ordering::Greater => Greater,
+    }
+}
+
+#[trusted]
+#[requires(cmp(elem, bound) === Less)]
+#[ensures(forall(|a: &T| matches!(cmp(bound, a), Less | Equal) ==>
+    matches!(cmp(elem, a), Less | Equal),
+    triggers=[(cmp(elem, a),)]
+))]
+fn lemma_cmp_less_transitive<T: Ord>(bound: &T, elem: &T) {}
+
+/*
+#[requires(0 <= bound && bound < array.len())]
+#[requires(0 <= elem && elem < array.len())]
+#[requires(cmp(array.lookup(elem), array.lookup(bound)) === Less)]
+#[ensures(forall(|a: &T| matches!(cmp(array.lookup(bound), a), Less | Equal) ==>
+    matches!(cmp(array.lookup(elem), a), Less | Equal),
+    triggers=[(cmp(array.lookup(elem), a),)]
+))]
+fn lemma_cmp_less_transitive_array<T: Ord>(array: &VecWrapper<T>, bound: usize, elem: usize) {
+    lemma_cmp_less_transitive(array.index(bound), array.index(elem));
+}
+*/
+
+#[trusted]
+#[requires(cmp(a, b) !== Less)]
+#[ensures(matches!(cmp(b, a), Less | Equal))]
+fn lemma_cmp_not_less<T: Ord>(a: &T, b: &T) {}
+
+macro_rules! smaller_equals {
+    ($array:expr, $k1:expr, $k2:expr) => {
+        matches!(cmp($array.lookup($k1), $array.lookup($k2)), Less | Equal)
+    };
+}
+
 #[ensures(array.len() == old(array.len()))]
+#[ensures(forall(|k1: usize, k2: usize| (0 <= k1 && k1 < k2 && k2 < array.len()) ==>
+             smaller_equals!(array, k1, k2)))]
 fn selection_sort<T: Ord>(mut array: &mut VecWrapper<T>) {
 
     let mut min;
@@ -94,17 +149,46 @@ fn selection_sort<T: Ord>(mut array: &mut VecWrapper<T>) {
         body_invariant!(array.len() == old(array.len()));
         body_invariant!(0 <= i && i <= array.len());
         body_invariant!(continue_loop_1 ==> i < array.len());
+        body_invariant!(!continue_loop_1 ==> i == array.len());
+        body_invariant!(forall(|k1: usize, k2: usize| (0 <= k1 && k1 < k2 && k2 < i) ==>
+                 smaller_equals!(array, k1, k2)));
+        body_invariant!(forall(|k1: usize, k2: usize| (0 <= k1 && k1 < i && i <= k2 && k2 < array.len()) ==>
+                 smaller_equals!(array, k1, k2)));
         min = i;
 
         let mut j = i+1;
         let mut continue_loop_2 = j < array.len();
         while continue_loop_2 {
             body_invariant!(array.len() == old(array.len()));
-            body_invariant!(0 < j && j <= array.len());
-            body_invariant!(continue_loop_2 ==> j < array.len());
-            body_invariant!(0 <= min && min < array.len());
-            if *array.index(j) < *array.index(min) {
+            body_invariant!(0 <= i && i < array.len());
+            body_invariant!(forall(|k1: usize, k2: usize| (0 <= k1 && k1 < k2 && k2 < i) ==>
+                     smaller_equals!(array, k1, k2)));
+            body_invariant!(forall(|k1: usize, k2: usize| (0 <= k1 && k1 < i && i <= k2 && k2 < array.len()) ==>
+                     smaller_equals!(array, k1, k2)));
+
+            body_invariant!(i < j);
+            body_invariant!(j < array.len());
+            body_invariant!(i <= min);
+            body_invariant!(min < array.len());
+            body_invariant!(forall(|k1: usize| (0 <= k1 && k1 < i) ==>
+                    smaller_equals!(array, k1, min)));
+            body_invariant!(forall(|k: usize|
+                     (i <= k && k < j && k < array.len()) ==>
+                     smaller_equals!(array, min, k)));
+            let cmp_result = cmp(array.index(j), array.index(min));
+            if matches!(cmp_result, Less) {
+                lemma_cmp_less_transitive(array.index(min), array.index(j));
+                //lemma_cmp_less_transitive_array(array, min, j);
+//prusti_assume!(
+//forall(|a: &T|
+//cmp(array.lookup(j), array.lookup(min)) === Less &&
+//matches!(cmp(array.lookup(min), a), Less | Equal) ==>
+//matches!(cmp(array.lookup(j), a), Less | Equal)
+       //)
+    //);
                 min = j;
+            } else {
+                lemma_cmp_not_less(array.index(j), array.index(min));
             }
             j += 1;
             continue_loop_2 = j < array.len();
