f38f8987d2d1e26964d6a4e2f54d3c783bba29d9
Vytautas Astrauskas <vastrauskas@gmail.com>
2024-02-22 23:41:43 +0100

    Ugly commit 30.42

--- prusti-viper/src/encoder/middle/core_proof/svirpti/smt/configuration.rs
+++ prusti-viper/src/encoder/middle/core_proof/svirpti/smt/configuration.rs
@@ -12,19 +12,30 @@ pub struct Configuration {
 
 impl Default for Configuration {
     fn default() -> Self {
-        let smt_conf = SmtConf::z3(config::svirpti_smt_solver());
+        let mut smt_conf = SmtConf::z3(config::svirpti_smt_solver());
+        smt_conf.check_success();
         let attributes = vec![(":smt-lib-version 2.0")]
             .into_iter()
             .map(|attribute| (attribute.into()))
             .collect();
         let options = vec![
-            (":AUTO_CONFIG", "false"),
-            (":smt.MBQI", "false"),
-            (":TYPE_CHECK", "true"),
-        ]
-        .into_iter()
-        .map(|(option, value)| (option.into(), value.into()))
-        .collect();
+            // Silicon.
+            (":global-decls".to_string(), "true".to_string()),
+            (":auto_config".to_string(), "false".to_string()),
+            (":smt.case_split".to_string(), "3".to_string()),
+            (":smt.delay_units".to_string(), "true".to_string()),
+            (":type_check".to_string(), "true".to_string()),
+            (":smt.mbqi".to_string(), "false".to_string()),
+            (":pp.bv_literals".to_string(), "false".to_string()),
+            (":smt.arith.solver".to_string(), "2".to_string()),
+            (":model.v2".to_string(), "true".to_string()),
+            (":smt.qi.max_multi_patterns".to_string(), "1000".to_string()),
+            // Prusti.
+            (":smt.qi.eager_threshold".to_string(), config::smt_qi_eager_threshold().to_string()),
+            (":model.partial".to_string(), config::counterexample().to_string()),
+            (":smt.arith.nl".to_string(), config::smt_use_nonlinear_arithmetic_solver().to_string()),
+            // (":smt.arith.nl.gb".to_string(), config::smt_use_nonlinear_arithmetic_solver().to_string()),
+        ];
         let tee_path = config::svirpti_smt_solver_log();
         Self {
             smt_conf,
