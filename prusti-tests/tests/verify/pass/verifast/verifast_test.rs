20e2d6212e3c1a8243e14a029e17b82755966d7b
Herddex <Tudor Jinga>
2025-07-01 08:34:58 +0300

    Simplify example

--- prusti-tests/tests/verify/pass/verifast/verifast_test.rs
+++ prusti-tests/tests/verify/pass/verifast/verifast_test.rs
@@ -1,44 +1,41 @@
 extern crate prusti_contracts;
 
-mod functions {
-    use prusti_contracts::*;
+#[repr(C)]
+pub struct Point {
+    pub x: i32,
+    pub y: i32,
+}
 
-    #[repr(C)]
-    pub struct Point {
-        pub x: i32,
-        pub y: i32,
-    }
+use prusti_contracts::*;
 
-    #[extern_spec(
-        {file = "prusti-tests/tests/verify/pass/verifast/functions.c",}
-        crate::functions)]
-    extern "C" {
-        #[ensures({translator = "verifast"}result == a + b)]
-        pub fn add(a: i32, b: i32) -> i32;
+#[extern_spec(
+    {file = "prusti-tests/tests/verify/pass/verifast/functions.c",}
+    crate)]
+extern "C" {
+    #[ensures({translator = "verifast"}result == a + b)]
+    pub fn add(a: i32, b: i32) -> i32;
 
-        #[requires(true)]
-        #[ensures({translator = "verifast"}p.x == old(p.y))]
-        #[ensures({translator = "verifast"}p.y == old(p.x))]
-        pub fn swap(p: &mut Point);
+    #[ensures({translator = "verifast"}p.x == old(p.y))]
+    #[ensures({translator = "verifast"}p.y == old(p.x))]
+    pub fn swap(p: &mut Point);
 
-        #[ensures({translator = "verifast"}
-          p.x == old(p.y + p.x) && p.y == old(p.y - p.x))]
-        #[ensures({translator = "verifast"}result == p.x * p.y)]
-        pub fn mangle(p: &mut Point) -> i32;
+    #[ensures({translator = "verifast"}
+        p.x == old(p.y + p.x) && p.y == old(p.y - p.x))]
+    #[ensures({translator = "verifast"}result == p.x * p.y)]
+    pub fn mangle(p: &mut Point) -> i32;
 
-        #[ensures({translator = "verifast"}result == p.x * p.x + p.y * p.y)]
-        pub fn squared_magnitude(p: &Point) -> i32;
-    }
+    #[ensures({translator = "verifast"}result == p.x * p.x + p.y * p.y)]
+    pub fn squared_magnitude(p: &Point) -> i32;
+}
 
-    extern "C" {
-        pub fn add(a: i32, b: i32) -> i32;
-        
-        pub fn swap(p: &mut Point);
+extern "C" {
+    pub fn add(a: i32, b: i32) -> i32;
+    
+    pub fn swap(p: &mut Point);
 
-        pub fn mangle(p: &mut Point) -> i32;
+    pub fn mangle(p: &mut Point) -> i32;
 
-        pub fn squared_magnitude(p: &Point) -> i32;
-    }
+    pub fn squared_magnitude(p: &Point) -> i32;
 }
 
 fn main() {
@@ -46,13 +43,13 @@ fn main() {
 
     let a = 3;
     let b = 4;
-    let r = unsafe { functions::add(a, b) };
+    let r = unsafe { add(a, b) };
     prusti_assert_eq!(r, a + b);
 
     let x_val = 11;
     let y_val = 5;
-    let mut p = functions::Point { x: x_val, y: y_val };
-    unsafe { functions::swap(&mut p);}
+    let mut p = Point { x: x_val, y: y_val };
+    unsafe { swap(&mut p);}
     prusti_assert_eq!(p.x, y_val);
     prusti_assert_eq!(p.y, x_val);
 
@@ -60,14 +57,14 @@ fn main() {
     let new_y_val = x_val - y_val;
     let r_val = new_x_val * new_y_val;
     
-    let r = unsafe { functions::mangle(&mut p) };
+    let r = unsafe { mangle(&mut p) };
     
     prusti_assert_eq!(r, r_val);
     prusti_assert_eq!(p.x, new_x_val);
     prusti_assert_eq!(p.y, new_y_val);
 
-    let p = functions::Point { x: a, y: b };
-    let r = unsafe { functions::squared_magnitude(&p) };
+    let p = Point { x: a, y: b };
+    let r = unsafe { squared_magnitude(&p) };
     prusti_assert_eq!(r, a*a + b*b);
     prusti_assert_eq!(p.x, a);
     prusti_assert_eq!(p.y, b);
